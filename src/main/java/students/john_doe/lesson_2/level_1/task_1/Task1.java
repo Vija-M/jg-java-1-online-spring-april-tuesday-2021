0e7c0c10dcf9031763352a6442555e199d776c63
Konstantins Buts <konstantins.buts@neotech.lv>
2021-04-27 21:46:01 +0300

    Completed Task 1 Lesson 2

---
+++ src/main/java/students/john_doe/lesson_2/level_1/task_1/Task1.java
@@ -0,0 +1,17 @@
+package students.john_doe.lesson_2.level_1.task_1;
+
+class Task1 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int sum = a + b;
+        System.out.println("sum = " + sum);
+
+        // TODO
+    }
+}
