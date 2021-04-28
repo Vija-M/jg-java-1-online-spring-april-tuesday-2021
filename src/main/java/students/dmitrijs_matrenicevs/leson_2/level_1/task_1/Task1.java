24ffbfb841b5cf28be081b1bfed3a6cb70745d41
DeTELo <mrdinas@gmail.com>
2021-04-28 23:54:51 +0300

    Task 1 Lesson 2 is ready

---
+++ src/main/java/students/dmitrijs_matrenicevs/leson_2/level_1/task_1/Task1.java
@@ -0,0 +1,36 @@
+package students.dmitrijs_matrenicevs.leson_2.level_1.task_1;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+class Task1 {
+
+    public static void main(String[] args) {
+
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int sum = a + b;
+        System.out.println("sum = " + sum);
+
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int result = c - d;
+        System.out.println("sum = " + result);
+
+        int e = scanner.nextInt();
+        int r = scanner.nextInt();
+
+        int kit = e * r;
+        System.out.println("sum = " + kit);
+
+        int y = scanner.nextInt();
+        int i = scanner.nextInt();
+
+        int bb = y / i;
+        System.out.println("sum = " + bb);
+    }
+}
