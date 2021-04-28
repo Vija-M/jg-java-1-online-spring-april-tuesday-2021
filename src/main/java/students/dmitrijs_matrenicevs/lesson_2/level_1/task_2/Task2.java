d5ddaeb42faa87034922735287d71b073bd692d2
DeTELo <mrdinas@gmail.com>
2021-04-29 00:23:58 +0300

    Task 2 Lesson 2 is ready

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_1/task_2/Task2.java
@@ -0,0 +1,12 @@
+package students.dmitrijs_matrenicevs.lesson_2.level_1.task_2;
+
+class Task2 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+
+
+    }
+}
