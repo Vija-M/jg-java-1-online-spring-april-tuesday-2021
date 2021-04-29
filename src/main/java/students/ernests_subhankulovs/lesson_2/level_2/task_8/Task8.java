ce9c544df0a07331ec70d5455e6d9ae1f9e6153a
Ernests1992 <ernests@neolain.lv>
2021-04-29 16:50:27 +0300

    Completed Lesson 2 Level 2 Task 8. Small correction.

--- src/main/java/students/ernests_subhankulovs/lesson_2/level_2/task_8/Task8.java
+++ src/main/java/students/ernests_subhankulovs/lesson_2/level_2/task_8/Task8.java
@@ -1,6 +1,6 @@
 package students.ernests_subhankulovs.lesson_2.level_2.task_8;
 
-public class Task8 {
+class Task8 {
     public static void main(String[] args) {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
 
