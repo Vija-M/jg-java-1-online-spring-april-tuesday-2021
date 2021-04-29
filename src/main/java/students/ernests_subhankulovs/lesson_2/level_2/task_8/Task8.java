61ff2ce95d630d22cb3ecc83b68e0c70305211b6
Ernests1992 <ernests@neolain.lv>
2021-04-29 16:25:03 +0300

    Draft.

---
+++ src/main/java/students/ernests_subhankulovs/lesson_2/level_2/task_8/Task8.java
@@ -0,0 +1,11 @@
+package students.ernests_subhankulovs.lesson_2.level_2.task_8;
+
+public class Task8 {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.println("Enter Your name:");
+        String name = scanner.nextLine();
+
+    }
+}
