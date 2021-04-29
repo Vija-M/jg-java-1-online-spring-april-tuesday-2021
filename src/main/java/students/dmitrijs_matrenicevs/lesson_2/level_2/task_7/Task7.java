b0d8d29001457d6cb35e84e287798ef8d425b006
DeTELo <mrdinas@gmail.com>
2021-04-29 22:56:01 +0300

    Task 7 Lesson 2 it's ready

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_2/task_7/Task7.java
@@ -0,0 +1,10 @@
+package students.dmitrijs_matrenicevs.lesson_2.level_2.task_7;
+
+class Task7 {
+
+    public static void main(String[] args) {
+
+        System.out.println("You can login!");
+    }
+
+}
