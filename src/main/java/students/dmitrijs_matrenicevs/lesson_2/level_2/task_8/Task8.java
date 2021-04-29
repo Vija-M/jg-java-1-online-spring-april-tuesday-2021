b9ebdd1e9525ce9727f6353cff101e7153ddae79
DeTELo <mrdinas@gmail.com>
2021-04-29 23:16:16 +0300

    Task 8 Lesson 2 it's ready

--- src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_2/task_8/Task8.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_2/task_8/Task8.java
@@ -4,7 +4,13 @@ class Task8 {
 
     public static void main(String[] args) {
 
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
 
+        System.out.print("Enter your name: ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
 
     }
 }
