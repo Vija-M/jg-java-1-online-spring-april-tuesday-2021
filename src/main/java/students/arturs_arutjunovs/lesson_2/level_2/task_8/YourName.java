eb096688fa30afd5fc6f8819b34abf690054934c
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-04-29 21:46:34 +0100

    Completed   Lesson 2 Level 2 Task 8

---
+++ src/main/java/students/arturs_arutjunovs/lesson_2/level_2/task_8/YourName.java
@@ -0,0 +1,13 @@
+package students.arturs_arutjunovs.lesson_2.level_2.task_8;
+
+public class YourName {
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name + "!");
+    }
+}
