eb344f1577806e260f2d73bf602d7eeaece1db25
Vija-M <vija.riga@gmailcom>
2021-04-29 20:49:11 +0300

    Completed Task 8, Lesson 2

--- src/main/java/students/vija_m/lesson_2/level_2/task_8/Task8.java
+++ src/main/java/students/vija_m/lesson_2/level_2/task_8/Task8.java
@@ -1,14 +1,16 @@
 package students.vija_m.lesson_2.level_2.task_8;
 
 class Task8 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+            java.util.Scanner scanner = new java.util.Scanner(System.in);
 
-        System.out.print("Please, enter your name: ");
-        String name = scanner.nextLine();
+            System.out.print("Please, enter your name: ");
+            String name = scanner.nextLine();
 
-        System.out.println("Hello " + name + "!");
+            System.out.println("Hello " + name + "!");
 
+        }
     }
-}
+
+
