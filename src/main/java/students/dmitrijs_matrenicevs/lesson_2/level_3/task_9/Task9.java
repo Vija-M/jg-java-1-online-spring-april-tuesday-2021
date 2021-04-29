2b57146d9426388af212e80d35078fcee6c03069
DeTELo <mrdinas@gmail.com>
2021-04-29 23:57:29 +0300

    Task 9 Lesson 2 it's ready

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_3/task_9/Task9.java
@@ -0,0 +1,60 @@
+package students.dmitrijs_matrenicevs.lesson_2.level_3.task_9;
+
+class Task9 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Enter number 1 or 10:" );
+
+        String number = scanner.nextLine();
+
+        int a = 8;
+        int b = 1;
+        int c = a * b;
+
+        System.out.println(a + " * " + b + " = " + c);
+
+        int q = 8;
+        int w = 2;
+        int e = a * w;
+
+        System.out.println(q + " * " + w + " = " + e);
+
+        int r = 8;
+        int t = 3;
+        int y = a * t;
+
+        System.out.println(r + " * " + t + " = " + y);
+
+        int u = 8;
+        int i = 4;
+        int o = a * i;
+
+        System.out.println(u + " * " + i + " = " + o);
+
+        int p = 8;
+        int f = 5;
+        int g = p * f;
+
+        System.out.println(p + " * " + f + " = " + g);
+
+        int h = 8;
+        int j = 6;
+        int k = h * j;
+
+        System.out.println(h + " * " + j + " = " + k);
+
+        int l = 8;
+        int z = 7;
+        int x = l * z;
+
+        System.out.println(l + " * " + z + " = " + x);
+
+        int v = 8;
+        int m = 8;
+        int n = v * m;
+
+        System.out.println(v + " * " + m + " = " + n);
+    }
+}
