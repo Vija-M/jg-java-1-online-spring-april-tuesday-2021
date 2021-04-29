ef2fdc250bb2bd9586919b4c06ba259db0c4ab5b
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-04-29 21:10:20 +0100

    Completed   Lesson 2 Level 2 Task 5

---
+++ src/main/java/students/arturs_arutjunovs/lesson_2/level_2/task_5/BankAccount.java
@@ -0,0 +1,8 @@
+package students.arturs_arutjunovs.lesson_2.level_2.task_5;
+
+class BankAccount {
+    public static void main(String[] args) {
+
+    }
+}        //добавлена фигурная скобка, закрывающая класс
+
