9d5c787c669969badc6e4050ade10a471d683a08
Vija-M <vija.riga@gmailcom>
2021-04-30 19:36:45 +0300

    Task 9, Lesson 2

---
+++ src/main/java/students/vija_m/lesson_2/level_3/task_9/Task9.java
@@ -0,0 +1,24 @@
+package students.vija_m.lesson_2.level_3.task_9;
+
+class Task9 {
+
+        public static void main(String[] args)
+java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Пожалуйста, с целью получения таблицы умножения введите целое число: ");
+        int a = scanner.nextInt();
+        System.out.print("Вот запрашиваемая таблица уиножения на" + a + ":");
+        System.out.println(a + " * 4 = " + (a * 4) + ";");
+        System.out.println(a + " * 1 = " + (a * 1) + ";");
+        System.out.println(a + " * 2 = " + (a * 2) + ";");
+        System.out.println(a + " * 3 = " + (a * 3) + ";");
+        System.out.println(a + " * 4 = " + (a * 4) + ";");
+        System.out.println(a + " * 5 = " + (a * 5) + ";");
+        System.out.println(a + " * 6 = " + (a * 6) + ";");
+        System.out.println(a + " * 7 = " + (a * 7) + ";");
+        System.out.println(a + " * 8 = " + (a * 8) + ";");
+        System.out.println(a + " * 9 = " + (a * 9) + ";");
+        System.out.println(a + " * 10 = " + (a * 10) + ";");
+
+}
+    }
+
