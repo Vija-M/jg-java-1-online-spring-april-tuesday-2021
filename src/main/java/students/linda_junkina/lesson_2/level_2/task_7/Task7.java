932fc972298b4a994e4f5a6c729773998ac6a3ee
Linda-Ju <linda.junkina@gmail.com>
2021-04-30 22:30:51 +0300

    Lesson 2, Level 2, Task 7

---
+++ src/main/java/students/linda_junkina/lesson_2/level_2/task_7/Task7.java
@@ -0,0 +1,14 @@
+package students.linda_junkina.lesson_2.level_2.task_7;
+
+class Task7 {
+
+//class LoginService {
+
+        public static void main(String[] args) {
+            java.util.Scanner scanner = new java.util.Scanner(System.in);
+            System.out.println("You can login!");
+        }
+
+
+    }
+// }
