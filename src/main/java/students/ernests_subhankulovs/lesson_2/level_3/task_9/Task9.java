243fc6d2be7883dd3eb1527e75e3ffdfca483dc6
Ernests1992 <ernests@neolain.lv>
2021-04-30 16:05:40 +0300

    Lesson 2 Level 3 Task 9.

---
+++ src/main/java/students/ernests_subhankulovs/lesson_2/level_3/task_9/Task9.java
@@ -0,0 +1,16 @@
+package students.ernests_subhankulovs.lesson_2.level_3.task_9;
+
+public class Task9 {
+    public static void main(String[] args) {
+        int result;
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.println("Input a number: ");
+        int number = scanner.nextInt();
+
+        for(int i = 1; i <=10; i++) {
+            result = number * i;
+            System.out.println(number + " X " + i + " = " + result);
+        }
+    }
+}
