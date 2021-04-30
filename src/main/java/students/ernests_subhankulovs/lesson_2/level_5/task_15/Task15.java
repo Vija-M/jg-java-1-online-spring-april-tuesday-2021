cb42050f65e0110c52a073a0ac7e4b062cc050ac
Ernests1992 <ernests@neolain.lv>
2021-04-30 17:08:31 +0300

    Lesson 2 Level 5 Task 15.

---
+++ src/main/java/students/ernests_subhankulovs/lesson_2/level_5/task_15/Task15.java
@@ -0,0 +1,9 @@
+package students.ernests_subhankulovs.lesson_2.level_5.task_15;
+
+class Task15 {
+    public static void main(String[] args) {
+        System.out.println("Line 1 (println): This sentence ends with a line break.");
+        System.out.print("Line 2 (print): This sentence ends... ");
+        System.out.println("Line 3 (println): ...with a line break.");
+    }
+}
