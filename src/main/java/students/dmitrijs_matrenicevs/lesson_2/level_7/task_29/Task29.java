0634207557bc556c2aa51d2b19adea47ae1f7e71
DeTELo <mrdinas@gmail.com>
2021-05-01 23:25:08 +0300

    Task 29 Lesson 2 it's ready

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_7/task_29/Task29.java
@@ -0,0 +1,17 @@
+package students.dmitrijs_matrenicevs.lesson_2.level_7.task_29;
+
+class Task29 {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("10 = " + numberOne);
+        System.out.println("20.0 = " + numberTwo);
+
+        double sum = numberOne + numberTwo;
+
+        System.out.println(numberOne + "+" + numberTwo + " = " + sum);
+    }
+}
