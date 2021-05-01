27e1d76b87d1cce900da19a51c665bbe5385e352
DeTELo <mrdinas@gmail.com>
2021-05-01 20:51:57 +0300

    Task 2 Lesson 2 it's ready

--- src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_1/task_2/Task2.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_1/task_2/Task2.java
@@ -5,39 +5,19 @@ class Task2 {
     public static void main(String[] args) {
 
         java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Enter any 2 numbers: ");
 
-        //sum
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
+        double c = a + b;
+        double d = a - b;
+        double f = a * b;
+        double g = a / b;
 
-        double sum = a + b;
-
-        System.out.println("sum = " + sum);
-
-        //minus
-        double c = scanner.nextDouble();
-        double d = scanner.nextDouble();
-
-        double minus = c - d;
-
-        System.out.println("sum = " + minus);
-
-        //multiply
-        double e = scanner.nextDouble();
-        double f = scanner.nextDouble();
-
-        double multiply = e * f;
-
-        System.out.println("sum = " + multiply);
-
-        //share
-        double t = scanner.nextDouble();
-        double y = scanner.nextDouble();
-
-        double share = t / y;
-
-        System.out.println("sum = " + share);
-
+        System.out.println("Plus:" + /*"a + b"*/ " = " + c);
+        System.out.println("Minus:" + /*"a - b"*/ " = " + d);
+        System.out.println("Multiply:" + /*"a * b"*/ " = " + f);
+        System.out.println("Share:" + /*"a / b"*/ " = " + g);
 
     }
 }
