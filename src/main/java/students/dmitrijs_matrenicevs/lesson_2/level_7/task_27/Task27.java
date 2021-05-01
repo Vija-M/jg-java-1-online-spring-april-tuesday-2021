060b0980a1015043c5a6548e2eb7de103b19e4b7
DeTELo <mrdinas@gmail.com>
2021-05-01 22:55:14 +0300

    Task 27 Lesson 2 it's ready

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_7/task_27/Task27.java
@@ -0,0 +1,50 @@
+package students.dmitrijs_matrenicevs.lesson_2.level_7.task_27;
+
+class Task27 {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Book {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class BankAccount {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class SystemUnit {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class LoginService {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class BankTransaction {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Task1 {
+
+    public static void main(String[] args) {
+
+    }
+}
