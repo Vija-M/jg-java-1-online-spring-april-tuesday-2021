6eccb380dfc1a5c2f07003b06b1be10d89a0a19a
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-01 22:25:28 +0100

    Completed   Lesson 2 Level 7 Task 27

---
+++ src/main/java/students/arturs_arutjunovs/lesson_2/level_7/task_27/Task_27.java
@@ -0,0 +1,13 @@
+package students.arturs_arutjunovs.lesson_2.level_7.task_27;
+
+class Task_27 {
+    /*исправленные названия классов:
+
+class Book
+class BankAccount
+class SystemUnit
+class LoginService
+class BankTransaction
+class Task1
+     */
+}
