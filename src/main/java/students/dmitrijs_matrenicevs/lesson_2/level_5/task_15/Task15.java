6aeb40c221bf3d235e7021151571f5befd582257
DeTELo <mrdinas@gmail.com>
2021-05-01 22:22:39 +0300

    Task 15 Lesson 2 it's ready

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_5/task_15/Task15.java
@@ -0,0 +1,11 @@
+package students.dmitrijs_matrenicevs.lesson_2.level_5.task_15;
+
+class Task15 {
+
+    public static void main(String[] args) {
+
+        System.out.print("Text: ");
+
+    }
+
+}
