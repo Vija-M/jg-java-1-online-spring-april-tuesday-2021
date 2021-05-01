37676c0030595303e8e7cfab9cef28df91971eca
DeTELo <mrdinas@gmail.com>
2021-05-01 22:45:18 +0300

    Task 24 Lesson 2 it's ready

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_6/task_24/Task24.java
@@ -0,0 +1,10 @@
+package students.dmitrijs_matrenicevs.lesson_2.level_6.task_24;
+
+class Task24 {
+
+    public static void main(String[] args) {
+
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
