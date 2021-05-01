5267d1edf6affa0ce13820cfd299827447f2290d
DeTELo <mrdinas@gmail.com>
2021-05-01 22:42:52 +0300

    Task 23 Lesson 2 it's ready

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_6/task_23/Task23.java
@@ -0,0 +1,12 @@
+package students.dmitrijs_matrenicevs.lesson_2.level_6.task_23;
+
+class Task23 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Code outside the method will not compile!");
+
+    }
+
+
+}
