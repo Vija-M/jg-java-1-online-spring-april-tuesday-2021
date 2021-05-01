d9279144ce13367a09a79d993683ac53143a0b48
DeTELo <mrdinas@gmail.com>
2021-05-01 20:49:33 +0300

    Task 1 Lesson 2 it's ready

--- src/main/java/students/dmitrijs_matrenicevs/leson_2/level_1/task_1/Task1.java
+++ src/main/java/students/dmitrijs_matrenicevs/leson_2/level_1/task_1/Task1.java
@@ -7,33 +7,17 @@ class Task1 {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         System.out.print("Enter any 2 numbers: ");
 
-        //sum
         int a = scanner.nextInt();
         int b = scanner.nextInt();
+        int c = a + b;
+        int d = a - b;
+        int f = a * b;
+        int g = a / b;
 
-        int sum = a + b;
+        System.out.println("Plus:" + /*"a + b"*/ " = " + c);
+        System.out.println("Minus:" + /*"a - b"*/ " = " + d);
+        System.out.println("Multiply:" + /*"a * b"*/ " = " + f);
+        System.out.println("Share:" + /*"a / b"*/ " = " + g);
 
-        System.out.println("sum = " + sum);
-
-        //minus
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
-
-        int minus = c - d;
-        System.out.println("sum = " + minus);
-
-        //multiply
-        int e = scanner.nextInt();
-        int r = scanner.nextInt();
-
-        int multiply = e * r;
-        System.out.println("sum = " + multiply);
-
-        //share
-        int y = scanner.nextInt();
-        int i = scanner.nextInt();
-
-        int share = y / i;
-        System.out.println("sum = " + share);
     }
 }
