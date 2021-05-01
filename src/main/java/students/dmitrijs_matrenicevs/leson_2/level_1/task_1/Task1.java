047a17d8da1f3d7542b4560e789828fe2e4b2425
DeTELo <mrdinas@gmail.com>
2021-05-01 20:15:01 +0300

    Task 1 Lesson 2 it's ready

--- src/main/java/students/dmitrijs_matrenicevs/leson_2/level_1/task_1/Task1.java
+++ src/main/java/students/dmitrijs_matrenicevs/leson_2/level_1/task_1/Task1.java
@@ -4,8 +4,8 @@ class Task1 {
 
     public static void main(String[] args) {
 
-
         java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Enter any 2 numbers: ");
 
         //sum
         int a = scanner.nextInt();
