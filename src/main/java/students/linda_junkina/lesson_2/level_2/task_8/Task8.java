d3b4922ce75ea6f278299c9c5a5b8c0cf6e6e3a5
Linda-Ju <linda.junkina@gmail.com>
2021-05-01 21:54:17 +0300

    Lesson 2, Level 2, Task 8

---
+++ src/main/java/students/linda_junkina/lesson_2/level_2/task_8/Task8.java
@@ -0,0 +1,15 @@
+package students.linda_junkina.lesson_2.level_2.task_8;
+
+ class Task8 {
+     public static void main(String[] args) {
+         java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+         System.out.print(" What is your name? ");
+         String name = scanner.nextLine();
+
+
+         System.out.println(" Hello " + name + "!" );
+
+
+     }
+}
