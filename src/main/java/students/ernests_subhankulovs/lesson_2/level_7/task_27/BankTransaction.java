f23819dd12405d17818fd93195634f58dcbe08c0
Ernests1992 <ernests@neolain.lv>
2021-05-02 16:56:57 +0300

    Lesson 2 Level 6 Task 27.

---
+++ src/main/java/students/ernests_subhankulovs/lesson_2/level_7/task_27/BankTransaction.java
@@ -0,0 +1,7 @@
+package students.ernests_subhankulovs.lesson_2.level_7.task_27;
+
+public class BankTransaction {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/students/ernests_subhankulovs/lesson_2/level_7/task_27/Task1.java
@@ -0,0 +1,7 @@
+package students.ernests_subhankulovs.lesson_2.level_7.task_27;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
