a1378bc9b905180e62a2339ce607bf98dcc59b37
OlgaT <ok.latvia@gmail.com>
2021-05-02 15:06:42 +0300

    Добавила Lesson 2 Level 7 Task 28

---
+++ src/main/java/students/olga_t/lesson_2/tasks/Task16.java
@@ -0,0 +1,25 @@
+package students.olga_t.lesson_2.tasks;
+
+class Task16 {
+    /*
+https://www.w3resource.com/java-exercises/basic/index.php
+    16. Write a Java program to print a face. Go to the editor
+Expected Output
+
+ +"""""+
+[| o o |]
+ |  ^  |
+ | '-' |
+ +-----+
+     */
+
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  |");
+        System.out.println(" | '-' |");
+        System.out.println(" +-----+");
+
+    }
+
+}
