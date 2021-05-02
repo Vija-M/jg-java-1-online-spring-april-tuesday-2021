681f5c0b6215dd59b4b1aab31c2cda3780017643
eriks <erikslv88@gmail.com>
2021-05-02 14:43:10 +0300

    Finish - 2 lesson 5 level 18 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_2/level_5/task_18/Fix.java
@@ -0,0 +1,14 @@
+package students.eriks_jaloveckis.lesson_2.level_5.task_18;
+
+class Fix {
+
+    /*
+    class SizeMatters {
+        public static void main(String[] args) {
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+        }
+    }
+    */
+
+}
