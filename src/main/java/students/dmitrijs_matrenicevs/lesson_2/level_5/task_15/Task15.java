e4eb5c77475b1dfa2573a7fb33bb152dd89e7ea2
DeTELo <mrdinas@gmail.com>
2021-05-02 20:29:57 +0300

    Task 15 Lesson 2 it's ready

--- src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_5/task_15/Task15.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_5/task_15/Task15.java
@@ -3,8 +3,11 @@ package students.dmitrijs_matrenicevs.lesson_2.level_5.task_15;
 class Task15 {
 
     public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
 
-        System.out.print("Text: ");
+        System.out.print("Введи что хочешь: ");
+        int a = scanner.nextInt();
+        System.out.print("То что ты ввел: " + a);
 
     }
 
