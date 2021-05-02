1d3810b24072275ccfd4f1036897ae5a767e08bc
Linda-Ju <linda.junkina@gmail.com>
2021-05-02 22:58:09 +0300

    Lesson 2, Level 3, Task 9

---
+++ src/main/java/students/linda_junkina/lesson_2/level_3/task_9/Task9.java
@@ -0,0 +1,33 @@
+package students.linda_junkina.lesson_2.level_3.task_9;
+
+class Task9 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+
+        System.out.print(" Please, enter a number: ");
+        int enteredNumber = scanner.nextInt();
+        int number1 = enteredNumber * 1;
+        int number2 = enteredNumber * 2;
+        int number3 = enteredNumber * 3;
+        int number4 = enteredNumber * 4;
+        int number5 = enteredNumber * 5;
+        int number6 = enteredNumber * 6;
+        int number7 = enteredNumber * 7;
+        int number8 = enteredNumber * 8;
+        int number9 = enteredNumber * 9;
+
+        System.out.println(" Here are your results: ");
+        System.out.println(enteredNumber + " * 1 = " + number1);
+        System.out.println(enteredNumber + " * 2 = " + number2);
+        System.out.println(enteredNumber + " * 3 = " + number3);
+        System.out.println(enteredNumber + " * 4 = " + number4);
+        System.out.println(enteredNumber + " * 5 = " + number5);
+        System.out.println(enteredNumber + " * 6 = " + number6);
+        System.out.println(enteredNumber + " * 7 = " + number7);
+        System.out.println(enteredNumber + " * 8 = " + number8);
+        System.out.println(enteredNumber + " * 9 = " + number9);
+    }
+}
