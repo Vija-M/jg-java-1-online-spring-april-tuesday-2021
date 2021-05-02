f8bbf115ea201f5b8c99713df412365ba9fc8ff6
OlgaT <ok.latvia@gmail.com>
2021-05-02 21:56:30 +0300

    Добавила Lesson 2 Level 7 Task 31

---
+++ src/main/java/students/olga_t/lesson_2/level_7/task_31/Comments.java
@@ -0,0 +1,18 @@
+package students.olga_t.lesson_2.level_7.task_31;
+
+class Comments {
+
+    public static void main(String[] args) {
+        int a = 2; //однострочный комментарий
+        int b = 3;
+        int sum = a + b;
+        System.out.println(sum);
+/*
+многострочный комментарий
+ */
+
+
+
+    }
+
+}
