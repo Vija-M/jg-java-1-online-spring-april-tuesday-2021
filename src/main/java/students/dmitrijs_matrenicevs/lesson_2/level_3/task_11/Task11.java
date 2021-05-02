32006e3e210a9dde3aea815354fb72ab982f0496
DeTELo <mrdinas@gmail.com>
2021-05-02 20:00:29 +0300

    Task 11 Lesson 2 it's ready

--- src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_3/task_11/Task11.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_2/level_3/task_11/Task11.java
@@ -4,16 +4,20 @@ class Task11 {
     public static void main(String[] args) {
 
         java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("Find out your average score, enter any 3 grades: ");
-
+        System.out.print("First: ");
         int a = scanner.nextInt();
+        System.out.print("Second: ");
         int b = scanner.nextInt();
+        System.out.print("Third: ");
         int c = scanner.nextInt();
         int sum = a + b + c;
-        int result = sum / 3;
+        double result = sum / 3.0;
 
         System.out.println(/*"a + b + c = " + sum*/);
         System.out.println(/*sum + "/" + 3 +*/ "Result: = " + result);
+
+
+
 
     }
 }
