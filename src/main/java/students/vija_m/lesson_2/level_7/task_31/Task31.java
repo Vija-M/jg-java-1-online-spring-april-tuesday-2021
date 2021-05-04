18a06a471f4e0325b0e9e1f4a8dff29cf9473dcd
Vija-M <vija.riga@gmailcom>
2021-05-04 18:24:21 +0300

    Task 31, Lesson 2

--- src/main/java/students/vija_m/lesson_2/level_7/task_31/Task31.java
+++ src/main/java/students/vija_m/lesson_2/level_7/task_31/Task31.java
@@ -5,24 +5,24 @@ class Task31 {
     public static void main(String[] args) {
 
         java.util.Scanner scanner = new java.util.Scanner(System.in);
-
-        System.out.println("Please, enter integer numbers: ");
+        /* Напишите программу в которой нужно вводить с клавиатуры целые числа и считать их сумму,
+        пока пользователь не введёт слово "ENTER".
+        Вывести на экран полученную сумму и завершить программу.
+        В программе необходимо использовать цикл while.
+         */
+                System.out.println("Please, enter integer numbers: ");
         int sum = 0; // так как изначачально цифры не были введены, то их сумма равна 0
-        boolean isExit = false;
-        while (!isExit) {
+        boolean isExit = false; // присваиваем переменной значение false
+        while (!isExit) { //  соответственно пока !isExit - true наш цикл работает
             if (scanner.hasNextInt()) {
                 int number = scanner.nextInt();
-                sum = sum + number;
+                sum = sum + number; // если введённое  численное значение, то оно прибавляется к сумме ранее введённых чисел.
             }
-            /* программа проверяет введено ли число.
-            если введённое значение число,
-     то оно прибавляется к сумме ранее введённых чисел.
-             */
             else if (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-            if (line.equals("ENTER")) { // если введён текст, то программа проверяет не является ли он ключевм словом "ENTER"
-                isExit = true;
+            if (line.equals("ENTER")) { // если введён текст и он является словом ENTER
+                isExit = true; // присваиваем переменной значение true и цикл прерывается
             }}}
-                System.out.print(sum);
+                System.out.println(sum); // программа выводит сумму чисел
 
     }}
