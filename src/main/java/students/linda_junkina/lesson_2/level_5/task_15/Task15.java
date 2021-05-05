984f8a8439cb89dbd02c4eea3bb214910af86174
Linda-Ju <linda.junkina@gmail.com>
2021-05-05 13:07:59 +0300

    Lesson 2, Level 5, Task 15

---
+++ src/main/java/students/linda_junkina/lesson_2/level_5/task_15/Task15.java
@@ -0,0 +1,12 @@
+package students.linda_junkina.lesson_2.level_5.task_15;
+
+class Task15 {
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+
+        System.out.println("Please, enter your name: ");
+        String name = scanner.nextLine();
+    }
+}
