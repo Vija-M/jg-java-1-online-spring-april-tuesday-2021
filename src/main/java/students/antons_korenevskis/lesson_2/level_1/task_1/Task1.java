d81f9a5e3e43009b2dade8905305228e87427cef
neontone <antons.korenevskis@gmail.com>
2021-05-05 18:21:38 +0300

    It works

---
+++ src/main/java/students/antons_korenevskis/lesson_2/level_1/task_1/Task1.java
@@ -0,0 +1,19 @@
+package students.antons_korenevskis.lesson_2.level_1.task_1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("Введи число а");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Введи число b");
+        int secondNumber = scanner.nextInt();
+
+        System.out.println("a + b = " + (firstNumber + secondNumber));
+        System.out.println("a - b = " + (firstNumber - secondNumber));
+        System.out.println("a / b = " + firstNumber / secondNumber);
+        System.out.println("a * b = " + firstNumber * secondNumber);
+
+    }
+}
