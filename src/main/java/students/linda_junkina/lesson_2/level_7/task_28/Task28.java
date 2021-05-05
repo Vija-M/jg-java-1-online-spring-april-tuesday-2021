a9fd834f8ceedba7bdaab64379dedd8761a90b93
Linda-Ju <linda.junkina@gmail.com>
2021-05-05 14:28:36 +0300

    Lesson 2, Level 7, Task 28

---
+++ src/main/java/students/linda_junkina/lesson_2/level_7/task_28/Task28.java
@@ -0,0 +1,22 @@
+package students.linda_junkina.lesson_2.level_7.task_28;
+
+class Task28 {
+
+ //   public class SumTwoNumbers {
+
+        public static void main(String[] args) {
+
+            int numberOne = 10;
+            int numberTwo = 20;
+
+            System.out.println("Number 1 = " + numberOne);
+            System.out.println("Number 2 = " + numberTwo);
+
+
+            int sum = numberOne + numberTwo;
+
+            System.out.println("Sum = " + sum);
+        }
+
+   // }
+}
