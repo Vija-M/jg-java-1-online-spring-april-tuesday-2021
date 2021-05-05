4c71ceb9993a22483bcbb3947694a958dcdf0973
Linda-Ju <linda.junkina@gmail.com>
2021-05-05 13:59:01 +0300

    Lesson 2, Level 6, Task 23

---
+++ src/main/java/students/linda_junkina/lesson_2/level_6/task_23/Task23.java
@@ -0,0 +1,13 @@
+package students.linda_junkina.lesson_2.level_6.task_23;
+
+class Task23 {
+
+   // class AttentionPlease {
+
+
+        public static void main(String[] args) {
+            System.out.println("Code outside the method will not compile!");
+
+        }
+  //  }
+}
