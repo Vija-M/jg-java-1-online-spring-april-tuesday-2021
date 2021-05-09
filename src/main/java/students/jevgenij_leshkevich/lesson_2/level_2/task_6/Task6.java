a6b1999188284d68340133fef94275e55692247f
jleshkev <evgeny.leshkevich@gmail.com>
2021-05-09 20:48:32 +0300

    Completed Task 6 - Lesson 2 - Level 2

---
+++ src/main/java/students/jevgenij_leshkevich/lesson_2/level_2/task_6/Task6.java
@@ -0,0 +1,14 @@
+package students.jevgenij_leshkevich.lesson_2.level_2.task_6;
+
+class Task6 {
+
+    class Turtle {
+
+        public static void main(String[] args) {
+// открыли скобочку в конце линии где public static...
+    }
+
+}
+
+
+}
