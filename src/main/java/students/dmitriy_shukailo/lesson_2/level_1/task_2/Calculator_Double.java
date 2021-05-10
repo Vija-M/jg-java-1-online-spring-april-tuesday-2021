98083d99378acdf2b5fabecbb3995c19e0bd1da1
dk-t <shukailo.d@gmail.com>
2021-05-10 23:18:27 +0300

    Calculator_Double has been done

--- src/main/java/students/dmitriy_shukailo/lesson_2/level_1/task_2/Calculator_Double.java
+++ src/main/java/students/dmitriy_shukailo/lesson_2/level_1/task_2/Calculator_Double.java
@@ -4,6 +4,21 @@ public class Calculator_Double {
 
     public static void main(String[] args) {
 
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        double sum = a + b;
+        System.out.println("sum = " + a + " + " + b + " = " + sum);
+
+        double sub = a - b;
+        System.out.println("sub = " + a + " - " + b + " = " + sub);
+
+        double div = a / b;
+        System.out.println("sub = " + a + " / " + b + " = " + div);
+
+        double mul = a * b;
+        System.out.println("sub = " + a + " * " + b + " = " + mul);
 
     }
 
