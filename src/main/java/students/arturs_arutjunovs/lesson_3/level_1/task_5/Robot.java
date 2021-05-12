2e2f8ea4d80ec6ab4abe4160ccbd9447fa8c0f37
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-12 17:53:55 +0100

    Completed   Lesson 3 Level 1 Task 5

---
+++ src/main/java/students/arturs_arutjunovs/lesson_3/level_1/task_5/Robot.java
@@ -0,0 +1,15 @@
+package students.arturs_arutjunovs.lesson_3.level_1.task_5;
+
+public class Robot {
+
+    public Robot() {
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
---
+++ src/main/java/students/arturs_arutjunovs/lesson_3/level_1/task_5/RobotDemo.java
@@ -0,0 +1,10 @@
+package students.arturs_arutjunovs.lesson_3.level_1.task_5;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robot = new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+    }
+}
