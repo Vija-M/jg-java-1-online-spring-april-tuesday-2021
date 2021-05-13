1934d6d9553cb376f7d0b18ab970aa972fb0c830
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-13 14:50:10 +0100

    Completed   Lesson 3 Level 3 Task 12

---
+++ src/main/java/students/arturs_arutjunovs/lesson_3/level_3/task_12/RobotDemo.java
@@ -0,0 +1,14 @@
+package students.arturs_arutjunovs.lesson_3.level_3.task_12;
+
+class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot("Rider");
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot("John");
+        robot2.sayHello();
+        robot2.sayYourName();
+    }
+}
