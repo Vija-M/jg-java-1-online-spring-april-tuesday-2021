6aa2c41c79617e5493bee031f1534fa9b0d670dd
Linda-Ju <linda.junkina@gmail.com>
2021-05-13 15:42:06 +0300

    Lesson 3, Level 4, Task 13

---
+++ src/main/java/students/linda_junkina/lesson_3/level_4/task_13/Book.java
@@ -0,0 +1,16 @@
+package students.linda_junkina.lesson_3.level_4.task_13;
+
+class Book {
+
+    String title;
+    String newTitle;
+
+    Book(String bookTitle) {
+        this.title = newTitle;
+    }
+
+    String getTitle() {
+        return newTitle;
+    }
+
+}
