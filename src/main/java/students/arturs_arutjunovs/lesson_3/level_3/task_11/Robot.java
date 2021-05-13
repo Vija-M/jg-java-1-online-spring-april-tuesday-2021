0c7286465040464d8fc159b6f9bf1fec99829673
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-13 14:39:39 +0100

    Completed   Lesson 3 Level 3 Task 11

---
+++ src/main/java/students/arturs_arutjunovs/lesson_3/level_3/task_11/Robot.java
@@ -0,0 +1,14 @@
+package students.arturs_arutjunovs.lesson_3.level_3.task_11;
+
+class Robot {
+
+    String name;
+
+    Robot(String robotName) {
+        this.name = robotName;
+    }
+
+    void sayHello() {
+        System.out.println("Hello!");
+    }
+}
