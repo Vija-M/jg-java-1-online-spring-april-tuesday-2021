389bfac073308faad829deda0d9c2b72b922a5eb
Linda-Ju <linda.junkina@gmail.com>
2021-05-13 11:37:18 +0300

    Lesson 3, Level 1, Task 1,2,3,4,5,6

---
+++ src/main/java/students/linda_junkina/lesson_3/level_1/task_1_2_3_4_5_6/Robot.java
@@ -0,0 +1,31 @@
+package students.linda_junkina.lesson_3.level_1.task_1_2_3_4_5_6;
+
+class Robot {
+
+
+    public void sayHello() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Human!");
+
+
+    }
+
+     Robot() {
+    }
+
+    String robot1;
+    String robot2;
+    String robot3;
+    String sayHello;
+
+    String sayYourName;
+
+
+    public void sayYourNAme () {
+        System.out.println("My name is ROBO!");
+    }
+}
+
---
+++ src/main/java/students/linda_junkina/lesson_3/level_1/task_1_2_3_4_5_6/RobotDemo.java
@@ -0,0 +1,30 @@
+package students.linda_junkina.lesson_3.level_1.task_1_2_3_4_5_6;
+
+class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot ();
+        robot1.sayHello = "Hello Human!";
+        System.out.println(robot1.sayHello);
+        robot1.sayYourName = "My name is ROBO!";
+        System.out.println(robot1.sayYourName);
+
+        System.out.println ();
+
+        Robot robot2 = new Robot ();
+        robot2.sayHello = "Hello Human!";
+        System.out.println(robot2.sayHello);
+        robot2.sayYourName = "My name is WALLE!";
+        System.out.println(robot2.sayYourName);
+
+        System.out.println ();
+
+        Robot robot3 = new Robot ();
+        robot3.sayHello = "Hello Human!";
+        System.out.println(robot3.sayHello);
+        robot3.sayYourName = "My name is EVA!";
+        System.out.println(robot3.sayYourName);
+
+    }
+
+}
