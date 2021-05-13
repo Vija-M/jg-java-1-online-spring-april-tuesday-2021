0d57ce898b51dcd293bf32ad51850d986a81eccb
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-13 04:06:24 +0100

    Completed   Lesson 3 Level 2 Task 8

---
+++ src/main/java/students/arturs_arutjunovs/lesson_3/level_2/task_8/Task_8.java
@@ -0,0 +1,9 @@
+package students.arturs_arutjunovs.lesson_3.level_2.task_8;
+
+class Task_8 {
+    public static void main(String[] args) {
+
+        String userName = "Artur";
+        System.out.println("Hi " + userName + "!");
+    }
+}
