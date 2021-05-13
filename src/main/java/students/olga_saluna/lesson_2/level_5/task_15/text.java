c6f8d373a97313a25942072f69024ea3103d910d
salun <salun.alex@gmail.com>
2021-05-13 21:04:46 +0300

    Changes in Task 15 Lesson 2 Level 5

--- src/main/java/students/olga_saluna/lesson_2/level_5/task_15/text.java
+++ src/main/java/students/olga_saluna/lesson_2/level_5/task_15/text.java
@@ -2,6 +2,10 @@ package students.olga_saluna.lesson_2.level_5.task_15;
 
 class text {
     public static void main(String[] args){
-        System.out.print("text");
+
+        System.out.println("text using println ");
+        System.out.print("and text using print ");
+        System.out.println("and now again text using println ");
+        System.out.println("and now again text using println ");
     }
 }
