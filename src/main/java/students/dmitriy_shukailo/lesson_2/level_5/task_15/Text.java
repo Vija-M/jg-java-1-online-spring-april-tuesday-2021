f168dc1f7fc0d4d6df73a13621b7c48d025a7ec3
dk-t <shukailo.d@gmail.com>
2021-05-13 22:30:59 +0300

    Text has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_2/level_5/task_15/Text.java
@@ -0,0 +1,13 @@
+package students.dmitriy_shukailo.lesson_2.level_5.task_15;
+
+class Text {
+
+    public static void main(String[] args) {
+
+        System.out.println("Print text on a new line: ");
+
+        System.out.print("Print text this line: ");
+
+    }
+
+}
