33705fe4c486a90f6453e5f0c3a8a01502d68d1f
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-13 04:10:59 +0100

    Completed   Lesson 3 Level 2 Task 9

---
+++ src/main/java/students/arturs_arutjunovs/lesson_3/level_2/task_9/Task_9.java
@@ -0,0 +1,15 @@
+package students.arturs_arutjunovs.lesson_3.level_2.task_9;
+
+import java.util.Scanner;
+
+class Task_9 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name + "!");
+    }
+}
