e09b0d407b7971e414d040c0fc242ccdcc3a37f5
dk-t <shukailo.d@gmail.com>
2021-05-14 22:32:22 +0300

    RobotDemo has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_1/task_3/Robot.java
@@ -0,0 +1,13 @@
+package students.dmitriy_shukailo.lesson_3.level_1.task_3;
+
+class Robot {
+
+    public Robot(){
+
+    }
+
+    public void sayHello() {
+
+        System.out.println("Hello!");
+    }
+}
