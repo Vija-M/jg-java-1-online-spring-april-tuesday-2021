56d82afbda38d4712a1c53fe8d58c4618d372343
dk-t <shukailo.d@gmail.com>
2021-05-14 23:18:46 +0300

    userName has been created

---
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_2/task_7/StringTest.java
@@ -0,0 +1,10 @@
+package students.dmitriy_shukailo.lesson_3.level_2.task_7;
+
+class StringTest {
+
+    public static void main(String[] args) {
+
+        String myName ="Dmitriy";
+        System.out.println(myName);
+    }
+}
