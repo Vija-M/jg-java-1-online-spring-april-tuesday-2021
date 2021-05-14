8a765d9f04c86e32eb328f8a09b348927f39d71c
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-14 19:01:42 +0100

    Completed   Lesson 3 Level 5 Task 21

---
+++ src/main/java/students/arturs_arutjunovs/lesson_3/level_5/task_21/Dog.java
@@ -0,0 +1,39 @@
+package students.arturs_arutjunovs.lesson_3.level_5.task_21;
+
+class Dog {
+    String color;
+    String name;
+    int age;
+
+    public Dog(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    void happyBirthday() {
+        this.age = age + 1;
+        System.out.println("Прошел год и теперь ему " + this.age);
+    }
+
+    void changeColor(String newColor) {
+        System.out.println("Цвет поменялся на " + newColor);
+    }
+
+    void voice() {
+        System.out.println("Кличка собаки - " + this.name);
+        System.out.println("Его возраст - " + this.age + " года");
+        System.out.println("Щенок имеет " + this.color + " цвет");
+
+    }
+}
+
+class DogDemo {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Малыш", 2, "черный");
+        dog.voice();
+        dog.happyBirthday();
+        dog.changeColor("рыжий");
+    }
+}
