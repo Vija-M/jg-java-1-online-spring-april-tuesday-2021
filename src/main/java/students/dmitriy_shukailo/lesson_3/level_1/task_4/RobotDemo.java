0eed34beee6273fc649eb0631fdcdff51756f8c2
dk-t <shukailo.d@gmail.com>
2021-05-14 22:39:49 +0300

    "Hello!" has been printed RobotDemo has been fixed

--- src/main/java/students/dmitriy_shukailo/lesson_3/level_1/task_4/RobotDemo.java
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_1/task_4/RobotDemo.java
@@ -1,9 +1,10 @@
-package students.dmitriy_shukailo.lesson_3.level_1.task_3;
+package students.dmitriy_shukailo.lesson_3.level_1.task_4;
 
 class RobotDemo {
 
     public static void main(String[] args) {
 
         Robot robot = new Robot();
+        robot.sayHello();
     }
 }
