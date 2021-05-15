2ea65d296fd5f1a026ecea6b41e7add16d0cea09
Vija-M <vija.riga@gmailcom>
2021-05-15 23:43:06 +0300

    Lesson 3, Level 3, Task 11, 12

--- src/main/java/students/vija_m/lesson_3/level_3/task_11_12/Robot/RobotDemo.java
+++ src/main/java/students/vija_m/lesson_3/level_3/task_11_12/Robot/RobotDemo.java
@@ -1,14 +1,16 @@
 package students.vija_m.lesson_3.level_3.task_11_12.Robot;
 
+class RobotDemo {
+
+    public static void main(String[] args) {
+        Robot robot1 = new Robot("Rider");
+        robot1.sayHello();
+        robot1.sayYourName();
 
 
-public class RobotDemo {
+        Robot robot2 = new Robot("John");
+        robot2.sayHello();
+        robot2.sayYourName();
 
-
-
+    }
 }
-
-
-
-
-
