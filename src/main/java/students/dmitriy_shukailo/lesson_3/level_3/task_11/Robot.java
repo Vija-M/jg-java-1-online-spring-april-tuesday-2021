8ae377de2455f35ae776022899bb21fe3bd0ea15
dk-t <shukailo.d@gmail.com>
2021-05-16 19:24:58 +0300

    Constructor with parameter String name has been added in class Robot
    Task 10 has been corrected

---
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_3/task_11/Robot.java
@@ -0,0 +1,18 @@
+package students.dmitriy_shukailo.lesson_3.level_3.task_11;
+
+class Robot {
+
+    String name;
+    Robot(String robotName) {
+        this.name = robotName;
+    }
+
+    public void sayHello(){
+
+    }
+
+    public void sayYourName(){
+
+    }
+
+}
