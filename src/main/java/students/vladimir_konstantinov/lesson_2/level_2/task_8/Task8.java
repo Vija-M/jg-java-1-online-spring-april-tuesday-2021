fcc4bed6dc316a9ad1824a0f925f13f633b764d1
VladimirK-Java1 <vladimir.a.konstantinov@gmail.com>
2021-05-17 00:03:32 +0300

    Completed Task 8 Level 2 Lesson 2

---
+++ src/main/java/students/vladimir_konstantinov/lesson_2/level_2/task_8/Task8.java
@@ -0,0 +1,18 @@
+package students.vladimir_konstantinov.lesson_2.level_2.task_8;
+
+class Task8 {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String userName;
+
+        System.out.println("Please enter your name!");
+        userName = scanner.nextLine();
+
+        System.out.println("Hello, " + userName + "! ");
+
+    }
+
+}
+
