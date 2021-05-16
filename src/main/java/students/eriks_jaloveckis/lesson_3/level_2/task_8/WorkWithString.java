6268739877656e1d16aabdeec1d2dbf48fb77228
eriks <erikslv88@gmail.com>
2021-05-16 15:29:41 +0300

    Finish - 3 lesson 2 level 8 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_2/task_8/WorkWithString.java
@@ -0,0 +1,10 @@
+package students.eriks_jaloveckis.lesson_3.level_2.task_8;
+
+class WorkWithString {
+    public static void main(String[] args) {
+        String myName = "Eriks";
+        String greeting = "Hi " + myName + "!";
+        System.out.println(greeting);
+    }
+
+}
