8f6f78610b3c81334f51173baee3cfabbb4714c1
DeTELo <mrdinas@gmail.com>
2021-05-16 16:00:17 +0300

    Lesson 3 Task-1-6 - LEVEL1 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/DmRobot.java
@@ -0,0 +1,21 @@
+package students.dmitrijs_matrenicevs.lesson_3;
+
+class Robot {
+
+
+    public void sayHello() {
+        System.out.println("Hello ");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+
+    }
+
+
+    public Robot() {
+
+    }
+
+}
+
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/DmRobotDemo.java
@@ -0,0 +1,23 @@
+package students.dmitrijs_matrenicevs.lesson_3;
+
+class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot();
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
+
+        Robot robot3 = new Robot();
+        robot3.sayHello();
+        robot3.sayYourName();
+
+
+    }
+
+
+}
