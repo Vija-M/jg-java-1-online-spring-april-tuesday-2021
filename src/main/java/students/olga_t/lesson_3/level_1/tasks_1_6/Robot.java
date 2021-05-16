b2002a0bac656780c6c8c1edbd5d80cfc95298c7
OlgaT <ok.latvia@gmail.com>
2021-05-16 17:14:17 +0300

    Добавила Lesson 3 Level 1 Tasks 1-6

---
+++ src/main/java/students/olga_t/lesson_3/level_1/tasks_1_6/Robot.java
@@ -0,0 +1,16 @@
+package students.olga_t.lesson_3.level_1.tasks_1_6;
+
+class Robot {
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public Robot() {
+
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+
+}
