a7639c5a59c7932ae1c520b840581cb2f0181e20
DeTELo <mrdinas@gmail.com>
2021-05-16 18:47:30 +0300

    Lesson 3 Task-18 - LEVEL5 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,17 @@
+package students.dmitrijs_matrenicevs.lesson_3.level_5.task_18;
+
+class DogDemo {
+
+
+    public static void main(String[] args) {
+
+        Dog dog2 = new Dog ("Бигель", 5);
+
+        dog2.voice();
+        dog2.voice();
+        dog2.voice();
+
+    }
+
+
+}
