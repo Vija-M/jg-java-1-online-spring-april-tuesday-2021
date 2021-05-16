bb457315538803fb693df9c207ee2830da3aee4d
Ernests1992 <ernests@neolain.lv>
2021-05-16 20:57:22 +0300

    Lesson 3 - Level 3

--- src/main/java/students/ernests_subhankulovs/lesson_1/level_4/task_10/Task10.java
+++
@@ -1,7 +0,0 @@
-package students.ernests_subhankulovs.lesson_1.level_4.task_10;
-
-class Task10 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
