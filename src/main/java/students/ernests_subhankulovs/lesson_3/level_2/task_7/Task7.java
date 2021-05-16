61448f2d81ee2004477b924d51a70d7e1330eced
Ernests1992 <ernests@neolain.lv>
2021-05-16 20:09:43 +0300

    Lesson 3 - Level 2 - Task 7

--- src/main/java/students/ernests_subhankulovs/lesson_3/level_2/task_7/Task7.java
+++ src/main/java/students/ernests_subhankulovs/lesson_3/level_2/task_7/Task7.java
@@ -1,4 +1,4 @@
-package students.ernests_subhankulovs.lesson_3.level_2;
+package students.ernests_subhankulovs.lesson_3.level_2.task_7;
 
 public class Task7 {
     public static void main(String[] args) {
