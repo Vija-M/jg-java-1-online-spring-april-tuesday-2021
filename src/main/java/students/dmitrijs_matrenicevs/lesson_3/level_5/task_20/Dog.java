efdc0762cd3e7846db3c8ac9a6a81c548a7590c3
DeTELo <mrdinas@gmail.com>
2021-05-16 19:31:46 +0300

    Lesson 3 Task-20 - LEVEL5 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/level_5/task_20/Dog.java
@@ -0,0 +1,32 @@
+package students.dmitrijs_matrenicevs.lesson_3.level_5.task_20;
+
+class Dog {
+
+    String name;
+    int age;
+
+    public Dog(String name, int age) {
+
+        this.name = name;
+        this.age = age;
+
+    }
+
+
+    void voice() {
+
+        System.out.println("Её имя: " + this.name);
+        System.out.println("Её возраст: " + this.age + " лет");
+    }
+
+    void happyBirthday() {
+
+        this.age = age + 1;
+
+        System.out.println("И вот днюха настала и теперь тебе " + this.age + " лет");
+
+    }
+
+
+
+}
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/level_5/task_20/DogDemo.java
@@ -0,0 +1,15 @@
+package students.dmitrijs_matrenicevs.lesson_3.level_5.task_20;
+
+class DogDemo {
+
+
+    public static void main(String[] args) {
+
+        Dog dog2 = new Dog("Бигель", 5);
+
+        dog2.voice();
+        dog2.happyBirthday();
+    }
+
+
+}
