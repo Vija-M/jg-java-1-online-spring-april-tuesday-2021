3be3fed502d47844db54a691944fe5fa14db45cd
eriks <erikslv88@gmail.com>
2021-05-16 15:51:00 +0300

    Finish - 3 lesson 2 level 9 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_2/task_9/WorkWithString.java
@@ -0,0 +1,16 @@
+package students.eriks_jaloveckis.lesson_3.level_2.task_9;
+import java.util.Scanner;
+
+
+class WorkWithString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите свое имя: ");
+        String yourName = scanner.nextLine();
+        System.out.println("Hello " +yourName+ "!");
+
+
+
+    }
+
+}
