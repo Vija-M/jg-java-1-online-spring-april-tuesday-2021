c296f24384bf89c21d725be21f91afaa74662e3c
eriks <erikslv88@gmail.com>
2021-05-16 17:54:13 +0300

    Finish - 3 lesson 4 level 13 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_4/task_13/Fix.java
@@ -0,0 +1,31 @@
+package students.eriks_jaloveckis.lesson_3.level_4.task_13;
+
+class Fix {
+
+    /*
+    class Book {
+
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
+
+class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+
+    }
+
+}
+     */
+}
