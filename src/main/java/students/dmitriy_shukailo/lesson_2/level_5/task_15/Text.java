abd23bd51f164c580fe06dda01d7d182febe2951
dk-t <shukailo.d@gmail.com>
2021-05-16 22:29:15 +0300

    Text has been fixed

--- src/main/java/students/dmitriy_shukailo/lesson_2/level_5/task_15/Text.java
+++ src/main/java/students/dmitriy_shukailo/lesson_2/level_5/task_15/Text.java
@@ -4,10 +4,9 @@ class Text {
 
     public static void main(String[] args) {
 
-        System.out.println("Print text on a new line: ");
-
         System.out.print("Print text this line: ");
 
+        System.out.println("PrintLn text on a new line: ");
     }
 
 }
