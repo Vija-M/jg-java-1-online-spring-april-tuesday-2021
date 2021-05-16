3bd5631b0e6b1f2ec169606278bd6dacd25e0219
eriks <erikslv88@gmail.com>
2021-05-16 15:03:35 +0300

    Finish - 3 lesson 1 level 5 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_1/task_5/Robot.java
@@ -0,0 +1,14 @@
+package students.eriks_jaloveckis.lesson_3.level_1.task_5;
+
+class Robot {
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+
+    public Robot() {
+
+    }
+}
