8e1699ef77d036396883f4a4893cef34903c2ca9
dk-t <shukailo.d@gmail.com>
2021-05-16 19:05:55 +0300

    Property name has been added in class Robot

---
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_3/task_10/Robot.java
@@ -0,0 +1,19 @@
+package students.dmitriy_shukailo.lesson_3.level_3.task_10;
+
+class Robot {
+
+    String name;
+
+    public Robot(){
+
+    }
+
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName(){
+        System.out.println("My name is ROBO");
+    }
+
+}
