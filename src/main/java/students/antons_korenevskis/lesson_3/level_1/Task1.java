598bb32f63d7b6c1164234ffe06c580c0bcaeaed
neontone <antons.korenevskis@gmail.com>
2021-05-16 11:39:29 +0300

    Done

--- src/main/java/students/antons_korenevskis/lesson_3/level_1/Task1.java
+++ src/main/java/students/antons_korenevskis/lesson_3/level_1/Task1.java
@@ -1,10 +1,22 @@
 package students.antons_korenevskis.lesson_3.level_1;
 
 
-class Robot {
+class Robot{
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
 
     public static void main(String[] args) {
-            System.out.println("Hello!");
-        }
+
     }
+
+}
 
---
+++ src/main/java/students/antons_korenevskis/lesson_3/level_2/Task7.java
@@ -0,0 +1,16 @@
+package students.antons_korenevskis.lesson_3.level_2;
+
+import java.util.Scanner;
+
+public class Task7 {
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String myName = scanner.nextLine();
+        System.out.println("Hi " + myName + "!");
+    }
+
+}
