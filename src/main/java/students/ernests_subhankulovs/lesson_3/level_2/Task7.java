946b06bb6611734dc50e4f461876413012100632
Ernests1992 <ernests@neolain.lv>
2021-05-16 19:56:44 +0300

    Lesson 3 - Level 2 - Task 7

---
+++ src/main/java/students/ernests_subhankulovs/lesson_3/level_2/Task7.java
@@ -0,0 +1,8 @@
+package students.ernests_subhankulovs.lesson_3.level_2;
+
+public class Task7 {
+    public static void main(String[] args) {
+        String myName = "Ernests";
+        System.out.println(myName);
+    }
+}
