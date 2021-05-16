ac5ff5d2e73ded9152771d141673ada302616ac0
DeTELo <mrdinas@gmail.com>
2021-05-16 18:25:31 +0300

    Lesson 3 Task-17 - LEVEL5 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/level_5/task_17_21/Dog.java
@@ -0,0 +1,21 @@
+package students.dmitrijs_matrenicevs.lesson_3.level_5.task_17_21;
+
+class Dog {
+
+String name;
+
+public Dog(String name) {
+
+    this.name = name;
+    System.out.println(name);
+
+}
+
+    void voice() {
+
+        System.out.println(this.name);
+
+    }
+
+
+}
