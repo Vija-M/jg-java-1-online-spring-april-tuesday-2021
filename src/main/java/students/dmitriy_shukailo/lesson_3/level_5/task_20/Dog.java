00ffa29dde3f70e502d77b8bc51316ca24428309
dk-t <shukailo.d@gmail.com>
2021-05-17 21:42:25 +0300

    Variable dogColor has been added

---
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_5/task_20/Dog.java
@@ -0,0 +1,12 @@
+package students.dmitriy_shukailo.lesson_3.level_5.task_20;
+
+class Dog {
+
+    String dogName = "Bobik";
+    int dogAge = 3;
+    String dogColor = "Orange";
+
+    void voice(){
+        System.out.println(dogName + "! " + "I am " + dogAge + " and I am " + dogColor);
+    }
+}
