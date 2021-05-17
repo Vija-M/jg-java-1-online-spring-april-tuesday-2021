469af95b7fb02e1dc1e5ce44cab5bbe122ee4365
dk-t <shukailo.d@gmail.com>
2021-05-17 22:52:25 +0300

    Task 20 has been fixed

--- src/main/java/students/dmitriy_shukailo/lesson_3/level_5/task_20/Dog.java
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_5/task_20/Dog.java
@@ -2,11 +2,21 @@ package students.dmitriy_shukailo.lesson_3.level_5.task_20;
 
 class Dog {
 
-    String dogName = "Bobik";
-    int dogAge = 3;
-    String dogColor = "Orange";
+    String dogName;
+    int dogAge;
+    String dogColor;
 
-    void voice(){
+    Dog (String dogName, int dogAge, String dogColor){
+        this.dogName = dogName;
+        this.dogAge = dogAge;
+        this.dogColor = dogColor;
+    }
+
+    public void voice(){
         System.out.println(dogName + "! " + "I am " + dogAge + " and I am " + dogColor);
+    }
+
+    void happyBirthday(){
+        dogAge = dogAge + 1;
     }
 }
