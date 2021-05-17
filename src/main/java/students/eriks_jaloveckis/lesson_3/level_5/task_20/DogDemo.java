e1c0e07e939cd2450cdf5c0d783f5c8966a59837
eriks <erikslv88@gmail.com>
2021-05-17 22:12:09 +0300

    Finish - 3 lesson 5 level 19 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_5/task_20/DogDemo.java
@@ -0,0 +1,9 @@
+package students.eriks_jaloveckis.lesson_3.level_5.task_20;
+
+class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Kuzja", 4, "Black");
+
+        dog.voice();
+    }
+}
