93f76fbb236102d1d5d3b2ef821bfd6e2afcb242
salun <salun.alex@gmail.com>
2021-05-17 19:55:52 +0300

    Task 8 Lesson 3 Level 2

---
+++ src/main/java/students/olga_saluna/lesson_3/level_2/task_8/YourName.java
@@ -0,0 +1,9 @@
+package students.olga_saluna.lesson_3.level_2.task_8;
+
+class YourName {
+    public static void main(String[] args) {
+        String userName = "Olga";
+        String greeting = "Hi " + userName + "!";
+        System.out.println(greeting);
+    }
+}
