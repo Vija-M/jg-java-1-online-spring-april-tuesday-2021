77d0fdbf782e0c5339f0bcc758208c2dc53bc1de
AzrailXD <whiteus@inbox.lv>
2021-05-18 02:14:42 +0300

    Lesson 2 - Level 3 - Task 10

--- src/main/java/students/vladimir_aboltins/lesson_2/level_3/task_10/CountPerimeterAndArea.java
+++ src/main/java/students/vladimir_aboltins/lesson_2/level_3/task_10/CountPerimeterAndArea.java
@@ -1,9 +1,13 @@
 package students.vladimir_aboltins.lesson_2.level_3.task_10;
 
 import java.util.Scanner;
+
 import static java.lang.Math.*;
+
 class CountPerimeterAndArea {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter circle radius: ");
         if (scanner.hasNextFloat()) {
