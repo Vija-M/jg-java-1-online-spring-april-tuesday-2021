ea6f987ab0c2f44e83fb7ef3d889abe7ea65d43a
eriks <erikslv88@gmail.com>
2021-05-17 21:28:10 +0300

    Finish - 3 lesson 5 level 17 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_5/task_17/Dog.java
@@ -0,0 +1,18 @@
+package students.eriks_jaloveckis.lesson_3.level_5.task_17;
+
+class Dog {
+ String name;
+ String voice;
+
+    Dog(String name) {
+        this.name = name;
+    }
+
+    void voice(){
+        System.out.println(name);
+
+    }
+
+}
+
+
---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_5/task_17/DogDemo.java
@@ -0,0 +1,16 @@
+package students.eriks_jaloveckis.lesson_3.level_5.task_17;
+
+class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Kuzja");
+
+    dog.voice();
+    dog.voice();
+    dog.voice();
+    }
+
+
+
+}
+
+
