7126b9bfa6a83bd9f6208a27cb819e7827074c95
salun <salun.alex@gmail.com>
2021-05-17 20:57:58 +0300

    Task 14 Lesson 3 Level 4

---
+++ src/main/java/students/olga_saluna/lesson_3/level_4/task_14/Phone.java
@@ -0,0 +1,13 @@
+package students.olga_saluna.lesson_3.level_4.task_14;
+
+class Phone {
+    String model;
+
+        Phone(String phoneModel) {
+            this.model = phoneModel;
+        }
+
+        String getModel() {
+            return this.model;
+        }
+}
