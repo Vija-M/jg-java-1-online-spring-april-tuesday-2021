1459b4d4df4496a66e61d257521bb106d1591824
salun <salun.alex@gmail.com>
2021-05-17 19:42:34 +0300

    Task 1-6 Lesson 3 Level 1

---
+++ src/main/java/students/olga_saluna/lesson_3/level_1/task1_6/Robot.java
@@ -0,0 +1,17 @@
+package students.olga_saluna.lesson_3.level_1.task1_6;
+
+class Robot {
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public Robot() {
+
+    }
+
+    public void sayYourName(){
+        System.out.println("My name is ROBO");
+    }
+}
+
