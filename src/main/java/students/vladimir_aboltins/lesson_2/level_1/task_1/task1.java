591cfca2cbcdb20bf6d718ce7808c40ca0edb852
AzrailXD <whiteus@inbox.lv>
2021-05-17 19:01:17 +0300

    Доделал

--- src/main/java/students/vladimir_aboltins/lesson_2/level_1/task_1/task1.java
+++ src/main/java/students/vladimir_aboltins/lesson_2/level_1/task_1/task1.java
@@ -14,15 +14,15 @@ class task1 {
         printGreetings();
 
         printAskFirstNumber();
-        int firstNumber = getNumFromUser(4);// Ввод первого числа
-        System.out.print("firstNumber");
+        int firstNumber = getNumFromUser();// Ввод первого числа
+        System.out.print(firstNumber);
 
-                printAskSecondNumber();
-        int secondNumber = getNumFromUser(4);// Ввод второго числа
-        System.out.print("secondNumber");
+        printAskSecondNumber();
+        int secondNumber = getNumFromUser();// Ввод второго числа
+        System.out.print(secondNumber);
 
         printAskOperation();
-        char operationSymbol = getOperationSymbolFromUser(4);// Ввод символа операции
+        char operationSymbol = getOperationSymbolFromUser();// Ввод символа операции
 
         switch (operationSymbol) {
             case '+':
@@ -79,67 +79,75 @@ class task1 {
                 "If you wish to see result of all operations, press | " + ANSI_RED + "a" + ANSI_RESET + " | symbol and press enter: ");
     }
 
-    // вывод error
+    // вывод error: неправильный ввод
     public static void printEnteredNumberError() {
         System.out.print(ANSI_RED + "ERROR" + ANSI_RESET + " number you entered is incorrect\n");
     }
 
+    // вывод error: достигнут лимит попыток
     public static void printErrorExit() {
-        System.out.print(ANSI_RED + "ERROR" + ANSI_RESET + ", You have exceeded the maximum attempts. Please restart program\n");
+        System.out.print(ANSI_RED + "ERROR" + ANSI_RESET + " You have exceeded the maximum attempts. Please restart program\n");
     }
 
+    // вывод error: остаток попыток для чисел
     public static void printRemainingAttempts(int counter) {
         System.out.print("You have left " + ANSI_RED + counter + ANSI_RESET + " attempts before the program closes\n" +
                 "Enter your number: ");
     }
 
+    // вывод error: остаток попыток для символа
     public static void printRemainingAttemptsSymbol(int counter) {
         System.out.print("You have left " + ANSI_RED + counter + ANSI_RESET + " attempts before the program closes\n" +
                 "Enter your symbol: ");
     }
 
-    // Input
-    public static int getNumFromUser(int counter) {
+// Input
+    public static int getNumFromUser() {
 
-        Scanner scanner = new Scanner(System.in);
-        int userInput=scanner.nextInt();
+        int counter = 4;
 
-        if ((userInput%1)==0) { // проверяет число на целочисленость
-          //  System.out.print("Block A");
-            return  userInput;
-        } if (counter == 0) {
-            //System.out.print("Block B");
-            System.exit(0);
-        } if (counter == 1) {
-           // System.out.print("Block C");
-            printErrorExit();
-            getNumFromUser(counter-1);// рекурсия
-        }
-        // System.out.print("Block D");
-        printEnteredNumberError();
-        printRemainingAttempts(counter-1);
-        getNumFromUser(counter-1);// рекурсия
+        do {
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNextInt()) { // проверяет число на целочисленость
+                int userInput = scanner.nextInt();
+                return userInput;
+            }
+            if (counter == 0) {
+                System.exit(0);
+            }
+            if (counter == 1) {
+                printErrorExit();
+            } else if (counter > 1) {
+                printEnteredNumberError();
+                printRemainingAttempts(counter - 1);
+            }
+            counter--;
+        } while (counter >= 1);
+        System.exit(0);
         return 0;
     }
 
-    public static char getOperationSymbolFromUser(int counter) {
+    public static char getOperationSymbolFromUser() {
 
-        if (counter == 0)
-            System.exit(0);
-        Scanner scanner = new Scanner(System.in);
-        char userInputOperation = scanner.next().charAt(0);
-        if (userInputOperation == '+' || userInputOperation == '-' || userInputOperation == '*' || userInputOperation == '/' || userInputOperation == 'a') {
-            return userInputOperation;
-        } else if (counter == 1) {
-            printErrorExit();
-            getOperationSymbolFromUser(counter-1);// рекурсия
-        } else {
-            printEnteredNumberError();
-            printRemainingAttemptsSymbol(counter-1);
-            getOperationSymbolFromUser(counter-1);// рекурсия
-        }
-        return userInputOperation;
+        int counter = 4;
 
+        do {
+            if (counter == 0)
+                System.exit(0);
+            Scanner scanner = new Scanner(System.in);
+            char userInputOperation = scanner.next().charAt(0);
+            if (userInputOperation == '+' || userInputOperation == '-' || userInputOperation == '*' || userInputOperation == '/' || userInputOperation == 'a') {
+                return userInputOperation;
+            } else if (counter == 1) {
+                printErrorExit();
+            } else if (counter > 1) {
+                printEnteredNumberError();
+                printRemainingAttemptsSymbol(counter - 1);
+            }
+            counter--;
+        } while (counter >= 1);
+        System.exit(0);
+        return 0;
     }
 
 }
