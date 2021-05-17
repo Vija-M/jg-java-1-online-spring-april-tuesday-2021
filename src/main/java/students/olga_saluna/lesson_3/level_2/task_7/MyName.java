f39e1173708cea81d965251609ecd0fd118b8185
salun <salun.alex@gmail.com>
2021-05-17 19:49:58 +0300

    Task 7 Lesson 3 Level 2

---
+++ src/main/java/students/olga_saluna/lesson_3/level_2/task_7/MyName.java
@@ -0,0 +1,8 @@
+package students.olga_saluna.lesson_3.level_2.task_7;
+
+class MyName {
+    public static void main(String[] args) {
+        String myName = "Olga";
+        System.out.println(myName);
+    }
+}
