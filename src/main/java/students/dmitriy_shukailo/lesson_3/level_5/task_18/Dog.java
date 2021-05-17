050b315bcc19a68b855a03908c8af38a580d459e
dk-t <shukailo.d@gmail.com>
2021-05-17 20:46:48 +0300

    Variable dogAge has been added

---
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_5/task_18/Dog.java
@@ -0,0 +1,13 @@
+package students.dmitriy_shukailo.lesson_3.level_5.task_18;
+
+class Dog {
+
+    String dogName = "Bobik";
+    int dogAge = 3;
+
+    public void voice(){
+        System.out.println(dogName + "! " + "I am " + dogAge);
+        System.out.println(dogName + "! " + "I am " + dogAge);
+        System.out.println(dogName + "! " + "I am " + dogAge);
+    }
+}
---
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,10 @@
+package students.dmitriy_shukailo.lesson_3.level_5.task_18;
+
+class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.voice();
+    }
+}
