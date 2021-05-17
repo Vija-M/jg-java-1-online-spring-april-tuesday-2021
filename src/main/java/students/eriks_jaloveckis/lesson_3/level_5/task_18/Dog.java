6757d1bd327dac1eb2f0b37d6f424bc5aaae6e4b
eriks <erikslv88@gmail.com>
2021-05-17 21:35:33 +0300

    Finish - 3 lesson 5 level 18 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_5/task_18/Dog.java
@@ -0,0 +1,18 @@
+package students.eriks_jaloveckis.lesson_3.level_5.task_18;
+
+class Dog {
+    String name;
+    int age;
+
+    Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void voice(){
+        System.out.println(name);
+        System.out.println(age);
+
+    }
+
+}
---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,10 @@
+package students.eriks_jaloveckis.lesson_3.level_5.task_18;
+
+class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Kuzja", 2);
+
+        dog.voice();
+
+    }
+}
