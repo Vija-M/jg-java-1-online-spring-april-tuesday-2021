bf81f36ea07e87ffcf117140c0e07f5b1fd51788
Vija-M <vija.riga@gmailcom>
2021-05-17 22:49:19 +0300

    Lesson 3, Level 4, Task 13

---
+++ src/main/java/students/vija_m/lesson_3/level_4/task_14/Phone.java
@@ -0,0 +1,15 @@
+package students.vija_m.lesson_3.level_4.task_14;
+
+class Phone {
+
+    String model;
+
+     Phone(String phonemodel) {
+        this.model = phonemodel;
+    }
+
+        String getModel() {
+            return this.model;
+        }
+
+    }
