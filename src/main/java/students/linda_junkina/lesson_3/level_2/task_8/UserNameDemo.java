d55fa96f58f3de9dbbcf4d026298365f1f16595f
Linda-Ju <linda.junkina@gmail.com>
2021-05-17 21:40:28 +0300

    Lesson 3, Level 2, Task 8

--- src/main/java/students/linda_junkina/lesson_3/level_2/task_8/UserNameDemo.java
+++ src/main/java/students/linda_junkina/lesson_3/level_2/task_8/UserNameDemo.java
@@ -2,14 +2,12 @@ package students.linda_junkina.lesson_3.level_2.task_8;
 
 class UserNameDemo {
 
+
     public static void main(String[] args) {
 
-        UserName userName = new UserName();
-        userName.userName = "Michael";
+        String userName = "Ben";
+        String greeting = "Hello";
 
-        UserName greeting = new UserName();
-        greeting.greeting = "Hi " + userName.userName + "!";
-
-        System.out.print(greeting.greeting);
+        System.out.print(greeting + " " + userName + "!");
     }
 }
