88c6bbd1ec00b73b6e0aafad378d147b4861592c
salun <salun.alex@gmail.com>
2021-05-17 20:48:46 +0300

    Task 14 Lesson 3 Level 4

---
+++ src/main/java/students/olga_saluna/lesson_3/level_4/task_13/Book.java
@@ -0,0 +1,15 @@
+package students.olga_saluna.lesson_3.level_4.task_13;
+
+class Book {
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
+
