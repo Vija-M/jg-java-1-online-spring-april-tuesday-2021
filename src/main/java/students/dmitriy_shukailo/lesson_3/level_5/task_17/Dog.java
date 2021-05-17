099063ee9e4e74f206e015b9697e8e6107da4b34
dk-t <shukailo.d@gmail.com>
2021-05-17 20:41:04 +0300

    Dog and DogDemo has been created Dog has been named

---
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_5/task_17/Dog.java
@@ -0,0 +1,12 @@
+package students.dmitriy_shukailo.lesson_3.level_5.task_17;
+
+class Dog {
+
+    String dogName = "Bobik";
+
+    public void voice(){
+        System.out.println(dogName + "! ");
+        System.out.println(dogName + "! ");
+        System.out.println(dogName + "! ");
+    }
+}
