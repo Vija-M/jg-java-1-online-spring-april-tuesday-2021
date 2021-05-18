b9c8f374df085688612cbaa05d59be0c2f4ec356
DeTELo <mrdinas@gmail.com>
2021-05-18 19:53:27 +0300

    Lesson 3 Task-21 - LEVEL5 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/level_5/task_21/DogDemo.java
@@ -0,0 +1,20 @@
+package students.dmitrijs_matrenicevs.lesson_3.level_5.task_21;
+
+import java.util.Scanner;
+
+class DogDemo {
+
+
+    public static void main(String[] args) {
+
+
+        Dog dog2 = new Dog("Бигель",5, "Коричневый");
+
+        dog2.voice();
+        dog2.happyBirthday();
+        dog2.changeColor("Фиолетовый");
+        dog2.voice();
+    }
+
+
+}
