23dd1a0b54d3b03a2763385d5e458bb2a70640e7
AzrailXD <whiteus@inbox.lv>
2021-05-18 23:09:21 +0300

    Lesson 2 - Level 5 - Task 15 (добавил второй вызов)

--- src/main/java/students/vladimir_aboltins/lesson_2/level_5/task_15/PrintOutLine.java
+++ src/main/java/students/vladimir_aboltins/lesson_2/level_5/task_15/PrintOutLine.java
@@ -2,6 +2,9 @@ package students.vladimir_aboltins.lesson_2.level_5.task_15;
 
 class PrintOutLine {
     public static void main(String[] args) {
-        System.out.print("text");
+
+        System.out.print("text ");
+        System.out.print("text 2");
+
     }
 }
