dfc965f1fe0cbb70d684c361084c3a8b38c4de07
OlgaT <ok.latvia@gmail.com>
2021-05-18 18:29:58 +0300

    Добавила Lesson 3 Level 4 Task 16

---
+++ src/main/java/students/olga_t/lesson_3/level_4/task_16/BankAccount.java
@@ -0,0 +1,25 @@
+package students.olga_t.lesson_3.level_4.task_16;
+
+class BankAccount {
+    String ownerFirstName;
+    String ownerLastName;
+    int moneyAmount;
+
+    BankAccount(String ownerFirstName, String ownerLastName, int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    String getOwnerLastName() {
+        return this.ownerLastName;
+    }
+
+    int getMoneyAmount() {
+        return this.moneyAmount;
+    }
+}
