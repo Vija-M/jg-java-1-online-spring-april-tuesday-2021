f4fc2a41600f899c475fe963d7a6aea32133dbc5
OlgaT <ok.latvia@gmail.com>
2021-05-18 17:59:30 +0300

    Добавила Lesson 3 Level 4 Task 14

---
+++ src/main/java/students/olga_t/lesson_3/level_4/task_14/PhoneDemo.java
@@ -0,0 +1,10 @@
+package students.olga_t.lesson_3.level_4.task_14;
+
+class PhoneDemo {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
