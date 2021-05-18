af4b6457dc20f24f65120db807dc1b9c8033095e
AzrailXD <whiteus@inbox.lv>
2021-05-18 20:28:38 +0300

    Lesson 2 - Level 5 - Task 18

--- src/main/java/students/vladimir_aboltins/lesson_2/level_5/task_18/SizeMatters.java
+++ src/main/java/students/vladimir_aboltins/lesson_2/level_5/task_18/SizeMatters.java
@@ -1,6 +1,6 @@
 package students.vladimir_aboltins.lesson_2.level_5.task_18;
 
-public class SizeMatters {
+class SizeMatters {
     public static void main(String[] args) {
         System.out.println("Please enter your name:");
         System.out.println("Please fix me!");
