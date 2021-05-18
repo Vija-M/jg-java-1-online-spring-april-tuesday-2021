073ae54fc6309c424d7807f975ebe30e43b5be2f
Linda-Ju <linda.junkina@gmail.com>
2021-05-18 18:11:51 +0300

    Lesson 3, Level 1, Task 1,2,3,4,5,6,

--- src/main/java/students/linda_junkina/lesson_3/level_1/task_1_2_3_4_5_6/Robot.java
+++ src/main/java/students/linda_junkina/lesson_3/level_1/task_1_2_3_4_5_6/Robot.java
@@ -2,8 +2,29 @@ package students.linda_junkina.lesson_3.level_1.task_1_2_3_4_5_6;
 
 class Robot {
 
-String sayYourName;
-String sayHello;
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO!");
+    }
+
+
+    String greeting;
+    String name;
+
+    Robot() {
+    }
+
+    Robot(String greeting, String name) {
+        this.greeting = greeting;
+        this.name = name;
+    }
+    void printInfo() {
+        System.out.println(this.greeting + ", " + this.name);
+    }
+
 
 }
 
--- src/main/java/students/linda_junkina/lesson_3/level_1/task_1_2_3_4_5_6/RobotDemo.java
+++ src/main/java/students/linda_junkina/lesson_3/level_1/task_1_2_3_4_5_6/RobotDemo.java
@@ -3,21 +3,30 @@ package students.linda_junkina.lesson_3.level_1.task_1_2_3_4_5_6;
 class RobotDemo {
     public static void main(String[] args) {
 
-        Robot robot1 = new Robot ();
-        robot1.sayHello = "Hello Human!";
-        robot1.sayYourName = "ROBO!";
+        Robot robot1 = new Robot("Hello", "my name is ROBO!");
+        robot1.printInfo ();
 
-        System.out.println(robot1.sayHello);
-        System.out.println("My name is " + robot1.sayYourName);
+        robot1.sayHello();
+        robot1.sayYourName();
 
-        System.out.println ();
 
-        Robot robot2 = new Robot ();
-        robot2.sayHello = "Hello Human!";
-        robot2.sayYourName = "ROBO!";
+        Robot robot2 = new Robot("Hello", "my name is ROBO!");
+        robot2.printInfo ();
 
-        System.out.println(robot2.sayHello);
-        System.out.println("My name is " + robot2.sayYourName);
+
+        robot2.sayHello();
+        robot2.sayYourName();
+
+
+
+
+
+
+
+
+
+
+
 
 
 
