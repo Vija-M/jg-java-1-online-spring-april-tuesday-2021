53ba3baadff61813b47d99d3d97c81618a39d269
Linda-Ju <linda.junkina@gmail.com>
2021-05-19 22:18:13 +0300

    Lesson 3, Level 4, Task 15

---
+++ src/main/java/students/linda_junkina/lesson_3/level_4/task_15/Car.java
@@ -0,0 +1,16 @@
+package students.linda_junkina.lesson_3.level_4.task_15;
+
+class Car {
+
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel () {
+        return this.model;
+    }
+
+
+}
