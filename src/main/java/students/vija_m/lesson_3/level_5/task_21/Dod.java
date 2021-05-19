4edc90e3e514b25f5cfd0c70675c20b90f65308e
Vija-M <vija.riga@gmailcom>
2021-05-19 14:08:10 +0300

    Lesson 3, Level 5, Task 20

---
+++ src/main/java/students/vija_m/lesson_3/level_5/task_21/Dod.java
@@ -0,0 +1,22 @@
+package students.vija_m.lesson_3.level_5.task_21;
+
+class Dog {
+    String name;
+    int age;
+    String color;
+
+    public Dog(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    void voice() {
+        System.out.println("Dog's name is " + this.name +", dog's age - " + this.age +", dog's color - " + this.color +".");
+    }
+    void changeColor(String newColor) {
+        color = newColor;
+        System.out.println("Now " + this.name +" has a new color!!!");
+    }
+
+}
