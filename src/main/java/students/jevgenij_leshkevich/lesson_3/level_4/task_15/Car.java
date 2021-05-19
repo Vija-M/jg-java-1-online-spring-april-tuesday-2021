c9ac14ff0b87cd33b34b87d8b85dbade115b6ad1
jleshkev <evgeny.leshkevich@gmail.com>
2021-05-20 00:37:01 +0300

    Completed Task 15 - Lesson 3 - Level 4

---
+++ src/main/java/students/jevgenij_leshkevich/lesson_3/level_4/task_15/Car.java
@@ -0,0 +1,16 @@
+package students.jevgenij_leshkevich.lesson_3.level_4.task_15;
+
+class Car {
+
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() { // добавил ()
+        return this.model; // добавил ;
+    }
+
+
+}
