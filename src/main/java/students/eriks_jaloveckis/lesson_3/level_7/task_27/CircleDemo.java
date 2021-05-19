bfcc7c76e9a9f52a6b168937fd1e9128588edd04
eriks <erikslv88@gmail.com>
2021-05-19 10:41:14 +0300

    Finish - 3 lesson 7 level 27 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_7/task_27/CircleDemo.java
@@ -0,0 +1,10 @@
+package students.eriks_jaloveckis.lesson_3.level_7.task_27;
+
+class CircleDemo {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        circle.calculateArea();
+    }
+
+}
