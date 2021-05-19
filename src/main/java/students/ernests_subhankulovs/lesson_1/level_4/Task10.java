44c9ad2ac2cc581de059d7f1c6cc6a7f9f2186b7
Ernests1992 <ernests@neolain.lv>
2021-05-19 19:43:45 +0300

    Lesson 1 - Level - 4 - Task 10. Folder order corrected.

--- src/main/java/students/ernests_subhankulovs/lesson_1/level_4/Task10.java
+++ src/main/java/students/ernests_subhankulovs/lesson_1/level_4/Task10.java
@@ -2,6 +2,6 @@ package students.ernests_subhankulovs.lesson_1.level_4;
 
 class Task10 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("HelloWorld!");
     }
 }
