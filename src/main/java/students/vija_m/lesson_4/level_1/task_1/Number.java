5fe2966c268352968ba4c58628c5f0571ca6934f
Vija-M <vija.riga@gmailcom>
2021-05-20 22:27:02 +0300

    Lesson 3, Level 7, Task 30

---
+++ src/main/java/students/vija_m/lesson_4/level_1/task_1/Number.java
@@ -0,0 +1,21 @@
+package students.vija_m.lesson_4.level_1.task_1;
+import java.util.Scanner;
+class Number {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Пожалуйста, введите целое число.");
+        int a = scanner.nextInt();
+        {
+
+            if (a > 0) {
+                System.out.println("Число " + a + " положительное");
+            } else if (a < 0) {
+                System.out.println("Число " + a + " отрицательное");
+            } else {
+                System.out.println("Число " + a + " равно нулю");
+            }
+        }
+    }}
+
+
