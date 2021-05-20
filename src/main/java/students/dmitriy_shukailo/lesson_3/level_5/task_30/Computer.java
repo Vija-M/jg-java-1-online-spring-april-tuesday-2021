ba56e41affdda32ca32b1a2549922c5e08532fba
dk-t <shukailo.d@gmail.com>
2021-05-20 21:50:53 +0300

    Computer has been fixed

---
+++ src/main/java/students/dmitriy_shukailo/lesson_3/level_5/task_30/Computer.java
@@ -0,0 +1,21 @@
+package students.dmitriy_shukailo.lesson_3.level_5.task_30;
+
+class Computer {
+
+    String manufacturer;
+    String model;
+
+    Computer(String manufacturer, String model) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+
+    String getManufacturer() {
+        return this.manufacturer;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
