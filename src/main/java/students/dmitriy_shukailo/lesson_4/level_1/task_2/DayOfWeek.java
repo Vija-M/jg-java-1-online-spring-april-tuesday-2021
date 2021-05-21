6dd27ab189d9f4e7ccddd6ea7a456d66acf40460
dk-t <shukailo.d@gmail.com>
2021-05-22 00:20:36 +0300

    DayOfWeek has been fixed

--- src/main/java/students/dmitriy_shukailo/lesson_4/level_1/task_2/DayOfWeek.java
+++ src/main/java/students/dmitriy_shukailo/lesson_4/level_1/task_2/DayOfWeek.java
@@ -25,17 +25,6 @@ import java.util.Scanner;
                  break;
              case 7:System.out.println("Sanday");
                  break;
-
-
-
-
-         /*case 2: System.out.println(“Tuesday”); break;
-         case 3: System.out.println(“Wednesday”); break;
-         case 4: System.out.println(“Thursday”); break;
-         case 5: System.out.println(“Friday”); break;
-         case 6: System.out.println(“Saturday”); break;
-         default : System.out.println(“Sanday”); break;*/
          }
-
      }
  }
