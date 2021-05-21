c55ad480228b781720b79c2c9d873f164d372b78
Ernests1992 <ernests@neolain.lv>
2021-05-21 20:33:02 +0300

    Lesson 4 - Level 2 - Task 4. "public" removed.

--- src/main/java/students/ernests_subhankulovs/lesson_4/level_2/task_4/Task4.java
+++ src/main/java/students/ernests_subhankulovs/lesson_4/level_2/task_4/Task4.java
@@ -2,7 +2,7 @@ package students.ernests_subhankulovs.lesson_4.level_2.task_4;
 
 import java.util.Scanner;
 
-public class Task4 {
+class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter integer number A:");
