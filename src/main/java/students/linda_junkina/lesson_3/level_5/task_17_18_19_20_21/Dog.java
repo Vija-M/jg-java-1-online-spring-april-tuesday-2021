3af81e1f93177192507e12fffcb2d09686b3c142
Linda-Ju <linda.junkina@gmail.com>
2021-05-21 17:43:35 +0300

    Lesson 3, Level 5, Task 17,18,19,20,21

---
+++ src/main/java/students/linda_junkina/lesson_3/level_5/task_17_18_19_20_21/Dog.java
@@ -0,0 +1,52 @@
+package students.linda_junkina.lesson_3.level_5.task_17_18_19_20_21;
+
+class Dog {
+
+    String name;
+        Dog(String name) {
+        this.name = name;
+    }
+
+
+    String voice;
+
+    String voice() {
+        System.out.println(name + ", " + name + ", " + name + "!");
+        return voice;
+    }
+
+
+    int age;
+
+    int Dog(int age) {
+        this.age = age;
+        return age;
+    }
+
+
+    int newAge;
+
+    void happyBirthDay () {
+        this.newAge = this.age + 1;
+    }
+
+    String colour;
+
+    String colour(String colour) {
+        this.colour = colour;
+        return colour;
+    }
+
+    String newColour;
+
+        void newColour () {
+        this.newColour = newColour;
+
+    }
+}
+
+
+
+
+
+
---
+++ src/main/java/students/linda_junkina/lesson_3/level_5/task_17_18_19_20_21/DogDemo.java
@@ -0,0 +1,28 @@
+package students.linda_junkina.lesson_3.level_5.task_17_18_19_20_21;
+
+class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog("Jack");
+
+        dog1.voice();
+
+
+        dog1.age = 10;
+        System.out.println(dog1.age + " years old.");
+
+
+        dog1.happyBirthDay();
+        System.out.println("Happy Birthday! You are now " + dog1.newAge);
+
+
+        dog1.colour = "black";
+        System.out.println("Jack was " + dog1.colour);
+
+        dog1.newColour = "grey";
+        System.out.println("But now he is " + dog1.newColour);
+
+
+    }
+}
