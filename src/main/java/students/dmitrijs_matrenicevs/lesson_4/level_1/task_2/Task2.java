762b8c5932e065b6e9f391230726efef5b71754f
DeTELo <mrdinas@gmail.com>
2021-05-22 11:48:00 +0300

    Lesson 4 Task-2 - LEVEL1 - FINISH

--- src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_1/task_2/Task2.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_1/task_2/Task2.java
@@ -7,7 +7,7 @@ class Task2 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Напиши целое чилсо: ");
+        System.out.print("Напиши целое чилсо от 1 до 7: ");
         int a = scanner.nextInt();
 
         if (a == 1){
@@ -16,6 +16,29 @@ class Task2 {
 
         else if (a == 2){
             System.out.print("Вторник");
+        }
+
+        else if (a == 3){
+            System.out.print("Среда");
+        }
+
+        else if (a == 4){
+            System.out.print("Четверг");
+        }
+
+        else if (a == 5){
+            System.out.print("Пятница");
+        }
+
+        else if (a == 6){
+            System.out.print("Суббота");
+        }
+
+        else if (a == 7){
+            System.out.print("Воскресенье");
+        }
+        else{
+            System.out.print("Ошибка, пробуй снова ввести число от 1 до 7");
         }
 
 
