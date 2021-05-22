469dca6dc319a26ae4112c7fa56fcbf054bdccdd
eriks <erikslv88@gmail.com>
2021-05-22 15:12:40 +0300

    Finish - 4 lesson 1 level 1 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_4/level_1/task_1/Task1.java
@@ -0,0 +1,22 @@
+package students.eriks_jaloveckis.lesson_4.level_1.task_1;
+
+    import java.util.Scanner;
+    class Task1 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите целое число: ");
+
+
+            int number = scanner.nextInt();
+            if (number < 0) {
+                System.out.println("Ваше число отрицательное!");
+            }
+            else if (number == 0){
+                System.out.println("Ваше число равно нулю!");
+            }
+            else {
+                System.out.println("Ваше число положительное!");
+            }
+        }
+    }
+
