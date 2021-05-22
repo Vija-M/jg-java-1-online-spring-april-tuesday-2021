a9c4a230b7f8ba4e5c5d9ef3798fd9adccb0c9d2
DeTELo <mrdinas@gmail.com>
2021-05-22 11:42:10 +0300

    Lesson 4 Task-2 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_1/task_2/Task2.java
@@ -0,0 +1,27 @@
+package students.dmitrijs_matrenicevs.lesson_4.level_1.task_2;
+
+import java.util.Scanner;
+
+class Task2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Напиши целое чилсо: ");
+        int a = scanner.nextInt();
+
+        if (a == 1){
+            System.out.print("Понедельник");
+        }
+
+        else if (a == 2){
+            System.out.print("Вторник");
+        }
+
+
+
+
+    }
+
+
+}
