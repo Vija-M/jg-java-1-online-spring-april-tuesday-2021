7e3100972bbd397767c9834c547b149cdba2cbee
DeTELo <mrdinas@gmail.com>
2021-05-22 23:37:26 +0300

    Lesson 4 Task-10-14 - LEVEL4

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_4/task_10_14/Calculator.java
@@ -0,0 +1,25 @@
+package students.dmitrijs_matrenicevs.lesson_4.level_4.task_10_14;
+
+class Calculator {
+
+   public int sum (int firstNumber, int secondNumber){
+       return firstNumber + secondNumber;
+
+   }
+
+    public int sub (int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+
+    }
+
+    public int div (int firstNumber, int secondNumber){
+        return firstNumber / secondNumber;
+
+    }
+
+    public int mul (int firstNumber, int secondNumber){
+        return firstNumber * secondNumber;
+
+    }
+
+}
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_4/task_10_14/CalculatorTest.java
@@ -0,0 +1,66 @@
+package students.dmitrijs_matrenicevs.lesson_4.level_4.task_10_14;
+
+class CalculatorTest {
+
+    public static void main(String[] args) {
+        CalculatorTest calculatorTest = new CalculatorTest();
+        calculatorTest.sumTest();
+        calculatorTest.subTest();
+        calculatorTest.divTest();
+        calculatorTest.mulTest();
+    }
+
+
+    public void sumTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 15;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Sum test = OK");
+        } else {
+            System.out.println("Sum test = FAIL");
+        }
+    }
+
+    public void subTest(){
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 15;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Sub test = OK");
+        } else {
+            System.out.println("Sub test = FAIL");
+        }
+    }
+
+    void divTest(){
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 15;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Div test = OK");
+        } else {
+            System.out.println("Div test = FAIL");
+        }
+    }
+
+    void mulTest(){
+        int firstNumber = 5;
+        int secondNumber = 2;
+        int expectedResult = 10;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Mul test = OK");
+        } else {
+            System.out.println("Mul test = FAIL");
+        }
+    }
+
+}
