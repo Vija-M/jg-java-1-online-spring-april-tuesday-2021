9a2666c4fe3cbafafff8e271d89efb4ce5622b10
eriks <erikslv88@gmail.com>
2021-05-22 16:25:18 +0300

    Finish - 4 lesson 4 level 11 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_4/level_4/task_11/Calculator.java
@@ -0,0 +1,19 @@
+package students.eriks_jaloveckis.lesson_4.level_4.task_11;
+
+class Calculator {
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int sub(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int div(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public int mul(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+}
