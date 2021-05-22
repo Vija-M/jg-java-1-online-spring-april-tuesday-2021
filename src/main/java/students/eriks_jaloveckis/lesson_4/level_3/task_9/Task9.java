ecca8e303bc6db617b32fdf5f0560e255db7d964
eriks <erikslv88@gmail.com>
2021-05-22 16:02:35 +0300

    Finish - 4 lesson 3 level 9 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_4/level_3/task_9/Task9.java
@@ -0,0 +1,23 @@
+package students.eriks_jaloveckis.lesson_4.level_3.task_9;
+import java.util.Scanner;
+class Task9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int secondNumber = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int thirdNumber = scanner.nextInt();
+
+        if (firstNumber > secondNumber && firstNumber > thirdNumber){
+            System.out.println("Наобильшое число: " + firstNumber);
+        }
+        else if (secondNumber > firstNumber && secondNumber > thirdNumber){
+            System.out.println("Наибольшое число: " + secondNumber);
+        }
+        else {
+            System.out.println("Наибольшое число: " + thirdNumber);
+        }
+    }
+}
