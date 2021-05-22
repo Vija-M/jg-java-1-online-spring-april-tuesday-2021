ec792e660bdc749b5a0866b78fb6c87b06e9263b
eriks <erikslv88@gmail.com>
2021-05-22 15:48:51 +0300

    Finish - 4 lesson 3 level 7 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_4/level_3/task_7/Task7.java
@@ -0,0 +1,25 @@
+package students.eriks_jaloveckis.lesson_4.level_3.task_7;
+import java.util.Scanner;
+class Task7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+        System.out.print("Введите третье число: ");
+        int thirdNumber = scanner.nextInt();
+
+        if (firstNumber == secondNumber && firstNumber == thirdNumber){
+            System.out.print("All numbers are equal");
+        }
+        else if (firstNumber != secondNumber && firstNumber != thirdNumber){
+            System.out.print("All numbers are different");
+        }
+        else {
+            System.out.print("Neither all are equal or different");
+        }
+    }
+
+
+}
