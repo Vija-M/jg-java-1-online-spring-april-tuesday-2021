51ed475ca7dec1ca4cc68f9d89b4e1d2a9c8b792
DeTELo <mrdinas@gmail.com>
2021-05-22 22:13:21 +0300

    Lesson 4 Task-6 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_2/task_6/Task6.java
@@ -0,0 +1,20 @@
+package students.dmitrijs_matrenicevs.lesson_4.level_2.task_6;
+
+import java.util.Scanner;
+
+class Task6 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введи первое целое число: ");
+        int a = scanner.nextInt();
+        System.out.print("Введи второе целое число: ");
+        int b = scanner.nextInt();
+
+
+
+
+
+    }
+}
