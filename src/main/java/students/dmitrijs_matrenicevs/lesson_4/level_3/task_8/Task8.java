62f24e0f4d31cdd67ef531e6a783fa3cadb93414
DeTELo <mrdinas@gmail.com>
2021-05-22 22:39:45 +0300

    Lesson 4 Task-8 - LEVEL3

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_3/task_8/Task8.java
@@ -0,0 +1,18 @@
+package students.dmitrijs_matrenicevs.lesson_4.level_3.task_8;
+
+import java.util.Scanner;
+
+class Task8 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое целое число: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите второе целое число: ");
+        int b = scanner.nextInt();
+        System.out.println();
+
+    }
+
+}
