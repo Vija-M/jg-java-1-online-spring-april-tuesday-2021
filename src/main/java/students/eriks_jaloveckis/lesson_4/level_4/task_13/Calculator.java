7cfad5e1362c1ebb67ef674adab93430a38dbf94
eriks <erikslv88@gmail.com>
2021-05-22 17:32:10 +0300

    Finish - 4 lesson 4 level 13 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_4/level_4/task_13/Calculator.java
@@ -0,0 +1,35 @@
+package students.eriks_jaloveckis.lesson_4.level_4.task_13;
+
+public class Calculator {
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int sub(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int div(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public int mul(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int maxOfTwoNumbers(int firstNumber, int secondNumber){
+        if (firstNumber > secondNumber)
+            return firstNumber;
+        else{
+            return secondNumber;
+        }
+    }
+}
