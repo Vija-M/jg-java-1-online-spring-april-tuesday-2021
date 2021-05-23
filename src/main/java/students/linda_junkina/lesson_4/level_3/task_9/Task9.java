cbefd25322cb27d78aabce56e986dd42b9f2356e
Linda-Ju <linda.junkina@gmail.com>
2021-05-23 16:52:05 +0300

    Lesson 4, Level 3, Task 9

---
+++ src/main/java/students/linda_junkina/lesson_4/level_3/task_9/Task9.java
@@ -0,0 +1,8 @@
+package students.linda_junkina.lesson_4.level_3.task_9;
+
+class Task9 {
+    int numberA;
+    int numberB;
+    int numberC;
+
+}
---
+++ src/main/java/students/linda_junkina/lesson_4/level_3/task_9/Task9Demo.java
@@ -0,0 +1,21 @@
+package students.linda_junkina.lesson_4.level_3.task_9;
+
+import java.util.Scanner;
+class Task9Demo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Enter 3 numbers: ");
+        int numberA = scanner.nextInt();
+        int numberB = scanner.nextInt();
+        int numberC = scanner.nextInt();
+
+        if (numberA > numberB && numberA > numberC) {
+            System.out.println(numberA + " is the biggest");
+        } else if (numberB > numberA && numberB > numberC){
+            System.out.println(numberB + " is the biggest");
+        } else if (numberC > numberA && numberC > numberB) {
+            System.out.println(numberC + " is the biggest");
+        }
+    }
+}
