7477b9fbc7bacecab85e47b812bde8d4d545b67f
Ernests1992 <ernests@neolain.lv>
2021-05-23 19:54:25 +0300

    Lesson 4 - Level 7 - Task 17.

---
+++ src/main/java/students/ernests_subhankulovs/lesson_4/level_7/task_17/FizzBuzz.java
@@ -0,0 +1,18 @@
+package students.ernests_subhankulovs.lesson_4.level_7.task_17;
+
+class FizzBuzz {
+    public String detect(int number) {
+        if (number % 3 == 0 && number % 5 != 0) {
+            return "Fizz";
+        }
+        else if (number % 5 == 0 && number % 3 != 0) {
+            return "Buzz";
+        }
+        else if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else {
+            return "" + number;
+        }
+    }
+}
