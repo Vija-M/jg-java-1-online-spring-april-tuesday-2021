0b2a054143537d10d53e5dc74b601574f8105d9e
DeTELo <mrdinas@gmail.com>
2021-05-23 16:08:12 +0300

    Lesson 3 Task-30 - LEVEL7 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/level_7/task_30/Computer.java
@@ -0,0 +1,23 @@
+package students.dmitrijs_matrenicevs.lesson_3.level_7.task_30;
+
+class Computer {
+
+    String manufacturer;
+    String model;
+
+    Computer(String manufacturer, String model) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+
+    String getManufacturer(){
+
+    return this.manufacturer;
+    }
+
+    String getModel () {
+
+   return this.model;
+    }
+
+}
