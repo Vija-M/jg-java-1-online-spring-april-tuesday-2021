39625b050e3096d05cdf8d5c450a1b10c362063c
neontone <antons.korenevskis@gmail.com>
2021-05-23 10:07:16 +0300

    Task_1 is done

---
+++ src/main/java/students/antons_korenevskis/lesson_4/Task_1.java
@@ -0,0 +1,27 @@
+package students.antons_korenevskis.lesson_4;
+
+import java.util.Scanner;
+
+class Task_1 {
+
+    public static void main(String[] args) {
+
+        int a = 0;
+        System.out.println("Введи число");
+
+        Scanner scanner = new Scanner(System.in);
+        int b = scanner.nextInt();
+
+        boolean equal = a == b;
+        boolean greaterThan = a > b;
+
+        if (equal) {
+            System.out.println("Число равно 0");
+        } else if (greaterThan) {
+            System.out.println("Это отрицательное число");
+        } else {
+            System.out.println("Это положительное число");
+        }
+
+    }
+}
