7b2e39d7a6fb8516534e9cefb31c32fcb9988ab0
salun <salun.alex@gmail.com>
2021-05-23 19:38:27 +0300

    Changes in Task 7, Lesson 4, Level 3

--- src/main/java/students/olga_saluna/lesson_4/level_3/task_7/Task7.java
+++ src/main/java/students/olga_saluna/lesson_4/level_3/task_7/Task7.java
@@ -1,8 +1,8 @@
 package students.olga_saluna.lesson_4.level_3.task_7;
-
+import java.util.Scanner;
 class Task7 {
     public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter X integer: ");
 
@@ -17,7 +17,7 @@ class Task7 {
         int z = scanner.nextInt();
 
         if (x==y&&y==z) {System.out.println ("All numbers are equal");}
-        if (x!=y&&y!=z) {System.out.println ("All numbers are different");}
+        if (x!=y && y!=z && z!=x) {System.out.println ("All numbers are different");}
         else {System.out.println ("Neither all are equal or different");}
 
     }
