46124571a8a27152e4b802e3e8dbaaf2706e0c1b
salun <salun.alex@gmail.com>
2021-05-23 13:25:35 +0300

    Task 18 Lesson 3 Level 5

---
+++ src/main/java/students/olga_saluna/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,9 @@
+package students.olga_saluna.lesson_3.level_5.task_18;
+
+class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog ("Fluffy", 4);
+        dog.voice();
+
+    }
+}
