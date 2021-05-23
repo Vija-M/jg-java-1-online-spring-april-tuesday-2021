e48763f8d3525ca577fe4800b19f0c4dd980d0d5
salun <salun.alex@gmail.com>
2021-05-23 19:26:45 +0300

    Changes in Task 6, Lesson 4, Level 2

--- src/main/java/students/olga_saluna/lesson_4/level_2/task_6/Task6.java
+++ src/main/java/students/olga_saluna/lesson_4/level_2/task_6/Task6.java
@@ -1,9 +1,9 @@
 package students.olga_saluna.lesson_4.level_2.task_6;
-
+import java.util.Scanner;
 class Task6 {
     public static void main(String[] args) {
 
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter X integer: ");
 
