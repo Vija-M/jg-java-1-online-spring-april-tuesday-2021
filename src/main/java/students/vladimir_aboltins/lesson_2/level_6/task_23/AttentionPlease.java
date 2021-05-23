bcff848418ebf2a650ee7800ed10fa71ab244722
AzrailXD <whiteus@inbox.lv>
2021-05-23 17:03:20 +0300

    Lesson 2 - Level 6 - Task 23

---
+++ src/main/java/students/vladimir_aboltins/lesson_2/level_6/task_23/AttentionPlease.java
@@ -0,0 +1,16 @@
+package students.vladimir_aboltins.lesson_2.level_6.task_23;
+
+class AttentionPlease {
+
+
+
+    public static void main(String[] args) {
+
+        printText();
+
+    }
+    public static void printText(){
+        System.out.println("Code outside the method will not compile!");
+    }
+
+}
