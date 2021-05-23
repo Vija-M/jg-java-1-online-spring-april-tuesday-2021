8bfa4c16ee729d54cefcc5e3592bbb4aebb28bef
DeTELo <mrdinas@gmail.com>
2021-05-23 15:28:49 +0300

    Lesson 3 Task-28 - LEVEL7

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/level_7/task_28/Product.java
@@ -0,0 +1,18 @@
+package students.dmitrijs_matrenicevs.lesson_3.level_7.task_28;
+
+class Product {
+
+    String name;
+    double regularPrice;
+    double discount;
+
+    Product(double regularPrice){
+
+        this.name = name;
+
+
+    }
+
+
+
+}
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_3/level_7/task_28/ProductDemo.java
@@ -0,0 +1,11 @@
+package students.dmitrijs_matrenicevs.lesson_3.level_7.task_28;
+
+class ProductDemo {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
