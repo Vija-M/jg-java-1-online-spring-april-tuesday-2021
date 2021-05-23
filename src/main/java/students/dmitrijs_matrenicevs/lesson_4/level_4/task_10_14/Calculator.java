f23312c2e9d23c3b10c2bd4d9d69c79d3f0377ac
DeTELo <mrdinas@gmail.com>
2021-05-23 14:03:10 +0300

    Lesson 4 Task-10-14 - LEVEL4 - FINISH

--- src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_4/task_10_14/Calculator.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_4/task_10_14/Calculator.java
@@ -2,36 +2,36 @@ package students.dmitrijs_matrenicevs.lesson_4.level_4.task_10_14;
 
 class Calculator {
 
-   public int sum (int firstNumber, int secondNumber){
-       return firstNumber + secondNumber;
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
 
-   }
+    }
 
-    public int sub (int firstNumber, int secondNumber){
+    public int sub(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
 
     }
 
-    public int div (int firstNumber, int secondNumber){
+    public int div(int firstNumber, int secondNumber) {
         return firstNumber / secondNumber;
 
     }
 
-    public int mul (int firstNumber, int secondNumber){
+    public int mul(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
 
     }
 
     public boolean isEven(int number) {
 
-       if (number % 2 == 0) {
+        if (number % 2 == 0) {
 
-           return true;
+            return true;
 
-       } else {
+        } else {
 
-           return false;
-       }
+            return false;
+        }
 
     }
 
@@ -39,12 +39,30 @@ class Calculator {
 
         if (firstNumber >= secondNumber) {
 
-        return firstNumber;
+            return firstNumber;
         } else {
-        return secondNumber;
+            return secondNumber;
         }
 
     }
+
+    public int numbersOneTwoTreeMore(int firstNumber, int secondNumber, int treeNumber) {
+
+        if (firstNumber >= secondNumber && firstNumber > treeNumber) {
+
+            return firstNumber;
+
+        } else if (secondNumber > treeNumber && secondNumber > firstNumber) {
+
+            return secondNumber;
+
+        } else {
+
+            return treeNumber;
+        }
+
+    }
+
 
 
 
--- src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_4/task_10_14/CalculatorTest.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_4/task_10_14/CalculatorTest.java
@@ -12,6 +12,11 @@ class CalculatorTest {
         calculatorTest.maxOfTwoNumbersOne();
         calculatorTest.maxOfTwoNumbersTwo();
         calculatorTest.maxOfTwoNumbersTree();
+        calculatorTest.numbersOneTwoTreeMoreOne();
+        calculatorTest.numbersOneTwoTreeMoreTwo();
+        calculatorTest.numbersOneTwoTreeMoreTree();
+        calculatorTest.numbersOneTwoTreeMoreFour();
+        calculatorTest.numbersOneTwoTreeMoreFive();
     }
 
 
@@ -115,6 +120,77 @@ class CalculatorTest {
             System.out.println("maxOfTwoNumbersTree test (=) = OK");
         } else {
             System.out.println("maxOfTwoNumbersTree test (=) = FAIL");
+        }
+    }
+
+//первое число больше второго и третьего
+    void numbersOneTwoTreeMoreOne(){
+        int firstNumber = 15;
+        int secondNumber = 5;
+        int treeNumber = 7;
+        int expectedResult = firstNumber;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.numbersOneTwoTreeMore(firstNumber, secondNumber, treeNumber);
+        if (realResult == expectedResult) {
+            System.out.println("numbersOneTwoTreeMore test (1) = OK");
+        } else {
+            System.out.println("numbersOneTwoTreeMore test (1) = FAIL");
+        }
+    }
+//второе число больше первого и третьего
+    void numbersOneTwoTreeMoreTwo(){
+        int firstNumber = 2;
+        int secondNumber = 15;
+        int treeNumber = 3;
+        int expectedResult = secondNumber;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.numbersOneTwoTreeMore(firstNumber, secondNumber, treeNumber);
+        if (realResult == expectedResult) {
+            System.out.println("numbersOneTwoTreeMore test (2) = OK");
+        } else {
+            System.out.println("numbersOneTwoTreeMore test (2) = FAIL");
+        }
+    }
+//третье число больше первого и второго
+    void numbersOneTwoTreeMoreTree(){
+        int firstNumber = 4;
+        int secondNumber = 15;
+        int treeNumber = 17;
+        int expectedResult = treeNumber;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.numbersOneTwoTreeMore(firstNumber, secondNumber, treeNumber);
+        if (realResult == expectedResult) {
+            System.out.println("numbersOneTwoTreeMore test (3) = OK");
+        } else {
+            System.out.println("numbersOneTwoTreeMore test (3) = FAIL");
+        }
+    }
+//первые два равны и больше третьего
+    void numbersOneTwoTreeMoreFour(){
+        int firstNumber = 14;
+        int secondNumber = 14;
+        int treeNumber = 5;
+        int expectedResult = firstNumber;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.numbersOneTwoTreeMore(firstNumber, secondNumber, treeNumber);
+        if (realResult == expectedResult && expectedResult == secondNumber && expectedResult != treeNumber) {
+            System.out.println("numbersOneTwoTreeMore test (4) = OK");
+        } else {
+            System.out.println("numbersOneTwoTreeMore test (4) = FAIL");
+        }
+    }
+//три числа равны
+    void numbersOneTwoTreeMoreFive(){
+        int firstNumber = 14;
+        int secondNumber = 14;
+        int treeNumber = 14;
+        int expectedResult = firstNumber;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.numbersOneTwoTreeMore(firstNumber, secondNumber, treeNumber);
+        if (realResult == expectedResult && firstNumber == secondNumber && treeNumber == secondNumber && firstNumber == treeNumber) {
+            System.out.println("numbersOneTwoTreeMore test (5) = OK");
+        } else {
+            System.out.println("numbersOneTwoTreeMore test (5) = FAIL");
         }
     }
 
