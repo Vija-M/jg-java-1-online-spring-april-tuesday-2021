402b0acd717050a0f7b58c1a36be72c7ca5e828a
AzrailXD <whiteus@inbox.lv>
2021-05-23 17:19:53 +0300

    Lesson 2 - Level 6 - Task 24

---
+++ src/main/java/students/vladimir_aboltins/lesson_2/level_6/task_24/NoSleepNoFocus.java
@@ -0,0 +1,9 @@
+package students.vladimir_aboltins.lesson_2.level_6.task_24;
+
+class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+
+}
