3a9468668882b18eeaeffe4056f142df5d9e2294
salun <salun.alex@gmail.com>
2021-05-23 14:30:15 +0300

    Task 27, lesson 3, level 7

---
+++ src/main/java/students/olga_saluna/lesson_3/level_7/task_27/CircleDemo.java
@@ -0,0 +1,9 @@
+package students.olga_saluna.lesson_3.level_7.task_27;
+
+class CircleDemo {
+    public static void main(String[] args) {
+
+    Circle circle = new Circle(6.55);
+    circle.circleArea();
+    }
+}
