8e38e87f5ee52994de319bb10ce1972e5456dc94
AzrailXD <whiteus@inbox.lv>
2021-05-25 01:21:18 +0300

    Lesson 3 - Level 2 - Task 11

---
+++ src/main/java/students/vladimir_aboltins/lesson_3/level_3/task_11/Robot.java
@@ -0,0 +1,21 @@
+package students.vladimir_aboltins.lesson_3.level_3.task_11;
+
+class Robot {
+
+    String name;
+
+Robot(String robotName){
+
+    this.name = robotName;
+}
+
+    void sayHello() {
+        System.out.println("Hello!");
+
+    }
+
+    void sayYourName() {
+        System.out.println("My name is " + this.name + "\n");
+
+    }
+}
