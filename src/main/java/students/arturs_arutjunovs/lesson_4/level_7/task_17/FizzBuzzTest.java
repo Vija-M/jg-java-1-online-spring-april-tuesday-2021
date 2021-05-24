6818d3a74e2171c82d5b264e099c6cd452ca46b5
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-24 21:59:12 +0100

    Completed   Lesson 4 Level 7 Task 17

--- src/main/java/students/arturs_arutjunovs/lesson_4/level_7/task_17/FizzBuzzTest.java
+++ src/main/java/students/arturs_arutjunovs/lesson_4/level_7/task_17/FizzBuzzTest.java
@@ -17,90 +17,65 @@ class FizzBuzzTest {
     }
 
     void divByZero() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int number = 0;
         String expectedResult = "FizzBuzz";
-        String actualResult = fizzBuzz.detect(number);
-
-        assertCheckDivision(expectedResult, actualResult, "Division by 0 test");
+        assertCheckDivision(expectedResult, number);
     }
 
     void divByOne() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int number = 1;
         String expectedResult = "1";
-        String actualResult = fizzBuzz.detect(number);
-
-        assertCheckDivision(expectedResult, actualResult, "Division by 1 test");
+        assertCheckDivision(expectedResult, number);
     }
 
     void divByThree() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int number = 9;
         String expectedResult = "Fizz";
-        String actualResult = fizzBuzz.detect(number);
-
-        assertCheckDivision(expectedResult, actualResult, "Division by 3 test");
+        assertCheckDivision(expectedResult, number);
     }
 
     void divByTen() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int number = 10;
         String expectedResult = "Buzz";
-        String actualResult = fizzBuzz.detect(number);
-
-        assertCheckDivision(expectedResult, actualResult, "Division by 10 test");
+        assertCheckDivision(expectedResult, number);
 
     }
 
     void divByFifteen() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int number = 15;
         String expectedResult = "FizzBuzz";
-        String actualResult = fizzBuzz.detect(number);
-
-        assertCheckDivision(expectedResult, actualResult, "Division by 15 test");
+        assertCheckDivision(expectedResult, number);
     }
 
     void divByNegativeFive() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int number = -5;
         String expectedResult = "Buzz";
-        String actualResult = fizzBuzz.detect(number);
-
-        assertCheckDivision(expectedResult, actualResult, "Division by -5 test");
+        assertCheckDivision(expectedResult, number);
     }
 
     void divByNegativeNine() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int number = -9;
         String expectedResult = "Fizz";
-        String actualResult = fizzBuzz.detect(number);
-
-        assertCheckDivision(expectedResult, actualResult, "Division by -9 test");
+        assertCheckDivision(expectedResult, number);
     }
 
     void divByNegativeFifteen() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int number = -15;
         String expectedResult = "FizzBuzz";
-        String actualResult = fizzBuzz.detect(number);
-
-        assertCheckDivision(expectedResult, actualResult, "Division by -15 test");
+        assertCheckDivision(expectedResult, number);
     }
 
     void divByNegativeFour() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         int number = -4;
         String expectedResult = "-4";
-        String actualResult = fizzBuzz.detect(number);
-
-        assertCheckDivision(expectedResult, actualResult, "Division by -4 test");
+        assertCheckDivision(expectedResult, number);
     }
 
-    void assertCheckDivision(String expectedResult, String actualResult, String description) {
+    void assertCheckDivision(String expectedResult, int number) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actualResult = fizzBuzz.detect(number);
         if (expectedResult.equals(actualResult)) {
-            System.out.println("[Test OK]: Actual result: " + actualResult + ", " + description);
+            System.out.println("[Test OK]: Actual result: " + actualResult + ", " + "Division by: " + "" + number);
         } else {
             System.out.println("[Test FAILED]: " + "Actual result: " + actualResult + ",Expected: " + expectedResult);
         }
