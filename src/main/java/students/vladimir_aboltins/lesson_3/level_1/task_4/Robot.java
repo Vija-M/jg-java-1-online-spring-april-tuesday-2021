d5e1699ff4f8f263d0c6c412d8c3f56ac1a62b2b
AzrailXD <whiteus@inbox.lv>
2021-05-24 20:04:15 +0300

    Lesson 3 - Level 1 - Task 4

---
+++ src/main/java/students/vladimir_aboltins/lesson_3/level_1/task_4/Robot.java
@@ -0,0 +1,7 @@
+package students.vladimir_aboltins.lesson_3.level_1.task_4;
+
+class Robot {
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/main/java/students/vladimir_aboltins/lesson_3/level_1/task_4/RobotDemo.java
@@ -0,0 +1,9 @@
+package students.vladimir_aboltins.lesson_3.level_1.task_4;
+
+class RobotDemo {
+    public static void main(String[] args) {
+
+Robot robotOne = new Robot();
+        robotOne.sayHello();
+    }
+}
