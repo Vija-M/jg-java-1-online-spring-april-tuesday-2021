36a08cd2700a7382329e8dd6a58f8a931bfe6f69
Vija-M <vija.riga@gmailcom>
2021-05-24 21:18:54 +0300

    Lesson 4, Level 4, Task 10

--- src/main/java/students/vija_m/lesson_4/level_4/task_10/Calculator.java
+++ src/main/java/students/vija_m/lesson_4/level_4/task_10/Calculator.java
@@ -1,6 +1,6 @@
 package students.vija_m.lesson_4.level_4.task_10;
 
-public class Calculator {
+class Calculator {
        public int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;}
     public int sub(int firstNumber, int secondNumber) {
