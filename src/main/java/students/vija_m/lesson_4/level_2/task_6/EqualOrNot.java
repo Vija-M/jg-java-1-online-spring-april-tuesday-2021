1d2ca41f389abc1823ed3d8916c29fe002aa124e
Vija-M <vija.riga@gmailcom>
2021-05-24 11:58:35 +0300

    Lesson 4, Level 2, Task 6

---
+++ src/main/java/students/vija_m/lesson_4/level_2/task_6/EqualOrNot.java
@@ -0,0 +1,18 @@
+package students.vija_m.lesson_4.level_2.task_6;
+import java.util.Scanner;
+public class EqualOrNot {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Пожалуйста, введите два целых числа от -2147483648 до 2147483647.");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        {
+            if (a == b) System.out.println("Введённые числа " + a + " равны между собой.");
+            else {
+                System.out.println("Число " + a + " не равно числу " + b);
+
+            }
+        }}
+
+}
