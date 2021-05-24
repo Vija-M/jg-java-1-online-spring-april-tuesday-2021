f09b6b2b2517baa78ee5c022c0b1f235574c45ac
AzrailXD <whiteus@inbox.lv>
2021-05-24 20:26:18 +0300

    Lesson 3 - Level 1 - Task 5

---
+++ src/main/java/students/vladimir_aboltins/lesson_3/level_1/task_5/Robot.java
@@ -0,0 +1,7 @@
+package students.vladimir_aboltins.lesson_3.level_1.task_5;
+
+class Robot {
+    public void sayYourName() {
+        System.out.println("My name is ROBO");// тут напишите команду распечатки на консоли строки "My name is ROBO"
+    }
+}
---
+++ src/main/java/students/vladimir_aboltins/lesson_3/level_1/task_5/RobotDemo.java
@@ -0,0 +1,8 @@
+package students.vladimir_aboltins.lesson_3.level_1.task_5;
+
+class RobotDemo {
+    public static void main(String[] args) {
+        Robot robotOne = new Robot();
+        robotOne.sayYourName();
+    }
+}
