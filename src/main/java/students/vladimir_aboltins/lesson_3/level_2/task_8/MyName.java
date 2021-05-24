378611cac48904321825c75f0341a9d798838892
AzrailXD <whiteus@inbox.lv>
2021-05-24 21:44:55 +0300

    Lesson 3 - Level 2 - Task 8

---
+++ src/main/java/students/vladimir_aboltins/lesson_3/level_2/task_8/MyName.java
@@ -0,0 +1,8 @@
+package students.vladimir_aboltins.lesson_3.level_2.task_8;
+
+class MyName {
+    public static void main(String[] args) {
+        String myName="Vladimir";
+        System.out.println("Hello "+ myName+"!");
+    }
+}
