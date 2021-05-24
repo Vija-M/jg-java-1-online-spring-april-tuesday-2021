26deffcb220dd490473a25bcbd996bad22a07869
AzrailXD <whiteus@inbox.lv>
2021-05-24 21:39:56 +0300

    Lesson 3 - Level 2 - Task 7

---
+++ src/main/java/students/vladimir_aboltins/lesson_3/level_2/task_7/MyName.java
@@ -0,0 +1,9 @@
+package students.vladimir_aboltins.lesson_3.level_2.task_7;
+
+class MyName {
+
+    public static void main(String[] args) {
+        String myName = "Vladimir";
+        System.out.println(myName);
+    }
+}
