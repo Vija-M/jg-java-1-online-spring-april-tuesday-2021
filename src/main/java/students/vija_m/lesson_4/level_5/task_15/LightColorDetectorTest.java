6a09999bd1dee5e282cc03de55c3d6043a31e3d7
Vija-M <vija.riga@gmailcom>
2021-05-25 13:55:02 +0300

    Lesson 4, Level 5, Task 15

---
+++ src/main/java/students/vija_m/lesson_4/level_5/task_15/LightColorDetectorTest.java
@@ -0,0 +1,8 @@
+package students.vija_m.lesson_4.level_5.task_15;
+
+class LightColorDetectorTest {
+
+    public static void main(String[] args) {
+        LightColorDetectorTest lightColorDetectorTest  = new LightColorDetectorTest();
+        lightColorDetectorTest.violetTest();
+}
