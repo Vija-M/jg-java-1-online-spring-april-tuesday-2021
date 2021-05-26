7819ddf436d7bb26de251d391dced37926412a6c
Vija-M <vija.riga@gmailcom>
2021-05-26 23:08:00 +0300

    Lesson 4, Level 5, Task 15

---
+++ src/main/java/students/vija_m/lesson_4/level_6/task_16/Stock.java
@@ -0,0 +1,31 @@
+package students.vija_m.lesson_4.level_6.task_16;
+
+class Stock {
+    String companyName;
+    int currentPrice;
+    int minPrice;
+    int maxPrice;
+
+    public Stock(String companyName, int currentPrice) {
+        this.companyName = companyName;
+        this.currentPrice = currentPrice;
+        this.minPrice = currentPrice;
+        this.maxPrice = currentPrice;
+    }
+
+    void updatePrice(int newPrice){
+        if ( newPrice > currentPrice && newPrice > maxPrice) {
+            this.maxPrice = newPrice;
+        }
+        else if ( newPrice < currentPrice && newPrice < minPrice) {
+            this.minPrice = newPrice;
+        }
+
+                this.currentPrice = newPrice;
+        }
+
+String getPriceInformation() {
+return "Company = " + companyName + " , Current Price = " + currentPrice + " , Min Price = " + minPrice + ", Max Price = " + maxPrice;
+    }
+
+}
---
+++ src/main/java/students/vija_m/lesson_4/level_6/task_16/StockTest.java
@@ -0,0 +1,17 @@
+package students.vija_m.lesson_4.level_6.task_16;
+
+class StockTest {
+    public static void main(String[] args) {
+        StockTest stockTest = new StockTest();
+        stockTest.test1();
+    }
+
+
+
+    void test1(){
+        Stock google = new Stock("GOOG", 10);
+
+
+
+    }
+}
