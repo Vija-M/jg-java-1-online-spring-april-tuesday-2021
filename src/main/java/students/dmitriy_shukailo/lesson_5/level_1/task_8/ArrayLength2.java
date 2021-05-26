1c21fbbffd89b4d22d7672311b149dc6e0740e17
dk-t <shukailo.d@gmail.com>
2021-05-27 00:22:05 +0300

    Task 8 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_5/level_1/task_8/ArrayLength2.java
@@ -0,0 +1,25 @@
+package students.dmitriy_shukailo.lesson_5.level_1.task_8;
+
+class ArrayLength2 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+        //или
+
+        int [] number = new int[2];
+            number[0] = 1;
+            number[1] = 2;
+
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+    }
+
+}
