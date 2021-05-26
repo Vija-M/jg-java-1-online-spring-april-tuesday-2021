ed5326bf13187f675ad0a4847cd32b45087e4bea
Linda-Ju <linda.junkina@gmail.com>
2021-05-26 21:20:14 +0300

    Lesson 5, Level 1, Task 9

--- src/main/java/students/linda_junkina/lesson_5/level_1/task_9/ArrayLength3.java
+++ src/main/java/students/linda_junkina/lesson_5/level_1/task_9/ArrayLength3.java
@@ -1,12 +1,11 @@
 package students.linda_junkina.lesson_5.level_1.task_9;
 
+import java.util.Arrays;
+
 class ArrayLength3 {
     public static void main(String[] args) {
-        int[] numbers = new int[2];
-        numbers[0] = 10;
-        numbers[1] = 20;
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+        int[] numbers = {10, 20};
+       System.out.println(Arrays.toString(numbers));
         }
     }
-}
+
