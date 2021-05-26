b9d605bb31d9040212e4f07d4158815eeb2ed35c
Linda-Ju <linda.junkina@gmail.com>
2021-05-26 15:15:39 +0300

    Lesson 5, Level 1, Task 1

---
+++ src/main/java/students/linda_junkina/lsson_5/level_1/task_1/ArrayDefinition1.java
@@ -0,0 +1,11 @@
+package students.linda_junkina.lsson_5.level_1.task_1;
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        numbers [0] = 1;
+        numbers [1] = 5;
+        numbers [2] = 8;
+    }
+}
