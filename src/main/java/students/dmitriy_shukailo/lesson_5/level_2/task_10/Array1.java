86b3ef3eeef35006db11eb1ce59343f5f05bf1b8
dk-t <shukailo.d@gmail.com>
2021-05-27 21:39:34 +0300

    Task 10 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_5/level_2/task_10/Array1.java
@@ -0,0 +1,14 @@
+package students.dmitriy_shukailo.lesson_5.level_2.task_10;
+
+class Array1 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 3, 5};
+
+        System.out.println("numbers [0] = " + numbers[0]);
+        System.out.println("numbers [1] = " + numbers[1]);
+        System.out.println("numbers [2] = " + numbers[2]);
+    }
+
+}
