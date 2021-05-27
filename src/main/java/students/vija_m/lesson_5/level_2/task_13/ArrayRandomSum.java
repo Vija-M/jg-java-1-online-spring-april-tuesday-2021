14577eb179d84d289a217431fad45acdc822f298
Vija-M <vija.riga@gmailcom>
2021-05-27 20:11:30 +0300

    Lesson 5, Level 2, Task 13

---
+++ src/main/java/students/vija_m/lesson_5/level_2/task_13/ArrayRandomSum.java
@@ -0,0 +1,14 @@
+package students.vija_m.lesson_5.level_2.task_13;
+import java.util.Random;
+class ArrayRandomSum {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        Random random = new Random();
+        numbers[0] = random.nextInt();
+        numbers[1] = random.nextInt();
+        numbers[2] = random.nextInt();
+        System.out.println(numbers[0] + " + " + numbers[1] + " + " + numbers[0] + " = " + (numbers[0]+numbers[1]+numbers[2]));
+    }
+
+
+}
