aa58edf22195871bc9ccef9ac4efc80e3b0b4a69
Vija-M <vija.riga@gmailcom>
2021-05-27 21:13:15 +0300

    Lesson 5, Level 2, Task 15

---
+++ src/main/java/students/vija_m/lesson_5/level_2/task_15/ArrayPlusTwo.java
@@ -0,0 +1,22 @@
+package students.vija_m.lesson_5.level_2.task_15;
+import java.util.Random;
+class ArrayPlusTwo {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        Random random = new Random();
+        numbers[0] = random.nextInt();
+        numbers[1] = random.nextInt();
+        numbers[2] = random.nextInt();
+        System.out.println("1st number = " + numbers[0]);
+        System.out.println("2nd number = " + numbers[1]);
+        System.out.println("3rd number = " + numbers[2]);
+        System.out.println();
+
+        int a = numbers[0] + 2;
+        int b = numbers[1] + 2;
+        int c = numbers[2] + 2;
+        System.out.println(numbers[0] + " + 2 = " + a);
+        System.out.println(numbers[1] + " + 2 = "  + b);
+        System.out.println(numbers[2] + " + 2 = "  + c);
+    }
+}
