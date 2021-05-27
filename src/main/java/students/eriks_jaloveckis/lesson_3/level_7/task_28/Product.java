17bfb01c46a66236b1cd03627800253ef3e1169e
eriks <erikslv88@gmail.com>
2021-05-27 20:52:09 +0300

    Finish - 3 lesson 7 level 28 task

--- src/main/java/students/eriks_jaloveckis/lesson_3/level_7/task_28/Product.java
+++ src/main/java/students/eriks_jaloveckis/lesson_3/level_7/task_28/Product.java
@@ -2,20 +2,20 @@ package students.eriks_jaloveckis.lesson_3.level_7.task_28;
 
 class Product {
     String name;
-    double regularPrice = 990.99;
-    double discount = 50.00;
+    double regularPrice;
+    double discount;
 
-    Product(String name){
+
+    Product(String name) {
         this.name = name;
     }
 
 
-
-    double actualPrice(){
+    double actualPrice() {
         return this.regularPrice - this.regularPrice * (this.discount / 100);
     }
 
-    void printInf0(){
+    void printInfo() {
         System.out.println("Product`s name is          - " + name);
         System.out.println("Product`s regular price is - " + regularPrice);
         System.out.println("Product`s discount is      - " + discount);
