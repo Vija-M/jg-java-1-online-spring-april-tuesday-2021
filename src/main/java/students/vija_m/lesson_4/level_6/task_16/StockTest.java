97d3e50b5685b025d6da6f9d75cb22b0f60cbb21
Vija-M <vija.riga@gmailcom>
2021-05-27 13:46:19 +0300

    Lesson 4, Level 56, Task 16

--- src/main/java/students/vija_m/lesson_4/level_6/task_16/StockTest.java
+++ src/main/java/students/vija_m/lesson_4/level_6/task_16/StockTest.java
@@ -10,6 +10,21 @@ class StockTest {
 
     void test1(){
         Stock google = new Stock("GOOG", 10);
+        String priceInformation = google.getPriceInformation();
+        System.out.println("Start values:");
+        System.out.println(priceInformation);
+        google.updatePrice(12);
+        google.updatePrice(5);
+        google.updatePrice(7);
+        google.updatePrice(99);
+        google.updatePrice(77);
+
+        priceInformation = google.getPriceInformation();
+        System.out.println("Current values:");
+        System.out.println(priceInformation);
+
+
+
 
 
 
