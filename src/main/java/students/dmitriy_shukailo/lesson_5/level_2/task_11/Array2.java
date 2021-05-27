a6a701b7d1d1093d365922a248c6405c2e82607f
dk-t <shukailo.d@gmail.com>
2021-05-27 21:48:27 +0300

    Task 11 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_5/level_2/task_11/Array2.java
@@ -0,0 +1,21 @@
+package students.dmitriy_shukailo.lesson_5.level_2.task_11;
+
+class Array2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please, enter 3 numbers: ");
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        int[] numbers = {a, b, c};
+
+        System.out.println("numbers [0] = " + numbers[0]);
+        System.out.println("numbers [1] = " + numbers[1]);
+        System.out.println("numbers [2] = " + numbers[2]);
+    }
+
+}
