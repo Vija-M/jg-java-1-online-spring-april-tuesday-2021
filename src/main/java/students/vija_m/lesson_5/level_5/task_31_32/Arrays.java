2dd4e90177d6ba5bc76a3df6d601d6b011762b9a
Vija-M <vija.riga@gmailcom>
2021-05-28 23:13:39 +0300

    Lesson 5, Level 4 Task 31,32

--- src/main/java/students/vija_m/lesson_5/level_5/task_31_32/Arrays.java
+++ src/main/java/students/vija_m/lesson_5/level_5/task_31_32/Arrays.java
@@ -1,17 +1,16 @@
 package students.vija_m.lesson_5.level_5.task_31_32;
-import java.util.Random;
+
 class Arrays {
-    int generate(int length) {
-        Random random = new Random();
-        int[] numbers = new int[length];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = random.nextInt();
-        }
-
-        return length;
+    int getArrayLength() {
+        return arrayLength
     }
 
-    public void printInfo() { System.out.println("Arrays.toString(numbers) = " + java.util.Arrays.toString(numbers));
+                ;   ;
+            int[] numbers = new int[arrayLength];
+            this
+
+    public int getArrayLength() {
 
     }
-}
+}}
+
--- src/main/java/students/vija_m/lesson_5/level_5/task_31_32/ArraysTest.java
+++ src/main/java/students/vija_m/lesson_5/level_5/task_31_32/ArraysTest.java
@@ -2,8 +2,7 @@ package students.vija_m.lesson_5.level_5.task_31_32;
 
 class ArraysTest {
     public static void main(String[] args) {
-        Arrays array = new Arrays();
-        System.out.println(array.generate(25));
-        array.printInfo();
+        Arrays array = new Arrays(arrayLength);
+
     }
 }
