7a4fa7d0ba6eed34f6212fb6dad0080f82ecbfcb
Vija-M <vija.riga@gmailcom>
2021-05-28 18:51:43 +0300

    Lesson 5, Level 4 Task 28

---
+++ src/main/java/students/vija_m/lesson_5/level_4/task_28/Task28.java
@@ -0,0 +1,19 @@
+package students.vija_m.lesson_5.level_4.task_28;
+
+import java.util.Random;
+import java.util.Arrays;
+class Task28 {
+    public static void main(String[] args) {
+       int[] numbers = new int[7];
+    Random random = new Random();
+    for (int i = 0; i < numbers.length; i++ );
+        numbers[i] = random.nextInt();}
+  System out
+
+
+}
+
+
+
+
+}
