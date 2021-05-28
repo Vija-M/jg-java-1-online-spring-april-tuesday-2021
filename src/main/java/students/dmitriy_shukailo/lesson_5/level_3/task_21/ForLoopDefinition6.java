35f06330c16132432d6f3c5f221159ac19271f51
dk-t <shukailo.d@gmail.com>
2021-05-28 22:58:17 +0300

    Tasks 21 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_5/level_3/task_21/ForLoopDefinition6.java
@@ -0,0 +1,11 @@
+package students.dmitriy_shukailo.lesson_5.level_3.task_21;
+
+class ForLoopDefinition6 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
