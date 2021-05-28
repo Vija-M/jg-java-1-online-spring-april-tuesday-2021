df9459b0c17a689aadab6cff7df03f34ee4bf973
Vija-M <vija.riga@gmailcom>
2021-05-28 14:18:53 +0300

    Lesson 5, Level 3, Task 25

---
+++ src/main/java/students/vija_m/lesson_5/level_4/task_25/Task25.java
@@ -0,0 +1,14 @@
+package students.vija_m.lesson_5.level_4.task_25;
+import java.util.Scanner;
+class Task25 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter the length of our array.");
+        int[] numbers  = new int[i];
+        i = scanner.nextInt();
+    }
+
+
+
+
+}
