6d058b7db62b73ad13a6cf35fe2dfbb3c68ba8eb
DeTELo <mrdinas@gmail.com>
2021-05-28 23:39:27 +0300

    Lesson 4 Task-18 - LEVEL7 - FINISH

--- src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_7/task_18/LeapYearTest.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_7/task_18/LeapYearTest.java
@@ -6,19 +6,36 @@ class LeapYearTest {
 
         LeapYearTest leapYearTest = new LeapYearTest();
 
+        leapYearTest.yearTestOne();
+        leapYearTest.yearTestTwo();
+        leapYearTest.yearTestThree();
+        leapYearTest.yearTestFour();
 
     }
 
-    void yearTestOne (){
-        yearTestFinal ("100", 1876, true);
+    void yearTestOne() {
+        yearTestFinal("100", 1876, true);
     }
-    void yearTestTwo (){
-        yearTestFinal ("4 и 400", 2500, true);
+
+    void yearTestTwo() {
+        yearTestFinal("4 и 400", 4000, true);
     }
-    void yearTestThree (){
-        yearTestFinal ("4", 1888, false);
+
+    void yearTestThree() {
+        yearTestFinal("4", 2999, false);
     }
-    void yearTestFour (){
-        yearTestFinal ("4, 100, 400", 1700, false);
+
+    void yearTestFour() {
+        yearTestFinal("4, 100, 400", 1700, false);
+    }
+
+    void yearTestFinal(String name, int year, boolean expectedResult) {
+        LeapYear leapYear = new LeapYear();
+        boolean realResult = leapYear.isLeapYear(year);
+        if (expectedResult == realResult) {
+            System.out.println(name + " = OK");
+        } else {
+            System.out.println(name + " = FAIL");
+        }
     }
 }
