92554967387ba836b1d5331cbd1d14b12965bfa8
DeTELo <mrdinas@gmail.com>
2021-05-28 23:28:40 +0300

    Lesson 4 Task-18 - LEVEL7

--- src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_7/task_18/LeapYearTest.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_7/task_18/LeapYearTest.java
@@ -1,4 +1,24 @@
 package students.dmitrijs_matrenicevs.lesson_4.level_7.task_18;
 
 class LeapYearTest {
+
+    public static void main(String[] args) {
+
+        LeapYearTest leapYearTest = new LeapYearTest();
+
+
+    }
+
+    void yearTestOne (){
+        yearTestFinal ("100", 1876, true);
+    }
+    void yearTestTwo (){
+        yearTestFinal ("4 и 400", 2500, true);
+    }
+    void yearTestThree (){
+        yearTestFinal ("4", 1888, false);
+    }
+    void yearTestFour (){
+        yearTestFinal ("4, 100, 400", 1700, false);
+    }
 }
