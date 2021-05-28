95761499d3284a297f71ee184dc0a7959e28c210
Vija-M <vija.riga@gmailcom>
2021-05-28 21:38:16 +0300

    Lesson 5, Level 4 Task 31,32

---
+++ src/main/java/students/vija_m/lesson_5/level_5/task_31/Arrays.java
@@ -0,0 +1,15 @@
+package students.vija_m.lesson_5.level_5.task_31;
+import java.util.Random;
+class Arrays {
+    int generate(int length) {
+        Random random = new Random();
+        int[] numbers = new int[length];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt();
+        }
+        void printInfo() {
+
+            System.out.println("Arrays.toString(numbers) = " + java.util.Arrays.toString(numbers));
+        }
+    }
+}
