75760dc100f596689754c1f0c3d0e4a8db9d69d6
Vija-M <vija.riga@gmailcom>
2021-05-28 19:58:55 +0300

    Lesson 5, Level 4 Task 28

--- src/main/java/students/vija_m/lesson_5/level_4/task_28/Task28.java
+++ src/main/java/students/vija_m/lesson_5/level_4/task_28/Task28.java
@@ -2,18 +2,24 @@ package students.vija_m.lesson_5.level_4.task_28;
 
 import java.util.Random;
 import java.util.Arrays;
+
 class Task28 {
     public static void main(String[] args) {
-       int[] numbers = new int[7];
-    Random random = new Random();
-    for (int i = 0; i < numbers.length; i++ );
-        numbers[i] = random.nextInt();}
-  System out
+        Random random = new Random();
 
+        int[] numbers = new int[7];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt();
+        }
+        System.out.println("Arrays.toString(numbers) = " + Arrays.toString(numbers));
 
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        System.out.println("min = " + min);
+    }
 }
 
-
-
-
-}
