8d89c31b45bea84ad132f29afe91074b26f18e18
DeTELo <mrdinas@gmail.com>
2021-05-28 23:17:35 +0300

    Lesson 4 Task-18 - LEVEL7

--- src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_7/task_18/LeapYear.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_7/task_18/LeapYear.java
@@ -4,15 +4,18 @@ class LeapYear {
 
     public boolean isLeapYear(int year) {
 
-        if (year % 4 != 0){
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 100 != 0) {
+            return true;
+        } else if (year % 400 == 0) {
+            return true;
+        } else {
             return false;
         }
-        else if (year % 100 != 0){
-            return true;
-        }
+
 
     }
-
 
 
 }
