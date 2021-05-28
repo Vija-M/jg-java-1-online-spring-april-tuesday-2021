85ac0acefb6e7775fc5be35d53c955263b55fb19
dk-t <shukailo.d@gmail.com>
2021-05-28 23:08:00 +0300

    Tasks 24 has been done

--- src/main/java/students/dmitriy_shukailo/lesson_5/level_3/task_20/ForLoopDefinition5.java
+++ src/main/java/students/dmitriy_shukailo/lesson_5/level_3/task_20/ForLoopDefinition5.java
@@ -4,7 +4,7 @@ class ForLoopDefinition5 {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
     }
