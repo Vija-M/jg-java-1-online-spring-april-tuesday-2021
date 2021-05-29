d609fbcbf372e8a9d91679ce24221cad366b82ae
DeTELo <mrdinas@gmail.com>
2021-05-29 22:36:16 +0300

    Lesson 4 Task-19 - LEVEL7 - FINISH

--- src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_7/task_19/CalculatorTest.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_4/level_7/task_19/CalculatorTest.java
@@ -15,60 +15,60 @@ public class CalculatorTest {
 
     public void sumTest() {
         Calculator calculator = new Calculator();
-        calculatorTest (calculator.sum(35,40),75);
+        calculatorTest (calculator.sum(35,40),75, "SUM");
     }
 
     public void subTest() {
 
         Calculator calculator = new Calculator();
-        calculatorTest (calculator.sub(35,15),20);
+        calculatorTest (calculator.sub(35,15),20, "SUB");
     }
 
     public void mulTest() {
 
         Calculator calculator = new Calculator();
-        calculatorTest (calculator.mul(5,5),25);
+        calculatorTest (calculator.mul(5,5),25, "MUL");
 
     }
 
     public void divTest() {
 
         Calculator calculator = new Calculator();
-        calculatorTest (calculator.div(40,8),5);
+        calculatorTest (calculator.div(40,8),5, "DIV");
 
     }
 
     public void isEvenTest1() {
         Calculator calculator = new Calculator();
-        calculatorTestTwo (calculator.isEven(40), true);
+        calculatorTestTwo (calculator.isEven(40), true, "IsEVEN1");
 
     }
 
 
     public void isEvenTest2() {
         Calculator calculator = new Calculator();
-        calculatorTestTwo (calculator.isEven(41),false);
+        calculatorTestTwo (calculator.isEven(41),false, "IsEVEN2");
 
     }
 
-    void calculatorTest (int expectedResult, int realResult){
+    void calculatorTest (int expectedResult, int realResult, String name){
 
 
         if (expectedResult == realResult){
-            System.out.println("TEST = " + "OK");
+            System.out.println(name + " = OK");
         }
         else {
-            System.out.println("TEST = " + "FAIL");
+            System.out.println(name + " = FAIL");
         }
     }
 
-    void  calculatorTestTwo (boolean expectedResult, boolean realResult){
+    void  calculatorTestTwo (boolean expectedResult, boolean realResult, String name){
 
         if (expectedResult == realResult){
-            System.out.println("TEST = " + "OK");
+            System.out.println(name  + " = OK");
         }
         else {
-            System.out.println("TEST = " + "FAIL");
+            System.out.println(name + " = FAIL");
         }
     }
 
