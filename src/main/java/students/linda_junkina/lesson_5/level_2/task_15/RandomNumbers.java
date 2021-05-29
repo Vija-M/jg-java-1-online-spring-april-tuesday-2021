1a2a869e5dd61c530a9b4f1f8765fc0890e275bc
Linda-Ju <linda.junkina@gmail.com>
2021-05-30 00:41:02 +0300

    Lesson 5, Level 2, Task 15

---
+++ src/main/java/students/linda_junkina/lesson_5/level_2/task_15/RandomNumbers.java
@@ -0,0 +1,26 @@
+package students.linda_junkina.lesson_5.level_2.task_15;
+
+import java.util.Random;
+
+class RandomNumbers {
+    void showNumbers(int length) {
+
+        Random random = new Random();
+
+        int[] numbers = new int[length];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (random.nextInt(10));
+            System.out.println(numbers[i]);
+        }
+        System.out.println();
+
+        int aPlusTwo = numbers[0] + 2;
+        System.out.println(aPlusTwo);
+        int bPlusTwo = numbers[1] + 2;
+        System.out.println(bPlusTwo);
+        int cPlusTwo = numbers[2] + 2;
+        System.out.println(cPlusTwo);
+
+
+    }
+}
