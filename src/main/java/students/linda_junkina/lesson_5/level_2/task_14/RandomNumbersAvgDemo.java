8d6c197904f7c050202f3cbd755100f6274678a6
Linda-Ju <linda.junkina@gmail.com>
2021-05-30 00:12:06 +0300

    Lesson 5, Level 2, Task 14

---
+++ src/main/java/students/linda_junkina/lesson_5/level_2/task_14/RandomNumbersAvgDemo.java
@@ -0,0 +1,8 @@
+package students.linda_junkina.lesson_5.level_2.task_14;
+
+class RandomNumbersAvgDemo {
+    public static void main(String[] args) {
+        RandomNumbersAvg randomNumbersAvg = new RandomNumbersAvg();
+        randomNumbersAvg.showNumbers(3);
+    }
+}
