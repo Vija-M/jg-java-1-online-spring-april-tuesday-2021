c213bb59801253b78bb28bb6909bab61c359e5f6
Ernests1992 <ernests@neolain.lv>
2021-05-30 17:54:23 +0300

    Lesson 5 - Level 7 - Task 41

---
+++ src/main/java/students/ernests_subhankulovs/lesson_5/level_7/task_41/Test.java
@@ -0,0 +1,11 @@
+package students.ernests_subhankulovs.lesson_5.level_7.task_41;
+
+class Test {
+    public static void main(String[] args) {
+        int[] array = {51,136,387};
+
+        for (int i:array) {
+            System.out.println(i);
+        }
+    }
+}
