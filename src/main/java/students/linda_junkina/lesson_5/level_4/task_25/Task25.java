50de3d2c2895be944d0fa807f66af782af982d17
Linda-Ju <linda.junkina@gmail.com>
2021-05-30 22:29:26 +0300

    Lesson 5, Level 4, Task 25

---
+++ src/main/java/students/linda_junkina/lesson_5/level_4/task_25/Task25.java
@@ -0,0 +1,27 @@
+package students.linda_junkina.lesson_5.level_4.task_25;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Task25 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter array length: ");
+
+        int arrayLength = scanner.nextInt();
+
+        int[] array = new int[arrayLength];
+        System.out.println("Ok, array consists of " + arrayLength + " numbers");
+
+        System.out.print("Enter array numbers: ");
+
+        for (int i = 0; i < arrayLength; i++) {
+            array[i] = (scanner.nextInt());
+
+        }
+        System.out.println("This is your array: " + Arrays.toString(array));
+    }
+}
