b3315c75086a6608e0a0bb75f433fcdad44178e6
Linda-Ju <linda.junkina@gmail.com>
2021-05-31 00:12:45 +0300

    Lesson 5, Level 4, Task 28

---
+++ src/main/java/students/linda_junkina/lesson_5/level_4/task_28/Task28.java
@@ -0,0 +1,24 @@
+package students.linda_junkina.lesson_5.level_4.task_28;
+
+import java.util.Random;
+import java.util.Arrays;
+
+class Task28 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(50);
+        }
+        System.out.println("Array: " + Arrays.toString(array));
+
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.print("min number is " + min);
+    }
+}
+
