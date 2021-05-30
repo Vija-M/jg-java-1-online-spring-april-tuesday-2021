92dd792b76d410f048dfcb15ec43dc16676323bf
Linda-Ju <linda.junkina@gmail.com>
2021-05-30 18:49:25 +0300

    Lesson 5, Level 3, Task 21

--- src/main/java/students/linda_junkina/lesson_5/level_3/task_21/ForLoopDefinition5.java
+++ src/main/java/students/linda_junkina/lesson_5/level_3/task_21/ForLoopDefinition5.java
@@ -2,8 +2,8 @@ package students.linda_junkina.lesson_5.level_3.task_21;
 
 class ForLoopDefinition5 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             System.out.println(i);
+        }
     }
-}
 }
