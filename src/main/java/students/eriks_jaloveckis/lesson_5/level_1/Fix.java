23234acdcde6be624e525d8f31cd41daec4a6e98
eriks <erikslv88@gmail.com>
2021-05-30 12:28:53 +0300

    Finish - 5 lesson 1 level

---
+++ src/main/java/students/eriks_jaloveckis/lesson_5/level_1/Fix.java
@@ -0,0 +1,78 @@
+package students.eriks_jaloveckis.lesson_5.level_1;
+
+class Fix {
+/*
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+
+}
+
+class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+
+}
+
+class ArrayDefinition3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+
+}
+
+class ArrayDefinition4 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+
+}
+
+class ArrayDefinition5 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+
+}
+
+class ArrayDefinition6 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+
+}
+
+class ArrayLength1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[1]);
+    }
+
+}
+
+public static void main(String[] args) {
+        int numbers = new int[2];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+    }
+
+}
+
+class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+}
+*/
+}
