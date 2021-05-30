e4f0a3f368575dea1d9a2d7d8596d496ecd39c1a
DeTELo <mrdinas@gmail.com>
2021-05-30 16:56:41 +0300

    Lesson 5 Task-1 - LEVEL1 - FINISH

--- src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_1/task_1/ArrayDefinition1.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_1/task_1/ArrayDefinition1.java
@@ -18,16 +18,16 @@ class ArrayDefinition1 {
         numbers[9] = 23;
 
 
-        System.out.println("numbers [0] = " + numbers [0]);
-        System.out.println("numbers [1] = " + numbers [1]);
-        System.out.println("numbers [2] = " + numbers [2]);
-        System.out.println("numbers [3] = " + numbers [3]);
-        System.out.println("numbers [4] = " + numbers [4]);
-        System.out.println("numbers [5] = " + numbers [5]);
-        System.out.println("numbers [6] = " + numbers [6]);
-        System.out.println("numbers [7] = " + numbers [7]);
-        System.out.println("numbers [8] = " + numbers [8]);
-        System.out.println("numbers [9] = " + numbers [9]);
+        System.out.println("numbers [0] = " + numbers[0]);
+        System.out.println("numbers [1] = " + numbers[1]);
+        System.out.println("numbers [2] = " + numbers[2]);
+        System.out.println("numbers [3] = " + numbers[3]);
+        System.out.println("numbers [4] = " + numbers[4]);
+        System.out.println("numbers [5] = " + numbers[5]);
+        System.out.println("numbers [6] = " + numbers[6]);
+        System.out.println("numbers [7] = " + numbers[7]);
+        System.out.println("numbers [8] = " + numbers[8]);
+        System.out.println("numbers [9] = " + numbers[9]);
 
     }
-    }
+}
