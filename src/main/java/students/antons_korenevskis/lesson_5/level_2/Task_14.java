ece5615b9259fa668c1832ed75f75e6217091eb5
neontone <antons.korenevskis@gmail.com>
2021-05-30 11:21:44 +0300

    Done

---
+++ src/main/java/students/antons_korenevskis/lesson_5/level_2/Task_14.java
@@ -0,0 +1,28 @@
+package students.antons_korenevskis.lesson_5.level_2;
+
+import java.util.Random;
+
+public class Task_14 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int[] numbers = new int[3];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(101);
+        }
+
+        int sum = numbers[0] + numbers[1] + numbers[2];
+        double avg = (double) sum / numbers.length;
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+
+        System.out.println("avg = " + avg);
+
+    }
+
+
+}
