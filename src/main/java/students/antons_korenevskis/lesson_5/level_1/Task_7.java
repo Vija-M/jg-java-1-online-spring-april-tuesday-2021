a9e9b08a6a50631fa96db765edc475d73a029b1f
neontone <antons.korenevskis@gmail.com>
2021-05-30 14:26:53 +0300

    Done

--- src/main/java/students/antons_korenevskis/lesson_5/level_1/Task_7.java
+++ src/main/java/students/antons_korenevskis/lesson_5/level_1/Task_7.java
@@ -4,7 +4,7 @@ class ArrayLength1 {
 
     public static void main(String[] args) {
         int[] numbers = new int[2];
-        System.out.println(numbers[-1]);
+        System.out.println(numbers[1]);
     }
 
 }
