6a3a6235f7ecc37437f2a013e12520f3e11c0296
neontone <antons.korenevskis@gmail.com>
2021-05-30 12:51:48 +0300

    Done

---
+++ src/main/java/students/antons_korenevskis/lesson_5/level_3/Task_24.java
@@ -0,0 +1,11 @@
+package students.antons_korenevskis.lesson_5.level_3;
+
+class ForLoopDefinition9 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
