1dd968bd2e2d9219cac101afd9516180e591a07f
Ernests1992 <ernests@neolain.lv>
2021-05-30 15:21:22 +0300

    Lesson 5 - Level 4 - Task 29

--- src/main/java/students/ernests_subhankulovs/lesson_5/level_4/task_29/Task29.java
+++ src/main/java/students/ernests_subhankulovs/lesson_5/level_4/task_29/Task29.java
@@ -18,7 +18,6 @@ class Task29 {
         System.out.println();
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(100);
-            //System.out.println("i = " + i + "; array[i] = " + array[i]);
             if (array[i] % 2 == 0) {
                 System.out.println("i = " + i + "; array[i] = " + array[i] + " is an even number.");
             }
