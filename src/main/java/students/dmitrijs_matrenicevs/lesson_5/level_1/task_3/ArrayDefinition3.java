f02338db953f1671ca205ae58d5e88e4c6167ed7
DeTELo <mrdinas@gmail.com>
2021-05-30 16:51:01 +0300

    Lesson 5 Task-3 - LEVEL1 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_1/task_3/ArrayDefinition3.java
@@ -0,0 +1,10 @@
+package students.dmitrijs_matrenicevs.lesson_5.level_1.task_3;
+
+class ArrayDefinition3 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[10];
+    }
+
+
+}
