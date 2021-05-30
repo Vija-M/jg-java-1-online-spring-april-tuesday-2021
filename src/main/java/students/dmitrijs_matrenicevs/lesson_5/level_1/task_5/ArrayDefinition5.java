7f7e0948fcfa4ca91fbd886242c0f9802e9323ce
DeTELo <mrdinas@gmail.com>
2021-05-30 16:54:03 +0300

    Lesson 5 Task-5 - LEVEL1 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_1/task_5/ArrayDefinition5.java
@@ -0,0 +1,10 @@
+package students.dmitrijs_matrenicevs.lesson_5.level_1.task_5;
+
+class ArrayDefinition5 {
+
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3, 4, 5};
+    }
+
+
+}
