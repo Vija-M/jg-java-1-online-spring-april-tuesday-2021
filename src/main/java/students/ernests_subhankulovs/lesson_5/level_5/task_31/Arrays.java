293e25b8cfc9051091c5576358e3353c44e9d0ae
Ernests1992 <ernests@neolain.lv>
2021-05-30 15:33:59 +0300

    Lesson 5 - Level 5 - Task 31

---
+++ src/main/java/students/ernests_subhankulovs/lesson_5/level_5/task_31/Arrays.java
@@ -0,0 +1,10 @@
+package students.ernests_subhankulovs.lesson_5.level_5.task_31;
+
+class Arrays {
+
+    public int[] create(int length) {
+        int[] array = new int[length];
+        return array;
+    }
+
+}
