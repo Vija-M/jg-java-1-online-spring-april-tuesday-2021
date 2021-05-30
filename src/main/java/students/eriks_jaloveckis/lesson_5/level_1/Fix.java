2bffee8b8484b06586cbc9fe0a692c752eb86a44
eriks <erikslv88@gmail.com>
2021-05-30 12:30:10 +0300

    Finish - 5 lesson 1 level

--- src/main/java/students/eriks_jaloveckis/lesson_5/level_1/Fix.java
+++ src/main/java/students/eriks_jaloveckis/lesson_5/level_1/Fix.java
@@ -60,7 +60,7 @@ class ArrayLength1 {
 }
 
 public static void main(String[] args) {
-        int numbers = new int[2];
+        int[] numbers = new int[2];
         System.out.println(numbers[0]);
         System.out.println(numbers[1]);
     }
