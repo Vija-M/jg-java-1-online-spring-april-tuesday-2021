8a21a7e37a61513b53b5ff4902363ae9c575ea10
DeTELo <mrdinas@gmail.com>
2021-05-30 17:01:01 +0300

    Lesson 5 Task-1 - LEVEL1 - FINISH

--- src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_1/task_1/ArrayDefinition1.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_1/task_1/ArrayDefinition1.java
@@ -5,29 +5,5 @@ class ArrayDefinition1 {
     public static void main(String[] args) {
         int[] numbers = new int[10];
 
-
-        numbers[0] = 11;
-        numbers[1] = 15;
-        numbers[2] = 16;
-        numbers[3] = 17;
-        numbers[4] = 18;
-        numbers[5] = 19;
-        numbers[6] = 20;
-        numbers[7] = 21;
-        numbers[8] = 22;
-        numbers[9] = 23;
-
-
-        System.out.println("numbers [0] = " + numbers[0]);
-        System.out.println("numbers [1] = " + numbers[1]);
-        System.out.println("numbers [2] = " + numbers[2]);
-        System.out.println("numbers [3] = " + numbers[3]);
-        System.out.println("numbers [4] = " + numbers[4]);
-        System.out.println("numbers [5] = " + numbers[5]);
-        System.out.println("numbers [6] = " + numbers[6]);
-        System.out.println("numbers [7] = " + numbers[7]);
-        System.out.println("numbers [8] = " + numbers[8]);
-        System.out.println("numbers [9] = " + numbers[9]);
-
     }
 }
