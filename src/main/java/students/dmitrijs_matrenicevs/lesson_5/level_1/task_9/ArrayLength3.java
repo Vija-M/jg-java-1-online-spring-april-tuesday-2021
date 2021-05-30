f3d3fb65c4f8092edc58aca39832daba7887522c
DeTELo <mrdinas@gmail.com>
2021-05-30 17:20:57 +0300

    Lesson 5 Task-9 - LEVEL1 - FINISH

--- src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_1/task_9/ArrayLength3.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_1/task_9/ArrayLength3.java
@@ -4,7 +4,7 @@ class ArrayLength3 {
 
 
     public static void main(String[] args) {
-        int [] numbers = new int[2];
+        int[] numbers = new int[2];
 
         System.out.println(numbers.length);
     }
