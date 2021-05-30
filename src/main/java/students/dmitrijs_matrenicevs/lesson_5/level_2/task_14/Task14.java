8a3dba499ad920cd663b0bd6e1aec7b5be84a2c5
DeTELo <mrdinas@gmail.com>
2021-05-30 18:35:54 +0300

    Lesson 5 Task-14 - LEVEL2 - FINISH

--- src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_2/task_14/Task14.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_2/task_14/Task14.java
@@ -17,6 +17,25 @@ class Task14 {
             middle += numbers[i];
 
         }
+
+        double avg = (double) middle  / numbers.length;
+
+        for (int i = 0; i < numbers.length; i++){
+
+            int min = numbers[0];
+
+            if (numbers[i] < min);
+            min = numbers[i];
+        }
+
+        System.out.println();
+        System.out.println("Общая сумма всех чисел = " + middle);
+        System.out.println();
+        System.out.println("Сумма деления = " + middle / numbers.length);
+        System.out.println();
+        System.out.println("Double = " + avg);
+
+
     }
 
 
