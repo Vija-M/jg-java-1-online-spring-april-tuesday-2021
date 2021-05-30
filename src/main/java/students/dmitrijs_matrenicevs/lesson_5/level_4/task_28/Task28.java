82b3ff71356c2a18b13d944e41931b51431d8285
DeTELo <mrdinas@gmail.com>
2021-05-30 20:41:19 +0300

    Lesson 5 Task-28 - LEVEL4 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_4/task_28/Task28.java
@@ -0,0 +1,31 @@
+package students.dmitrijs_matrenicevs.lesson_5.level_4.task_28;
+
+import java.util.Random;
+
+class Task28 {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(50);
+            System.out.println((i + 1) + " = " + numbers[i]);
+        }
+
+
+        int numberSmall = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < numberSmall) {
+                numberSmall = numbers[i];
+            }
+
+        }
+        System.out.println();
+        System.out.println("Наименьшее число: " + numberSmall);
+
+
+    }
+
+}
