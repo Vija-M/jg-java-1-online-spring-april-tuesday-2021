5178a40f1553453b64b8c358641158626909230c
DeTELo <mrdinas@gmail.com>
2021-05-30 19:17:12 +0300

    Lesson 5 Task-24 - LEVEL3 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_3/task_24/ForLoopDefinition8.java
@@ -0,0 +1,12 @@
+package students.dmitrijs_matrenicevs.lesson_5.level_3.task_24;
+
+class ForLoopDefinition8 {
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 101; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
