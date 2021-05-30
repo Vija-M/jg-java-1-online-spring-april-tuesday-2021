1af145d0fe4ec5e030401a3f824ed87f218566b7
DeTELo <mrdinas@gmail.com>
2021-05-30 17:31:25 +0300

    Lesson 5 Task-10 - LEVEL2 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_2/task_10/Task10.java
@@ -0,0 +1,23 @@
+package students.dmitrijs_matrenicevs.lesson_5.level_2.task_10;
+
+class Task10 {
+
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+
+        numbers[0] = 15;
+        numbers[1] = 16;
+        numbers[2] = 17;
+
+
+        System.out.println("numbers[0] = " + numbers[0]);
+        System.out.println("numbers[1] = " + numbers[1]);
+        System.out.println("numbers[2] = " + numbers[2]);
+
+
+    }
+
+
+}
