6bb30c1751c5997095030c68a0805f07d738f53b
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-31 08:30:03 +0300

    Completed   Lesson 5 Level 1 Task 1

--- src/main/java/students/arturs_arutjunovs/lesson_5/level_1/task_1/ArrayDefinition1.java
+++ src/main/java/students/arturs_arutjunovs/lesson_5/level_1/task_1/ArrayDefinition1.java
@@ -1,6 +1,6 @@
 package students.arturs_arutjunovs.lesson_5.level_1.task_1;
 
-public class ArrayDefinition1 {
+ class ArrayDefinition1 {
     public static void main(String[] args) {
 
         int[] numbers = new int[3];  //3 - размер массива, как пример
