215a1d870c88f134140160081138e7858a51a488
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-05-31 10:49:12 +0300

    Completed   Lesson 5 Level 2 Task 10

---
+++ src/main/java/students/arturs_arutjunovs/lesson_5/level_2/task_10/Task_10.java
@@ -0,0 +1,17 @@
+package students.arturs_arutjunovs.lesson_5.level_2.task_10;
+
+class Task_10 {
+    public static void main(String[] args) {
+
+        int[] num = new int[3];
+        num[0] = 1;
+        num[1] = 2;
+        num[2] = 3;
+
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+
+
+    }
+}
