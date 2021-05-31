4ba2958e12a10bc0ab4561838341d8de44d5c70b
Daniils Buts <daniilb@outlook.com>
2021-05-31 19:45:21 +0300

    Revert changes to an unrelated file

--- src/main/java/students/jevgenij_leshkevich/lesson_2/level_2/task_7/Task7.java
+++ src/main/java/students/jevgenij_leshkevich/lesson_2/level_2/task_7/Task7.java
@@ -4,7 +4,7 @@ class Task7 {
 
     static class LoginService {
 
-        public static void main(String[] args){
+        public static void main(String[] args) {
             System.out.println("You can login!");
         }
 
