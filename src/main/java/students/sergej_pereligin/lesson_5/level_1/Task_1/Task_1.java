c43b0e52adb277f6218266fb5c269630d4e5523f
zilite <pepellv@inbox.lv>
2021-05-31 17:37:47 +0300

    lesson-5/level_1/Task_1

---
+++ src/main/java/students/sergej_pereligin/lesson_5/level_1/Task_1/Task_1.java
@@ -0,0 +1,9 @@
+package students.sergej_pereligin.lesson_5.level_1.Task_1;
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+    }
+
+}
