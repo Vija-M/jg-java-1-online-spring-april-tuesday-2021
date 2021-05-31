440df0bbdb710604c856c10103d13768af087aaf
zilite <pepellv@inbox.lv>
2021-05-31 22:20:57 +0300

    lesson-5/level_2/Task_1

---
+++ src/main/java/students/sergej_pereligin/lesson_5/level_2/task_1/Task_1.java
@@ -0,0 +1,10 @@
+package students.sergej_pereligin.lesson_5.level_2.task_1;
+
+class Task1 {
+    public static void main(String[] args) {
+        int[] numbers ={1,2,3,1};
+        for (int i = 0; i < 4; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
