05966200a0ccff4bb2cbdd798e7eeddf0a819656
DeTELo <mrdinas@gmail.com>
2021-05-31 23:13:26 +0300

    Lesson 5 Task-14 - LEVEL2 - FINISH - FIXED

--- src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_2/task_14/Task14.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_2/task_14/Task14.java
@@ -7,31 +7,24 @@ class Task14 {
     public static void main(String[] args) {
 
         int[] numbers = new int[3];
-        int middle = 5;
+        int sum = 0;
 
         Random random = new Random();
 
         for (int i = 0; i < numbers.length; i++){
             numbers[i] = random.nextInt(50);
             System.out.println(i + " = " + numbers[i]);
-            middle += numbers[i];
+            sum += numbers[i];
 
         }
 
-        double avg = (double) middle  / numbers.length;
+        double avg = (double) sum  / numbers.length;
 
-        for (int i = 0; i < numbers.length; i++){
-
-            int min = numbers[0];
-
-            if (numbers[i] < min);
-            min = numbers[i];
-        }
 
         System.out.println();
-        System.out.println("Общая сумма всех чисел = " + middle);
+        System.out.println("Общая сумма всех чисел = " + sum);
         System.out.println();
-        System.out.println("Сумма деления = " + middle / numbers.length);
+        System.out.println("Среднее арифметическое = " + sum / numbers.length);
         System.out.println();
         System.out.println("Double = " + avg);
 
