d7ec6493dad6eb9e22d2fe134e702a3286c24de3
zilite <pepellv@inbox.lv>
2021-05-31 18:39:16 +0300

    lesson-5/level_1/Task_2

--- src/main/java/students/sergej_pereligin/lesson_5/task_2/Task_2.java
+++ src/main/java/students/sergej_pereligin/lesson_5/task_2/Task_2.java
@@ -1,10 +1,23 @@
 package students.sergej_pereligin.lesson_5.task_2;
 
 
+import java.util.Arrays;
+
 class ArrayDefinition2 {
 
     public static void main(String[] args) {
-        byte[] numbers = new byte[10];
+        int[] numbers = new int[9];
+         numbers[0]=1;
+        numbers[1]=1;
+        numbers[2]=1;
+        numbers[3]=1;
+        numbers[4]=1;
+        numbers[5]=1;
+        numbers[6]=1;
+        numbers[7]=1;
+        numbers[8]=1;
+
+        System.out.println(Arrays.toString(numbers));
     }
 
 }
