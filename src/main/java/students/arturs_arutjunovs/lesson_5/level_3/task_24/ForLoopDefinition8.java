74de7083101645fe7f69100cff5c445088386b79
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-06-01 09:52:45 +0300

    Completed   Lesson 5 Level 3 Task 24

---
+++ src/main/java/students/arturs_arutjunovs/lesson_5/level_3/task_24/ForLoopDefinition8.java
@@ -0,0 +1,10 @@
+package students.arturs_arutjunovs.lesson_5.level_3.task_24;
+
+class ForLoopDefinition8 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
