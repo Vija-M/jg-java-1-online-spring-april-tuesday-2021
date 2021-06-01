f185028e9443734c57df9fa401dffadcf06a8edb
Linda-Ju <linda.junkina@gmail.com>
2021-06-01 13:01:26 +0300

    Lesson 3, Level 7, Task 27

---
+++ src/main/java/students/linda_junkina/lesson_3/level_7/task_27/CircleDemo.java
@@ -0,0 +1,10 @@
+package students.linda_junkina.lesson_3.level_7.task_27;
+
+class CircleDemo {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.radius = 5;
+        System.out.println("Area = " + circle.calculateArea());
+
+    }
+}
