4cd65c891d4d8400ce8282d803c7477ff4a5c1dd
DeTELo <mrdinas@gmail.com>
2021-06-01 15:29:58 +0300

    Lesson 5 Task-34 - LEVEL6 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_6/task_34_38/Arrays.java
@@ -0,0 +1,29 @@
+package students.dmitrijs_matrenicevs.lesson_5.level_6.task_34_38;
+
+import java.util.Random;
+
+class Arrays {
+
+    Random random = new Random();
+
+    public int[] create(int length) {
+        return new int[length];
+
+    }
+
+
+    public void fillRandomly(int[] array) {
+
+        for (int i = 0; i < array.length; i++) ; {
+            array[i] = random.nextInt(50);
+        }
+    }
+
+
+    public void print(int[] array) {
+        for (int i = 0; i < array.length; i++);{
+            System.out.println(i + " = " + array[i]]);
+        }
+    }
+
+}
