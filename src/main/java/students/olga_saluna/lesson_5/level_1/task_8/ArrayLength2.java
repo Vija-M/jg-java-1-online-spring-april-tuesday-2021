ca490d6ccbbd8054635943ae60b19b4d125acd7e
salun <salun.alex@gmail.com>
2021-06-01 18:57:06 +0300

    Task 8, Lesson 5, Level 1

---
+++ src/main/java/students/olga_saluna/lesson_5/level_1/task_8/ArrayLength2.java
@@ -0,0 +1,14 @@
+package students.olga_saluna.lesson_5.level_1.task_8;
+
+class ArrayLength2 {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        numbers[0] = 123;
+        numbers[1] = 321;
+        numbers[2] = 456;
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
