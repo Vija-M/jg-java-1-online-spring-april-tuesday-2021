680b002316e70ba6e720ad4472e22d351b908a38
DeTELo <mrdinas@gmail.com>
2021-06-01 16:14:38 +0300

    Lesson 5 Task-37 - LEVEL6 - FINISH

--- src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_6/task_34_38/Arrays.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_5/level_6/task_34_38/Arrays.java
@@ -36,6 +36,14 @@ class Arrays {
         return max;
     }
 
+    public int findMin(int[] array) {
+       int min = 0;
+       for (int i = 0; i < array.length; i++){
+           if (array[i] < min)
+               min = array[i];
+       }
+       return min;
+    }
 
 
 
