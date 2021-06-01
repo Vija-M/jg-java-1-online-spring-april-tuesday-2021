ca6970113397df4538b05df9e13ea044e15bf456
dk-t <shukailo.d@gmail.com>
2021-06-01 18:04:13 +0300

    Task 37 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_5/level_6/task_37/ArraysDemo.java
@@ -0,0 +1,21 @@
+package students.dmitriy_shukailo.lesson_5.level_6.task_37;
+
+public class ArraysDemo {
+
+    public static void main(String[] args) {
+
+        ArraysDemo demo = new ArraysDemo();
+        demo.shouldCreateArray();
+    }
+
+    public void shouldCreateArray() {
+        Arrays arrays = new Arrays();
+
+        int[] array = arrays.create(10);
+        arrays.fillRandomly(array);
+        arrays.print(array);
+        arrays.findMax(array);
+        arrays.findMin(array);
+    }
+
+}
