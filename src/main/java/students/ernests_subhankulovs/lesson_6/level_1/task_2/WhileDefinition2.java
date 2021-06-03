4d286a09d7b776029cbe75eb418e96bb37a4ecac
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-03 10:06:16 +0300

    Lesson 6 - Level 1 - Task 2. Corrected.

--- src/main/java/students/ernests_subhankulovs/lesson_6/level_1/task_2/WhileDefinition2.java
+++ src/main/java/students/ernests_subhankulovs/lesson_6/level_1/task_2/WhileDefinition2.java
@@ -1,6 +1,6 @@
 package students.ernests_subhankulovs.lesson_6.level_1.task_2;
 
-public class WhileDefinition2 {
+class WhileDefinition2 {
     public static void main(String[] args) {
         int i = 1;
         while (i <= 100) {
