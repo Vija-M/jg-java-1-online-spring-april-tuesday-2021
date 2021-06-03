efd561039d4f19468e45522199e6cefd40a8721d
DeTELo <mrdinas@gmail.com>
2021-06-03 22:40:42 +0300

    Lesson 6 Task-5 - LEVEL1 - FINISH

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_1/task_5/WhileDefinition5.java
@@ -0,0 +1,16 @@
+package students.dmitrijs_matrenicevs.lesson_6.level_1.task_5;
+
+class WhileDefinition5 {
+
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+
+
+}
