cfd2d081013705ad8b98a21dbd41bdb4353d7da7
Vija-M <vija.riga@gmailcom>
2021-06-03 13:42:44 +0300

    Lesson 6, Level 2, Task 8

---
+++ src/main/java/students/vija_m/lesson_6/level_2/task_8/DayOfTheWeekDetector.java
@@ -0,0 +1,18 @@
+package students.vija_m.lesson_6.level_2.task_8;
+
+import java.util.Scanner;
+
+class DayOfTheWeekDetector {
+
+    public int getDayNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7 : ");
+        return scanner.nextInt();
+    }
+
+    public void findDayOfTheWeek(int dayNumber) {
+        // реализуйте данный метод !
+        return String findDayOfTheWeek(int dayNumber)
+    }
+
+}
