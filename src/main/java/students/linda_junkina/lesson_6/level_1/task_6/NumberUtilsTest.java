833c9276454d75aefb7ec2f57cc467086c874130
Linda-Ju <linda.junkina@gmail.com>
2021-06-04 12:08:09 +0300

    Lesson 6, Level 1, Task 6

--- src/main/java/students/linda_junkina/lesson_6/level_1/task_6/NumberUtilsTest.java
+++ src/main/java/students/linda_junkina/lesson_6/level_1/task_6/NumberUtilsTest.java
@@ -3,29 +3,35 @@ package students.linda_junkina.lesson_6.level_1.task_6;
 class NumberUtilsTest {
     public static void main(String[] args) {
         NumberUtils numberUtils = new NumberUtils();
-        System.out.println(numberUtils.isEven(8));
-        System.out.println(numberUtils.isEven(5));
-
         NumberUtilsTest numberUtilsTest = new NumberUtilsTest();
         numberUtilsTest.scenario1();
-        numberUtilsTest.scenario1();
+        numberUtilsTest.scenario2();
     }
 
-    void scenario1() {
-        NumberUtils number = new NumberUtils();
+    NumberUtils subject = new NumberUtils();
 
+    void scenario1() {
         int a = 8;
 
         boolean expected = true;
-        boolean actual = number.isEven(a);
+        boolean actual = subject.isEven(a);
+        assertEqual(subject.isEven(8),true,"scenario1");
+
     }
 
     void scenario2() {
-        NumberUtils number = new NumberUtils();
-
         int a = 5;
 
         boolean expected = false;
-        boolean actual = number.isEven(a);
+        boolean actual = subject.isEven(a);
+        assertEqual(subject.isEven(5),false,"scenario2");
+    }
+
+    private void assertEqual(boolean a, boolean b, String testName) {
+        if (a == b) {
+            System.out.println(testName + " = OK!");
+        } else {
+            System.out.println(testName + " = FAIL!");
+        }
     }
 }
