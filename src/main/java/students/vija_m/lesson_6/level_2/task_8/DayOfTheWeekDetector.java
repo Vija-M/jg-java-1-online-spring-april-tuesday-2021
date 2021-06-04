d4edcd036b33d41e3b2fc31e3c112d168a35e1b2
Vija-M <vija.riga@gmailcom>
2021-06-04 14:09:14 +0300

    Lesson 6, Level 2, Task 8

--- src/main/java/students/vija_m/lesson_6/level_2/task_8/DayOfTheWeekDetector.java
+++ src/main/java/students/vija_m/lesson_6/level_2/task_8/DayOfTheWeekDetector.java
@@ -10,9 +10,20 @@ class DayOfTheWeekDetector {
         return scanner.nextInt();
     }
 
-    public void findDayOfTheWeek(int dayNumber) {
-        // реализуйте данный метод !
-        return String findDayOfTheWeek(int dayNumber)
+    public String findDayOfTheWeek(int dayNumber) {
+        String dayOfTheWeek;
+            switch (dayNumber) {
+            case 1 -> dayOfTheWeek = "Monday";
+            case 2 -> dayOfTheWeek = "Tuesday";
+            case 3 -> dayOfTheWeek = "Wednesday";
+            case 4 -> dayOfTheWeek = "Thursday";
+            case 5 -> dayOfTheWeek = "Friday";
+            case 6 -> dayOfTheWeek = "Saturday";
+            case 7 -> dayOfTheWeek = "Sunday";
+            default -> dayOfTheWeek = "Not correct day number";
+        }
+
+        return dayOfTheWeek;
     }
 
 }
