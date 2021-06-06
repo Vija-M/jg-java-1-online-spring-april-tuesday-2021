47d83d271395cf2f64bb05f816668546f80357de
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-06 22:18:48 +0300

    Lesson 6 - Level 4 - Task 19

---
+++ src/main/java/students/ernests_subhankulovs/lesson_6/level_4/task_19/ForInfiniteLoopBreak.java
@@ -0,0 +1,10 @@
+package students.ernests_subhankulovs.lesson_6.level_4.task_19;
+
+class ForInfiniteLoopBreak {
+    public static void main(String[] args) {
+        for (;;) {
+            System.out.println("This infinite loop will end... Right now!");
+            break;
+        }
+    }
+}
---
+++ src/main/java/students/ernests_subhankulovs/lesson_6/level_4/task_19/WhileInfiniteLoopBreak.java
@@ -0,0 +1,10 @@
+package students.ernests_subhankulovs.lesson_6.level_4.task_19;
+
+class WhileInfiniteLoopBreak {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("This infinite loop will end... Right now!");
+            break;
+        }
+    }
+}
