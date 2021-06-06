d862cd96f8745ddb56ad377361a7bebf81c826fe
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-06 23:12:26 +0300

    Lesson 6 - Level 4 - Task 20

--- src/main/java/students/ernests_subhankulovs/lesson_6/level_4/task_20/ForLoopContinue.java
+++ src/main/java/students/ernests_subhankulovs/lesson_6/level_4/task_20/ForLoopContinue.java
@@ -1,6 +1,6 @@
 package students.ernests_subhankulovs.lesson_6.level_4.task_20;
 
-public class ForLoopContinue {
+class ForLoopContinue {
     public static void main(String[] args) {
         for (int i = 0; i <= 10; i++) {
             if (i % 2 == 0) {
