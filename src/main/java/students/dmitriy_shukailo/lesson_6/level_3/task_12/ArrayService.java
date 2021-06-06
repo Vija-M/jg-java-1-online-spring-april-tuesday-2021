6b00171beae55283e4034872dc18aaf4a59ed7f4
dk-t <shukailo.d@gmail.com>
2021-06-06 22:30:06 +0300

    Task 12 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_6/level_3/task_12/ArrayService.java
@@ -0,0 +1,32 @@
+package students.dmitriy_shukailo.lesson_6.level_3.task_12;
+
+import java.util.Random;
+
+public class ArrayService {
+
+    Random random = new Random();
+
+    public int[] create(int length) {
+        return new int[length];
+    }
+
+    public void fillRandomly(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(10);
+        }
+    }
+
+    boolean contains(int[] arr, int numberToSearch) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToSearch) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void print(int[] arr) {
+        System.out.println("Array [" + arr.length + "]: " + java.util.Arrays.toString(arr));
+    }
+
+}
