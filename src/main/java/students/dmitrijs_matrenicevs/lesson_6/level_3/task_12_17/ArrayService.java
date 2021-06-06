98881151a4e9a0cc1e1245e8d57a497c14a7bb96
DeTELo <mrdinas@gmail.com>
2021-06-06 20:28:30 +0300

    Lesson 6 Task-12-17 - LEVEL3

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_3/task_12_17/ArrayService.java
@@ -0,0 +1,50 @@
+package students.dmitrijs_matrenicevs.lesson_6.level_3.task_12_17;
+
+class ArrayService {
+
+    boolean contains(int[] arr, int numberToSearch) {
+        for (final int i : arr) {
+            if (i == numberToSearch) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    int countOccurrences(int[] arr, int numberToSearch) {
+
+        int res = 0;
+        for (int j : arr) {
+            if (j == numberToSearch) {
+                res++;
+            }
+        }
+        return res;
+
+    }
+
+    boolean replaceFirst(int[] arr, int numberToReplace, int newNumber) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToReplace) {
+                arr[i] = newNumber;
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    int replaceAll(int[] arr, int numberToReplace, int newNumber) {
+
+    }
+
+    void reverse(int[] arr) {
+
+    }
+
+    void sort(int[] arr){
+
+    }
+
+}
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_3/task_12_17/ArrayServiceTest.java
@@ -0,0 +1,8 @@
+package students.dmitrijs_matrenicevs.lesson_6.level_3.task_12_17;
+
+class ArrayServiceTest {
+
+
+
+
+}
