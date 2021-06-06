664e9cb2d6bcf5e451e9d65ff831e3ba2ea5d7e8
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-06 20:09:53 +0300

    Lesson 6 - Level 3 - Task 15

---
+++ src/main/java/students/ernests_subhankulovs/lesson_6/level_3/task_15/ArrayService.java
@@ -0,0 +1,45 @@
+package students.ernests_subhankulovs.lesson_6.level_3.task_15;
+
+class ArrayService {
+
+    boolean contains(int[] arr, int numberToSearch) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToSearch) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int countOccurrences(int[] arr, int numberToSearch) {
+        int occurrences = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToSearch) {
+                occurrences++;
+            }
+        }
+        return occurrences;
+    }
+
+    boolean replaceFirst(int[] arr, int numberToReplace, int newNumber) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToReplace) {
+                arr[i] = newNumber;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int replaceAll(int[] arr, int numberToReplace, int newNumber) {
+        int replacementCounter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToReplace) {
+                arr[i] = newNumber;
+                replacementCounter++;
+            }
+        }
+        return replacementCounter;
+    }
+
+}
