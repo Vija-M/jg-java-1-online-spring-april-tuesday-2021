4a7a0a5bd8e14be3cdf29a6d6cf9e4b010d58965
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-06 21:50:35 +0300

    Lesson 6 - Level 3 - Task 12. Corrected.

--- src/main/java/students/ernests_subhankulovs/lesson_6/level_3/task_12/ArrayServiceTest.java
+++ src/main/java/students/ernests_subhankulovs/lesson_6/level_3/task_12/ArrayServiceTest.java
@@ -1,6 +1,9 @@
 package students.ernests_subhankulovs.lesson_6.level_3.task_12;
 
 class ArrayServiceTest {
+
+    ArrayService arrayService = new ArrayService();
+
     public static void main(String[] args) {
         ArrayServiceTest test = new ArrayServiceTest();
         test.checkForExistingPositiveNumber();
@@ -12,37 +15,31 @@ class ArrayServiceTest {
     }
 
     public void checkForExistingPositiveNumber() {
-        ArrayService arrayService = new ArrayService();
         int[] arrayToTest = {1, 2, 3, 4};
         checkTestResult(true, arrayService.contains(arrayToTest, 1), "Test if number should be found");
     }
 
     public void checkForNonExistingPositiveNumber() {
-        ArrayService arrayService = new ArrayService();
         int[] arrayToTest = {1, 2, 3, 4};
         checkTestResult(false, arrayService.contains(arrayToTest, 5), "Test if number should not be found");
     }
 
     public void checkForExistingZero() {
-        ArrayService arrayService = new ArrayService();
         int[] arrayToTest = {-1, 0, 1, 2};
         checkTestResult(true, arrayService.contains(arrayToTest, 0), "Test if zero should be found");
     }
 
     public void checkForNonExistingZero() {
-        ArrayService arrayService = new ArrayService();
         int[] arrayToTest = {1, 2, 3, 4};
         checkTestResult(false, arrayService.contains(arrayToTest, 0), "Test if zero should not be found");
     }
 
     public void checkForExistingNegativeNumber() {
-        ArrayService arrayService = new ArrayService();
         int[] arrayToTest = {-1, 0, 1, 2};
         checkTestResult(true, arrayService.contains(arrayToTest, -1), "Test negative number should be found");
     }
 
     public void checkForNonExistingNegativeNumber() {
-        ArrayService arrayService = new ArrayService();
         int[] arrayToTest = {1, 2, 3, 4};
         checkTestResult(false, arrayService.contains(arrayToTest, 0), "Test if negative number not be found");
     }
