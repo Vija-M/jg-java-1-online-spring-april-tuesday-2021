434bb9481397098d144ed04d5fbd560f189dbd58
DeTELo <mrdinas@gmail.com>
2021-06-07 22:58:11 +0300

    Lesson 6 Task-19 - LEVEL4

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_4/task_19/ForInfiniteLoopBreak.java
@@ -0,0 +1,15 @@
+package students.dmitrijs_matrenicevs.lesson_6.level_4.task_19;
+
+class ForInfiniteLoopBreak {
+
+    public static void main(String[] args) {
+
+        for (; ; ) {
+
+            System.out.println("IT best of the best!");
+            break;
+        }
+
+    }
+
+}
