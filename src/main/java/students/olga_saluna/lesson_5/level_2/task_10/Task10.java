7c5bb8da0a63866fb36032dff894627d9042a302
salun <salun.alex@gmail.com>
2021-06-07 18:22:10 +0300

    Task 10, Lesson 5, Level 2

---
+++ src/main/java/students/olga_saluna/lesson_5/level_2/task_10/Task10.java
@@ -0,0 +1,13 @@
+package students.olga_saluna.lesson_5.level_2.task_10;
+
+class Task10 {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        numbers[0] = 123;
+        numbers[1] = 321;
+        numbers[2] = 232;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
