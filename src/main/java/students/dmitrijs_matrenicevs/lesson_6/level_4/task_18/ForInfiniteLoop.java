a0db1b2a28a90a87d74047d1353eb6f7dc5c5211
DeTELo <mrdinas@gmail.com>
2021-06-07 22:47:15 +0300

    Lesson 6 Task-18 - LEVEL4

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_4/task_18/ForInfiniteLoop.java
@@ -0,0 +1,12 @@
+package students.dmitrijs_matrenicevs.lesson_6.level_4.task_18;
+
+class ForInfiniteLoop {
+
+    public static void main(String[] args) {
+
+        for (; ; ) {
+
+            System.out.println("IT best of the best!");
+        }
+    }
+}
