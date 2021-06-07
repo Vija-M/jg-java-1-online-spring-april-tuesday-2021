eb2c868229734415d4f8817de8122f6296f09ec1
salun <salun.alex@gmail.com>
2021-06-07 19:57:23 +0300

    Task 18, Lesson 5, Level 3 - correction

--- src/main/java/students/olga_saluna/lesson_5/level_3/task_18/ForLoopDefinition3.java
+++ src/main/java/students/olga_saluna/lesson_5/level_3/task_18/ForLoopDefinition3.java
@@ -2,7 +2,6 @@ package students.olga_saluna.lesson_5.level_3.task_18;
 
 class ForLoopDefinition3 {
     public static void main(String[] args) {
-        int[] array = new int[10];
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
