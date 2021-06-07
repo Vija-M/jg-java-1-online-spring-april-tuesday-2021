fcd6ad38c7c787fd1a6a1429d2c2174ef3b81323
neontone <antons.korenevskis@gmail.com>
2021-06-07 22:15:49 +0300

    Done Tasks 7-8-9

---
+++ src/main/java/students/antons_korenevskis/lesson_6/level_2/Task_1.java
@@ -0,0 +1,15 @@
+package students.antons_korenevskis.lesson_6.level_2;
+
+import java.util.Scanner;
+
+class DayOfTheWeekDetectorDemo {
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber = detector.getDayNumberFromUser();
+        String dayOfTheWeek = detector.findDayOfTheWeek(dayNumber);
+        System.out.println(dayOfTheWeek);
+
+    }
+
+}
