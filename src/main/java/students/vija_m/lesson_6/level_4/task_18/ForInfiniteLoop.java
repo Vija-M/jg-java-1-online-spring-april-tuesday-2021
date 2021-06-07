002e4e1269d887bac1e317d58c15d5c9f87dfd3c
Vija-M <vija.riga@gmailcom>
2021-06-07 22:01:37 +0300

    Lesson 6, Level 4, Task 18

---
+++ src/main/java/students/vija_m/lesson_6/level_4/task_18/ForInfiniteLoop.java
@@ -0,0 +1,9 @@
+package students.vija_m.lesson_6.level_4.task_18;
+
+class ForInfiniteLoop {
+    public static void main(String[] args) {
+        for (; ; ) {
+            System.out.println("Hello, world!");
+        }
+    }
+}
---
+++ src/main/java/students/vija_m/lesson_6/level_4/task_18/WhileInfiniteLoop.java
@@ -0,0 +1,9 @@
+package students.vija_m.lesson_6.level_4.task_18;
+
+class WhileInfiniteLoop {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Hello, world!");
+        }
+    }
+}
