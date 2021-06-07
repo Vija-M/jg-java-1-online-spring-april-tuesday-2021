2c1064ea10a043015591975800afd9e1c026bf18
DeTELo <mrdinas@gmail.com>
2021-06-07 23:28:05 +0300

    Lesson 6 Task-20 - LEVEL4

--- src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_4/task_20/ForLoopContinue.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_4/task_20/ForLoopContinue.java
@@ -6,6 +6,7 @@ class ForLoopContinue {
 
         for (int i = 0; i < 50; i++) {
             if (i % 15 == 0) {
+                System.out.println("Shares!");
                 continue;
             }
             System.out.println(i);
--- src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_4/task_20/WhileLoopContinue.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_4/task_20/WhileLoopContinue.java
@@ -2,6 +2,19 @@ package students.dmitrijs_matrenicevs.lesson_6.level_4.task_20;
 
 class WhileLoopContinue {
 
+    public static void main(String[] args) {
 
+        int i = 0;
+        while (i < 50) {
+            i++;
+
+            if (i % 2 == 0) {
+                System.out.println("Shares!");
+                continue;
+            }
+            System.out.println(i);
+        }
+
+    }
 
 }
