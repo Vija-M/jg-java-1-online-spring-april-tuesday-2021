055a5d0b34dd4f43d47ca0863ae712696c5104ca
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-07 07:58:46 +0300

    Lesson 6 - Level 5 - Task 22

---
+++ src/main/java/students/ernests_subhankulovs/lesson_6/level_5/task_22/TicTacToe.java
@@ -0,0 +1,22 @@
+package students.ernests_subhankulovs.lesson_6.level_5.task_22;
+
+class TicTacToe {
+
+    public boolean isWinPositionForHorizontals(int[][] field, int playerToCheck) {
+        int counter = 0;
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field.length ; j++) {
+                if (field[i][j] == playerToCheck) {
+                    counter++;
+                }
+            }
+            if (counter == field[i].length) {
+                return true;
+            }
+            else {
+                counter = 0;
+            }
+        }
+        return false;
+    }
+}
