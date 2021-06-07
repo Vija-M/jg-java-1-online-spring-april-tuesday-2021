f482c247741fe88a61a5e6b1fea9db251a4645db
salun <salun.alex@gmail.com>
2021-06-07 18:31:17 +0300

    Task 11, Lesson 5, Level 2

---
+++ src/main/java/students/olga_saluna/lesson_5/level_2/task_11/Task11.java
@@ -0,0 +1,27 @@
+package students.olga_saluna.lesson_5.level_2.task_11;
+
+import java.util.Scanner;
+
+class Task11 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1st integer:  ");
+        int firstInt = scanner.nextInt();
+        System.out.println("Enter 2nd integer:  ");
+        int secondInt = scanner.nextInt();
+        System.out.println("Enter 3rd integer:  ");
+        int thirdInt = scanner.nextInt();
+
+        int[] numbers = new int[3];
+
+        numbers[0] = firstInt;
+        numbers[1] = secondInt;
+        numbers[2] = thirdInt;
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
