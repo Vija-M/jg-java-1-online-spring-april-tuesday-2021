67c720368828eac7b3c431d7fcc1a902e6b18659
salun <salun.alex@gmail.com>
2021-06-07 19:35:24 +0300

    Task 16, Lesson 5, Level 3

---
+++ src/main/java/students/olga_saluna/lesson_5/level_3/task_16/ForLoopDefinition1.java
@@ -0,0 +1,11 @@
+package students.olga_saluna.lesson_5.level_3.task_16;
+
+class ForLoopDefinition1 {
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
