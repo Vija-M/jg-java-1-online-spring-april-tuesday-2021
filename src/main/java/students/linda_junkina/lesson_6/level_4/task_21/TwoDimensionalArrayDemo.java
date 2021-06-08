cf1c93a06b24c7e139e51d17371b6a141a446c38
Linda-Ju <linda.junkina@gmail.com>
2021-06-08 12:00:34 +0300

    Lesson 6, Level 4, Task 21

---
+++ src/main/java/students/linda_junkina/lesson_6/level_4/task_21/TwoDimensionalArrayDemo.java
@@ -0,0 +1,10 @@
+package students.linda_junkina.lesson_6.level_4.task_21;
+
+
+class TwoDimensionalArrayDemo {
+    public static void main(String[] args) {
+
+        TwoDimensionalArray twoDimensionalArray = new TwoDimensionalArray();
+        twoDimensionalArray.array(2);
+    }
+}
