f44471e8df2e8e1fc4c5dcc7c96fd2246cbeffa7
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-06-08 22:53:18 +0300

    Completed   Lesson 6 Level 4 Task 18

---
+++ src/main/java/students/arturs_arutjunovs/lesson_6/level_4/task_18/WhileInfiniteLoop.java
@@ -0,0 +1,12 @@
+package students.arturs_arutjunovs.lesson_6.level_4.task_18;
+
+class WhileInfiniteLoop {
+    public static void main(String[] args) {
+
+        int x = 1;
+        while (x > 0) {
+            x++;
+            System.out.println(x);
+        }
+    }
+}
