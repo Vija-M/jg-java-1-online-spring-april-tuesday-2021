6b30fd33c9af0defa2a26da56fde516970753118
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-10 14:50:58 +0300

    Lesson 7 - Level 2 - Task 3.

---
+++ src/main/java/students/ernests_subhankulovs/lesson_7/level_2/task_3/Word.java
@@ -0,0 +1,11 @@
+package students.ernests_subhankulovs.lesson_7.level_2.task_3;
+
+class Word {
+    String value;
+    int count;
+
+    public Word(String value, int count) {
+        this.value = value;
+        this.count = count;
+    }
+}
