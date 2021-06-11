3b08bd453bdf28704aa42ecc21cd996941c07e9b
zilite <pepellv@inbox.lv>
2021-06-11 23:39:27 +0300

    lesson-6/level_2/Task_7

---
+++ src/main/java/students/sergej_pereligin/lesson_6/level_2/task_7/DayOfTheWeekDetector.java
@@ -0,0 +1,12 @@
+package students.sergej_pereligin.lesson_6.level_2.task_7;
+
+import java.util.Scanner;
+
+public class DayOfTheWeekDetector {
+    public int getNumberFromUser(){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Input number between 1 and 7:");
+        return scanner.nextInt();
+
+    }
+}
---
+++ src/main/java/students/sergej_pereligin/lesson_6/level_2/task_7/Task_7.java
@@ -0,0 +1,19 @@
+package students.sergej_pereligin.lesson_6.level_2.task_7;
+
+class DayOfTheWeekDetectorDemo {
+
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber=detector.getNumberFromUser();
+
+        switch (dayNumber) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Not correct day number");
+        }
+    }}
