c22400f547d3e9bacabe3dd8f3451b58c96351f8
DeTELo <mrdinas@gmail.com>
2021-06-12 13:37:06 +0300

    Lesson 7 Task-1 - LEVEL1

--- src/main/java/students/dmitrijs_matrenicevs/lesson_7/level_1/task_1/WordService.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_7/level_1/task_1/WordService.java
@@ -3,8 +3,9 @@ package students.dmitrijs_matrenicevs.lesson_7.level_1.task_1;
 class WordService {
 
     String findMostFrequentWord(String text) {
-       String [] findWords = splitWords(text);
+        String[] findWords = splitWords(text);
         for (int i = 0; i < findWords.length; i++) {
+            int[] frequentWords = new int[0];
             frequentWords[i] = mostWords(findWords, findWords[i]);
         }
         return findWords[maxWords(findWords)];
@@ -14,19 +15,28 @@ class WordService {
         return text.split("A");
     }
 
-    int mostWords(String[] words, String findWords){
+    int mostWords(String[] words, String findWords) {
         int sum = 0;
-        for (String word : words){
-            if (word.equals(findWords)){
+        for (String word : words) {
+            if (word.equals(findWords)) {
                 sum++;
             }
             return sum;
         }
+        return sum;
     }
 
-    int maxWords(int[] findWords) {
+    int maxWords(String[] findWords) {
         int words = 0;
-    }
+        String max = findWords[words];
+        for (int i = 0; i < findWords.length; i++) {
+            if (!findWords[i].equals(max)) {
+                max = findWords[i];
+                words = i;
 
+            }
+        }
+        return words;
+    }
 
 }
--- src/main/java/students/dmitrijs_matrenicevs/lesson_7/level_1/task_1/WordServiceTest.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_7/level_1/task_1/WordServiceTest.java
@@ -2,6 +2,28 @@ package students.dmitrijs_matrenicevs.lesson_7.level_1.task_1;
 
 class WordServiceTest {
 
+WordService wordService = new WordService();
+
+    public static void main(String[] args) {
+        WordServiceTest wordServiceTest = new WordServiceTest();
+
+    }
+
+    void testWordsOne () {
+        String textTest = "JAVA";
+        resultTestWords("IT",wordService.findMostFrequentWord(textTest),"JAVA LIKE MANY PEOPLE");
+    }
+
+    void  testWordsTwo () {
+        String textTest = "JAVA BEST";
+        resultTestWords("IT",wordService.findMostFrequentWord(textTest),"JAVA LIKE MANY COUNTRY");
+    }
+
+    void  testWordsTwo () {
+        String textTest = "JAVA FAIL";
+        resultTestWords("IT",wordService.findMostFrequentWord(textTest),"JAVA FAIL");
+    }
+
 
 
 
