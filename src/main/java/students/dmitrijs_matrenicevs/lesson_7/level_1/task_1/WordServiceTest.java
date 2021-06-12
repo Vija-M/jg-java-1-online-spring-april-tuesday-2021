1c382f2daa38a4c99062d8f209d25b15145205c3
DeTELo <mrdinas@gmail.com>
2021-06-12 13:44:38 +0300

    Lesson 7 Task-1 - LEVEL1

--- src/main/java/students/dmitrijs_matrenicevs/lesson_7/level_1/task_1/WordServiceTest.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_7/level_1/task_1/WordServiceTest.java
@@ -6,22 +6,36 @@ WordService wordService = new WordService();
 
     public static void main(String[] args) {
         WordServiceTest wordServiceTest = new WordServiceTest();
+        wordServiceTest.testWordsOne();
+        wordServiceTest.testWordsTwo();
+        wordServiceTest.testWordsResult();
 
     }
 
     void testWordsOne () {
-        String textTest = "JAVA";
-        resultTestWords("IT",wordService.findMostFrequentWord(textTest),"JAVA LIKE MANY PEOPLE");
+        String textTest = "WORDS";
+        resultTestWords("IT WORDS",wordService.findMostFrequentWord(textTest),"WORDS LIKE MANY PEOPLE");
     }
 
     void  testWordsTwo () {
-        String textTest = "JAVA BEST";
-        resultTestWords("IT",wordService.findMostFrequentWord(textTest),"JAVA LIKE MANY COUNTRY");
+        String textTest = "WORDS BEST";
+        resultTestWords("JAVA WORDS",wordService.findMostFrequentWord(textTest),"WORDS LIKE MANY COUNTRY");
     }
 
-    void  testWordsTwo () {
-        String textTest = "JAVA FAIL";
-        resultTestWords("IT",wordService.findMostFrequentWord(textTest),"JAVA FAIL");
+    void testWordsResult () {
+        String textTest = "WORDS FAIL";
+        resultTestWords("WORDS",wordService.findMostFrequentWord(textTest),"WORDS FAIL");
+    }
+
+    void resultTestWords(String expectedResult,String finishResult, String name){
+        if (expectedResult.equals(finishResult)) {
+            System.out.println(name + " = OK");
+        }
+        else {
+            System.out.println(name + " = FAIL");
+            
+        }
+        
     }
 
 
