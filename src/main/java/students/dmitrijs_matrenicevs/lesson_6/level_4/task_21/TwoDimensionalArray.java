c1654ab47f76cf8e3f935ee39b78bae59fb2ad5f
DeTELo <mrdinas@gmail.com>
2021-06-12 14:08:06 +0300

    Lesson 6 Task-21 - LEVEL4

--- src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_4/task_21/TwoDimensionalArray.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_6/level_4/task_21/TwoDimensionalArray.java
@@ -1,25 +1,40 @@
 package students.dmitrijs_matrenicevs.lesson_6.level_4.task_21;
 
+import java.util.Arrays;
 import java.util.Random;
 
 class TwoDimensionalArray {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
 
         int[][] dimensional = new int[9][8];
-        dimensionalRandom(dimensional);
+        dimensionalRandom();
         System.out.println(sumArray(dimensional));
     }
 
+    private static void dimensionalRandom() {
+    }
 
-    static void randomArray (int[][] dimension){
+    void randomArray(int[][] dimensional) {
         Random random = new Random();
         System.out.println("RandomArray: ");
-        for (int i = 0; i < dimension.length; i++) {
-            for (int j = 0; j < dimension.length; j++) {
-                dimension[i][j] = random.nextInt(30);
+        for (int i = 0; i < dimensional.length; i++) {
+            for (int j = 0; j < dimensional.length; j++) {
+                dimensional[i][j] = random.nextInt(30);
+            }
+            System.out.println(Arrays.toString(dimensional[i]));
+        }
+    }
+
+    static int sumArray(int[][] dimensional){
+        int sum = 0;
+        for (int[] ints : dimensional){
+            for (int i : ints){
+                sum += i;
+
             }
         }
+        return sum;
     }
 
 }
