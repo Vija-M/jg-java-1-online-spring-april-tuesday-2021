d44b7caa0b66d876e8322c8a06f2ae6b69fa136d
zilite <pepellv@inbox.lv>
2021-06-12 13:01:12 +0300

    lesson-6/level_2/Task_8

---
+++ src/main/java/students/sergej_pereligin/lesson_6/level_2/task_8/DayOfTheWeekDetectorDemo.java
@@ -0,0 +1,15 @@
+package students.sergej_pereligin.lesson_6.level_2.task_8;
+
+
+
+class DayOfTheWeekDetectorDemo {
+    public static void main(String[] args) {
+      DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+
+        System.out.println(detector.findDayOfTheWeek(detector.getDayNumberFromUser()));
+    }
+}
+
+
+
+
