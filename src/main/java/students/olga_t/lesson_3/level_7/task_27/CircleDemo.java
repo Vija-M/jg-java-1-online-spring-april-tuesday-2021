f5726c89b1b8fd040f1a38e1bb5253050dc00205
OlgaT <ok.latvia@gmail.com>
2021-06-14 22:20:28 +0300

    Добавила Lesson 3 Level 7 Tasks 27

---
+++ src/main/java/students/olga_t/lesson_3/level_7/task_27/CircleDemo.java
@@ -0,0 +1,10 @@
+package students.olga_t.lesson_3.level_7.task_27;
+
+class CircleDemo {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(12.5);
+
+        System.out.println("Circle area: " + circle1.calculateArea());
+
+    }
+}
