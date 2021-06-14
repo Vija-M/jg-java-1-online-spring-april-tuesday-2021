f8ddee6512a252cb22a1d9a841d39ad483f51dcf
eriks <erikslv88@gmail.com>
2021-06-14 20:28:31 +0300

    Finish - 5 lesson 5 level 31 task

--- src/main/java/students/eriks_jaloveckis/lesson_5/level_5/task_31/Task31.java
+++ src/main/java/students/eriks_jaloveckis/lesson_5/level_5/task_31/Task31.java
@@ -1,13 +1,7 @@
 package students.eriks_jaloveckis.lesson_5.level_5.task_31;
 
-import java.util.Arrays;
-
 class Task31 {
     public int[] create(int length) {
-        int [] array = new int[length];
-        for (int i = 0; i < array.length; i++){
-            System.out.println(Arrays.toString(array));
-        }
-        return array;
+        return new int[length];
     }
 }
