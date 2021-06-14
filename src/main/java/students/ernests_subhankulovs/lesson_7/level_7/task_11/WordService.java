aee02e5d629ee205ff525b6011bef4f4906a99fa
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-15 00:39:54 +0300

    Lesson 7 - Level 7 - Task 11

---
+++ src/main/java/students/ernests_subhankulovs/lesson_7/level_7/task_11/WordService.java
@@ -0,0 +1,44 @@
+package students.ernests_subhankulovs.lesson_7.level_7.task_11;
+
+class WordService {
+    public String findMostFrequentWord(String text) {
+        int mostFrequentWordNumber;
+        String[] words;
+        int[] wordRepetitions;
+
+        words = splitTextToWords(text);
+        wordRepetitions = new int[words.length];
+
+        calculateWordFrequency(words, wordRepetitions);
+        mostFrequentWordNumber = detectMostRepeatedWord(wordRepetitions);
+
+        return words[mostFrequentWordNumber];
+    }
+
+    private String[] splitTextToWords(String text) {
+        return text.split("[,.()\\-\\s*]+");
+    }
+
+    private void calculateWordFrequency(String[] words, int[] wordRepetitions) {
+        int wordCounter = 0;
+        for (int wordCount = 0; wordCount < words.length; wordCount++) {
+            for (String word : words) {
+                if (words[wordCount].equals(word)) {
+                    wordCounter++;
+                }
+            }
+            wordRepetitions[wordCount] = wordCounter;
+            wordCounter = 0;
+        }
+    }
+
+    private int detectMostRepeatedWord(int[] wordRepetitions) {
+        int max = 0;
+        for (int count = 0; count < wordRepetitions.length; count++) {
+            if (wordRepetitions[count] > max) {
+                max = count;
+            }
+        }
+        return max;
+    }
+}
---
+++ src/main/java/students/ernests_subhankulovs/lesson_7/level_7/task_11/WordServiceTest.java
@@ -0,0 +1,39 @@
+package students.ernests_subhankulovs.lesson_7.level_7.task_11;
+
+class WordServiceTest {
+
+    WordService wordService = new WordService();
+
+    public static void main(String[] args) {
+        WordServiceTest test = new WordServiceTest();
+        test.testExample1();
+        test.testExample2();
+    }
+
+    public void testExample1() {
+        String sampleText = "Процедурный код (код, использующий структуры данных) позволяет легко добав-" +
+                            "лять новые функции без изменения существующих структур данных. Объектно-" +
+                            "ориентированный код, напротив, упрощает добавление новых классов без изме-" +
+                            "нения существующих функций.";
+        assertTestResult("код", wordService.findMostFrequentWord(sampleText), "Test 1");
+    }
+
+    public void testExample2() {
+        String sampleText = "Хорошо известное эвристическое правило, называемое законом Деметры, гласит," +
+                            "что модуль не должен знать внутреннее устройство тех объектов, с которыми он" +
+                            "работает. Как мы видели в предыдущем разделе, объекты скрывают свои данные" +
+                            "и предоставляют операции для работы с ними. Это означает, что объект не дол-" +
+                            "жен раскрывать свою внутреннюю структуру через методы доступа, потому что" +
+                            "внутреннюю структуру следует скрывать.";
+        assertTestResult("что", wordService.findMostFrequentWord(sampleText), "Test 2");
+    }
+
+    private void assertTestResult(String expectedResult, String realResult, String testName) {
+        if (expectedResult.equals(realResult)) {
+            System.out.println(testName + " = OK");
+        }
+        else {
+            System.out.println(testName + " = FAIL");
+        }
+    }
+}
