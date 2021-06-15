966dce0c6518089a91a2401ac944f3fd89b149be
jleshkev <evgeny.leshkevich@gmail.com>
2021-06-15 22:56:30 +0300

    Completed Task 12 - Lesson 5 - Level 2

--- src/main/java/students/jevgenij_leshkevich/lesson_5/level_2/task_12/Task12.java
+++ src/main/java/students/jevgenij_leshkevich/lesson_5/level_2/task_12/Task12.java
@@ -1,5 +1,6 @@
 package students.jevgenij_leshkevich.lesson_5.level_2.task_12;
 
+import java.util.Arrays;
 import java.util.Random;
 
 class Task12 {
@@ -7,10 +8,10 @@ class Task12 {
     public static void main(String[] args) {
         int[] numbers = new int[3];
         Random random = new Random();
-        System.out.println("Содержимое массива (значения от -100  до 100): ");
-        for (int number : numbers) {
-            number = random.nextInt(201) - 100;
-            System.out.println(number);
+        System.out.println("Содержимое массива (значения от 0 до 100): ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(101);
+            System.out.println(numbers[i]);
         }
     }
 }
