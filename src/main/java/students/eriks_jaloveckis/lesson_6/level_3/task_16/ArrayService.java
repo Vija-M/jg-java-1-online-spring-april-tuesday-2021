a55c741482f8a0630d3280aa89889e6426efa7ca
eriks <erikslv88@gmail.com>
2021-06-15 21:33:30 +0300

    Finish - 6 lesson 3 level 16 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_6/level_3/task_16/ArrayService.java
@@ -0,0 +1,53 @@
+package students.eriks_jaloveckis.lesson_6.level_3.task_16;
+
+class ArrayService {
+    boolean contains(int[] arr, int numberToSearch) {
+        for (int number : arr) {
+            if (number == numberToSearch) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int countOccurrences(int[] arr, int numberToSearch) {
+        int count = 0;
+        for (int number : arr) {
+            if (number == numberToSearch) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    boolean replaceFirst(int[] arr, int numberToReplace, int newNumber) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToReplace) {
+                newNumber = arr[i];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int replaceAll(int[] arr, int numberToReplace, int newNumber) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToReplace) {
+                arr[i] = newNumber;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    void reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = tmp;
+        }
+    }
+
+
+}
