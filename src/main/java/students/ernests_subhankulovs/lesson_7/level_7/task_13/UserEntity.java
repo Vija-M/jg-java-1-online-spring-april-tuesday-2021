d10d87098972f10c4b98a1b8b137df430e6aa394
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-15 08:57:16 +0300

    Lesson 7 - Level 7 - Task 13

---
+++ src/main/java/students/ernests_subhankulovs/lesson_7/level_7/task_13/UserEntity.java
@@ -0,0 +1,45 @@
+package students.ernests_subhankulovs.lesson_7.level_7.task_13;
+
+import java.util.UUID;
+
+class UserEntity {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String personalCode;
+
+    public UserEntity(String firstName, String lastName, String personalCode) {
+        id = UUID.randomUUID();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.personalCode = personalCode;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPersonalCode() {
+        return personalCode;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPersonalCode(String personalCode) {
+        this.personalCode = personalCode;
+    }
+}
