2034cfdc0db556ce6ef740d76c3a143a2f300d06
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-06-15 10:04:38 +0300

    Completed   Lesson 7 Level 2 Task 3

---
+++ src/main/java/students/arturs_arutjunovs/lesson_7/level_2/task_3/WordService.java
@@ -0,0 +1,41 @@
+package students.arturs_arutjunovs.lesson_7.level_2.task_3;
+
+class WordService {
+
+    public String findMostFrequentWord(String text) {
+        String[] words = splitWords(text);
+
+        int[] numOfEqualWords = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            numOfEqualWords[i] = countOccurrences(words, words[i]);
+        }
+        return words[maxIndexed(numOfEqualWords)];
+
+    }
+
+    private String[] splitWords(String text) {
+        return text.split("\\W+");
+    }
+
+    private int maxIndexed(int[] numbers) {
+        int index = 0;
+        int max = numbers[index];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    private int countOccurrences(String[] words, String wordToSearch) {
+        int count = 0;
+        for (String word : words) {
+            if (word.equals(wordToSearch)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/main/java/students/arturs_arutjunovs/lesson_7/level_2/task_3/WordServiceTest.java
@@ -0,0 +1,41 @@
+package students.arturs_arutjunovs.lesson_7.level_2.task_3;
+
+class WordServiceTest {
+
+    WordService wordService = new WordService();
+
+    public static void main(String[] args) {
+
+        WordServiceTest test = new WordServiceTest();
+        test.EqualWords_Test1();
+        test.EqualWords_Test2();
+        test.notDetectEqualWords();
+    }
+
+    public void EqualWords_Test1() {
+        String testText = "Intellij Idea is the best Java language IDE. One of most popular programming language \n" +
+                "is Java. Try that most popular language once, and you will love Java.";
+        assertCheckResult("Java", wordService.findMostFrequentWord(testText), "Four equal words(Java and language)");
+    }
+
+    public void EqualWords_Test2() {
+        String testText = "In this program, we need to find the most repeated word present in given text file. \n" +
+                "This can be done by opening a file in read mode using file pointer.";
+        assertCheckResult("file", wordService.findMostFrequentWord(testText), "Three equal words(file)");
+    }
+
+    public void notDetectEqualWords() {
+        String testText = "Oracle (and others) highly recommend uninstalling outdated Java versions because of \n" +
+                "serious risks due to unresolved security issues.";
+        assertCheckResult("Oracle", wordService.findMostFrequentWord(testText), "No equal words");
+
+    }
+
+    public void assertCheckResult(String expectedResult, String actualResult, String testName) {
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("[ OK ] " + testName);
+        } else {
+            System.out.println("[ FAILED ] " + testName);
+        }
+    }
+}
