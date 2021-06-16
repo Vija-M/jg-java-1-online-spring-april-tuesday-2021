d9536e908b07065dc1cc532ae9418b08bd17c123
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-06-16 15:32:55 +0300

    Completed   Lesson 8 Level 1 Task 2

---
+++ src/main/java/students/arturs_arutjunovs/lesson_8/level_1/task_2/ImplicitDefaultConstructor.java
@@ -0,0 +1,11 @@
+package students.arturs_arutjunovs.lesson_8.level_1.task_2;
+
+class ImplicitDefaultConstructor {
+
+    ImplicitDefaultConstructor() {
+        super();
+        //Если d классе не создается конструктор с какой-либо реализацией, то по умолчанию
+        // создается (генерируется) такой "пустой" конструктор без параметров, он же default
+
+    }
+}
---
+++ src/main/java/students/arturs_arutjunovs/lesson_8/level_1/task_2/ImplicitDefaultConstructorDemo.java
@@ -0,0 +1,11 @@
+package students.arturs_arutjunovs.lesson_8.level_1.task_2;
+
+class ImplicitDefaultConstructorDemo {
+
+    public static void main(String[] args) {
+
+
+        ImplicitDefaultConstructorDemo demo = new ImplicitDefaultConstructorDemo();
+
+    }
+}
