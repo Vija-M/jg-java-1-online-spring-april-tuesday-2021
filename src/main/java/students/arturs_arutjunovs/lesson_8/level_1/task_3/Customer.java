ad9e463440a7ac3d719fe65d482ee66eebd14c35
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-06-16 15:34:39 +0300

    Completed   Lesson 8 Level 1 Task 3

---
+++ src/main/java/students/arturs_arutjunovs/lesson_8/level_1/task_3/Customer.java
@@ -0,0 +1,29 @@
+package students.arturs_arutjunovs.lesson_8.level_1.task_3;
+
+class Customer {
+
+    private String nickname;
+    private int messagesCount;
+    private boolean isOnline;
+    private boolean isModerator;
+
+    Customer() {
+
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getMessagesCount() {
+        return messagesCount;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public Boolean getModerator() {
+        return isModerator;
+    }
+}
