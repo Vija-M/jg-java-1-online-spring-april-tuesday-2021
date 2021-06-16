3d10275c36b7fac47d94660d5df039f2548109af
DeTELo <mrdinas@gmail.com>
2021-06-16 13:08:47 +0300

    Lesson 8 Task-2 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_1/task_2/ImplicitDefaultConstructor.java
@@ -0,0 +1,14 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_1.task_2;
+
+class ImplicitDefaultConstructor {
+
+    String name;
+    int age;
+    String city;
+
+    public ImplicitDefaultConstructor(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+}
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_1/task_2/ImplicitDefaultConstructorDemo.java
@@ -0,0 +1,14 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_1.task_2;
+
+class ImplicitDefaultConstructorDemo {
+
+    public static void main(String[] args) {
+
+        ImplicitDefaultConstructor implicitDefaultConstructor = new ImplicitDefaultConstructor("Dmitry",26, "Riga");
+
+        System.out.println(implicitDefaultConstructor);
+
+    }
+
+
+}
