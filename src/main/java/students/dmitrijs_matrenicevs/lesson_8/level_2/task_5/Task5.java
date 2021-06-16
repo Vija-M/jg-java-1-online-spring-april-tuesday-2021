390465e938d6ac77a09ad4534ee5d7c1f1f045c4
DeTELo <mrdinas@gmail.com>
2021-06-16 21:28:21 +0300

    Lesson 8 Task-5 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_2/task_5/Task5.java
@@ -0,0 +1,8 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_2.task_5;
+
+class Task5 {
+
+    //Java не поддерживает множественное наследование классов потому,
+    // что это может привести к ромбовидной проблеме.
+
+}
