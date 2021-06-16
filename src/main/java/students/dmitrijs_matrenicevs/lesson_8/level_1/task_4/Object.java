f41056be166bed687deb4d3f1a0621b5ba4660d8
DeTELo <mrdinas@gmail.com>
2021-06-16 21:08:06 +0300

    Lesson 8 Task-4 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_1/task_4/Object.java
@@ -0,0 +1,5 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_1.task_4;
+
+class Object {
+
+}
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_1/task_4/Person.java
@@ -0,0 +1,6 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_1.task_4;
+
+class Person extends Object {
+
+
+}
