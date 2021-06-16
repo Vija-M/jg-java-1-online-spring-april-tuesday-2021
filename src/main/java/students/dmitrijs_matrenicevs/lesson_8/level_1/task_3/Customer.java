0b4f43ad3aa4f52fe21525686c3c2ad47b0477b6
DeTELo <mrdinas@gmail.com>
2021-06-16 13:50:15 +0300

    Lesson 8 Task-3 - LEVEL1

--- src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_1/task_3/Customer.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_1/task_3/Customer.java
@@ -5,50 +5,30 @@ class Customer {
     private String nickname;
     private int messagesCount;
     private boolean isOnline;
+    private Boolean isModerator;
 
     public String getNickname() {
         return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
     }
 
     public int getMessagesCount() {
         return messagesCount;
     }
 
-    public void setMessagesCount(int messagesCount) {
-        this.messagesCount = messagesCount;
-    }
-
     public boolean isOnline() {
         return isOnline;
     }
 
-    public void setOnline(boolean online) {
-        isOnline = online;
-    }
-
-    public Boolean getModerator() {
+    public boolean getModerator() {
         return isModerator;
     }
 
-    public void setModerator(Boolean moderator) {
-        isModerator = moderator;
-    }
-
-    private Boolean isModerator;
-
-    void PrintInfo(){
-        System.out.println(getNickname());
+    void PrintInfo() {
         System.out.println(nickname);
-        System.out.println(getMessagesCount());
         System.out.println(messagesCount);
         System.out.println(isOnline);
         System.out.println(isModerator);
 
     }
-
 
 }
