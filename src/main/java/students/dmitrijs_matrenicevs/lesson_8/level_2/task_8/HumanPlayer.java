dd038f3baaa20f797a49f532c725c7a3281ae068
DeTELo <mrdinas@gmail.com>
2021-06-17 22:53:22 +0300

    Lesson 8 Task-8 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_2/task_8/HumanPlayer.java
@@ -0,0 +1,11 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_2.task_8;
+
+class HumanPlayer extends Player {
+
+    HumanPlayer(String name) {
+    super(name);
+    }
+
+    HumanPlayer humanPlayer = new HumanPlayer("Dmitriy");
+
+}
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_2/task_8/Player.java
@@ -0,0 +1,11 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_2.task_8;
+
+class Player {
+
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+
+}
