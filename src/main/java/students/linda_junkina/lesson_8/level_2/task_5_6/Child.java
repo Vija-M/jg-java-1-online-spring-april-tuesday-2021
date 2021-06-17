d2654b473886fc3bd8342b743430f55dabb34585
Linda-Ju <linda.junkina@gmail.com>
2021-06-17 22:31:25 +0300

    Lesson 8, Level 2, Task 5,6

--- src/main/java/students/linda_junkina/lesson_8/level_2/task_5_6/Child.java
+++ src/main/java/students/linda_junkina/lesson_8/level_2/task_5_6/Child.java
@@ -2,3 +2,5 @@ package students.linda_junkina.lesson_8.level_2.task_5_6;
 
 class Child extends Father {
 }
+// проблема, к которой может привести множественное наследование - diamond problem.
+// программа не поймет какой метод наследовать
