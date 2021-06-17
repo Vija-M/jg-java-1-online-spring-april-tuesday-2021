678b327e1dec5f4c0e573f4eb180076693fae801
Linda-Ju <linda.junkina@gmail.com>
2021-06-17 15:54:06 +0300

    Lesson 8, Level 2, Task 7

---
+++ src/main/java/students/linda_junkina/lesson_8/level_2/task_7/Account.java
@@ -0,0 +1,12 @@
+package students.linda_junkina.lesson_8.level_2.task_7;
+
+class Account {
+
+    String name;
+    String emailAddress;
+
+    public Account(String name, String emailAddress) {
+        this.name = name;
+        this.emailAddress = emailAddress;
+    }
+}
