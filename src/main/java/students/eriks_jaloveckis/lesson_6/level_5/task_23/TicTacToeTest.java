61d5ea3d8d8f5ce385f348af0394bdd6077002db
eriks <erikslv88@gmail.com>
2021-06-17 19:46:44 +0300

    Finish - 6 lesson 5 level 23 task

--- src/main/java/students/eriks_jaloveckis/lesson_6/level_5/task_23/TicTacToeTest.java
+++ src/main/java/students/eriks_jaloveckis/lesson_6/level_5/task_23/TicTacToeTest.java
@@ -49,39 +49,46 @@ class TicTacToeTest {
         int[][] field = {{0, 1, 0}, {1, -1, 1}, {0, 0, 0}};
         assertTest(true, ticTacToe.isWinPositionForHorizontals(field, 0), "Win (0) - last line");
     }
+
     public void test4() {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{1, 1, 1}, {1, -1, 1}, {0, 0, 0}};
         assertTest(true, ticTacToe.isWinPositionForHorizontals(field, 0), "Win first (1)");
         assertTest(true, ticTacToe.isWinPositionForHorizontals(field, 1), "Win second (0)");
     }
+
     public void test5() {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{1, -1, 1}, {1, -1, 1}, {0, 1, 0}};
         assertTest(false, ticTacToe.isWinPositionForHorizontals(field, 0), "Lose (1)");
         assertTest(false, ticTacToe.isWinPositionForHorizontals(field, 1), "Lose (0)");
     }
+
     public void test6() {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{1, 1, 0}, {1, 0, 1}, {1, 0, 1}};
         assertTest(true, ticTacToe.isWinPositionForVerticals(field, 1), "Win (1 verticals) - first line");
     }
+
     public void test7() {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{-1, 0, 0}, {1, 0, 1}, {0, 0, 1}};
         assertTest(true, ticTacToe.isWinPositionForVerticals(field, 0), "Win (0 verticals) - second line");
     }
+
     public void test8() {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{-1, 1, 1}, {1, 0, 1}, {0, 0, 1}};
         assertTest(true, ticTacToe.isWinPositionForVerticals(field, 1), "Win (1 verticals) - last line");
     }
-    public void test9 () {
+
+    public void test9() {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{0, 1, 0}, {0, 1, 0}, {0, 1, 1}};
         assertTest(true, ticTacToe.isWinPositionForVerticals(field, 1), "Win (1 verticals) - second line");
         assertTest(true, ticTacToe.isWinPositionForVerticals(field, 0), "Win (0 verticals) - first line");
     }
+
     public void test10() {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{1, -1, 1}, {0, -1, 1}, {0, 1, 0}};
