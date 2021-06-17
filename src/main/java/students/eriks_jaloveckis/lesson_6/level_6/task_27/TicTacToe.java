63febbeb0618fe4f05bb5a25e289dd56dc414579
eriks <erikslv88@gmail.com>
2021-06-17 22:42:29 +0300

    Finish - 6 lesson 6 level 27 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_6/level_6/task_27/TicTacToe.java
@@ -0,0 +1,13 @@
+package students.eriks_jaloveckis.lesson_6.level_6.task_27;
+
+class TicTacToe {
+    public int[][] createField() {
+        int emptyCell = -1;
+        int[][] field = new int[3][3];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field.length; j++)
+            field[i][j] = emptyCell;
+        }
+        return field;
+    }
+}
