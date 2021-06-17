00a5ac3c84b2568b791455765d5590ce8d355984
DeTELo <mrdinas@gmail.com>
2021-06-17 22:47:02 +0300

    Lesson 8 Task-7 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_2/task_7/Task7.java
@@ -0,0 +1,15 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_2.task_7;
+
+class Task7 extends TaskDemo  {
+
+    String name;
+    String city;
+
+    public Task7(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    }
+
+
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_2/task_7/TaskDemo.java
@@ -0,0 +1,13 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_2.task_7;
+
+class TaskDemo {
+
+    public static void main(String[] args) {
+
+        Task7 task7 = new Task7("Dmitry","Riga");
+
+        System.out.println(task7.name);
+        System.out.println(task7.city);
+
+    }
+}
