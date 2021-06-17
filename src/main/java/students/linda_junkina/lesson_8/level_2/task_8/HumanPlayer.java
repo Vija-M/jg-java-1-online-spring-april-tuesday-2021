b96743421aca6f5dbff181f7d5b5a4310548fc87
Linda-Ju <linda.junkina@gmail.com>
2021-06-17 23:07:45 +0300

    Lesson 8, Level 2, Task 5,6

--- src/main/java/students/linda_junkina/lesson_8/level_2/task_8/HumanPlayer.java
+++ src/main/java/students/linda_junkina/lesson_8/level_2/task_8/HumanPlayer.java
@@ -1,6 +1,6 @@
 package students.linda_junkina.lesson_8.level_2.task_8;
 
-class HumanPlayer extends Player{
+class HumanPlayer extends Player {
 
     HumanPlayer(String name) {
         super(name);
