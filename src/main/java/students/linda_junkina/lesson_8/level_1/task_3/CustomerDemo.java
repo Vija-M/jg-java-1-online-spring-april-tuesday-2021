595a6098c55e2f7a10498ef1e704568da968eeaf
Linda-Ju <linda.junkina@gmail.com>
2021-06-17 14:11:43 +0300

    Lesson 8, Level 1, Task 2

---
+++ src/main/java/students/linda_junkina/lesson_8/level_1/task_3/CustomerDemo.java
@@ -0,0 +1,12 @@
+package students.linda_junkina.lesson_8.level_1.task_3;
+
+class CustomerDemo {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+
+        System.out.println("customer.getNickname() = " + customer.getNickname());
+        System.out.println("customer.getNickname() = " + customer.getMessagesCount());
+        System.out.println("customer.getNickname() = " + customer.getOnline());
+        System.out.println("customer.getNickname() = " + customer.getModerator());
+    }
+}
