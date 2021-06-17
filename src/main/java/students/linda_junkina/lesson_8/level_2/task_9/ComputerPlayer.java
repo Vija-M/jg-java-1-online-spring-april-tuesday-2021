3b507d93ac931e4272e2999e8c4765d6b1594e02
Linda-Ju <linda.junkina@gmail.com>
2021-06-17 23:15:29 +0300

    Lesson 8, Level 2, Task 9

--- src/main/java/students/linda_junkina/lesson_8/level_2/task_9/ComputerPlayer.java
+++ src/main/java/students/linda_junkina/lesson_8/level_2/task_9/ComputerPlayer.java
@@ -1,6 +1,6 @@
 package students.linda_junkina.lesson_8.level_2.task_9;
 
-class ComputerPlayer extends Player{
+class ComputerPlayer extends Player {
     ComputerPlayer(String name) {
         super(name);
     }
