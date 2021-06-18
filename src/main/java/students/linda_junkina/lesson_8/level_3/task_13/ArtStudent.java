cc5f89e11f946517ad4d42e4339c40ed00e83cc2
Linda-Ju <linda.junkina@gmail.com>
2021-06-18 12:35:18 +0300

    Lesson 8, Level 3, Task 13

---
+++ src/main/java/students/linda_junkina/lesson_8/level_3/task_13/ArtStudent.java
@@ -0,0 +1,9 @@
+package students.linda_junkina.lesson_8.level_3.task_13;
+
+class ArtStudent extends Student {
+
+    public ArtStudent(String name, String program, double gradePointAverage) {
+
+        super("Anne", "Linguistics", 9.4);
+    }
+}
