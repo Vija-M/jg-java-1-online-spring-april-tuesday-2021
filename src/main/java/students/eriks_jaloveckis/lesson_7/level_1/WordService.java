1417af773c30249c77fb78d4cbf983c1adb16f0f
eriks <erikslv88@gmail.com>
2021-06-19 10:19:19 +0300

    Finish - 7 lesson 1 level 1 task

--- src/main/java/students/eriks_jaloveckis/lesson_7/level_1/WordService.java
+++ src/main/java/students/eriks_jaloveckis/lesson_7/level_1/WordService.java
@@ -1,19 +1,41 @@
 package students.eriks_jaloveckis.lesson_7.level_1;
 
-class WordService { // В ГОЛОВУ НЕ ПРИХОДИЛО РЕШЕНИЕ, ПОЭТОМУ ПРИШЛОСЬ БРАТЬ С ИНТЕРНЕТА
+class WordService {
     public String findMostFrequentWord(String text) {
-        String[] words = text.split(" ");
-        int[] howMuch = new int[text.length()];    // каждому слову соответствует число одинаковых
-        int maxCount = 0;                       // число макс кол-вал встречающихся
-        int index = 0;                          // последний индекс с макс.
-        for (int i = 0; i < words.length; i++)
-            for (int j = 0; j < words.length; j++)
-                if (words[i].equals(words[j])) howMuch[i]++; // заполним массив
-        for (int i = 0; i < howMuch.length; i++)
-            if (howMuch[i] > maxCount) {
-                maxCount = howMuch[i];                  // найдем макс.
-                index = i;                              // найдем индекс с макс.
+
+        String[] words = splitWords(text);
+
+        int[] numOfEqualWords = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            numOfEqualWords[i] = countOccurrences(words, words[i]);
+        }
+
+        return words[maxIndexed(numOfEqualWords)];
+    }
+
+    private String[] splitWords(String text) {
+        return text.split("\\W+");
+    }
+
+    private int countOccurrences(String[] words, String wordToSearch) {
+        int count = 0;
+        for (String word : words) {
+            if (word.equals(wordToSearch)) {
+                count++;
             }
-        return words[index];
+        }
+        return count;
+    }
+
+    private int maxIndexed(int[] numbers) {
+        int index = 0;
+        int max = numbers[index];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+                index = i;
+            }
+        }
+        return index;
     }
 }
--- src/main/java/students/eriks_jaloveckis/lesson_7/level_1/WordServiceTest.java
+++ src/main/java/students/eriks_jaloveckis/lesson_7/level_1/WordServiceTest.java
@@ -21,18 +21,20 @@ class WordServiceTest {
     }
 
     private void testFirst() {
-        String text = "Мяч упал. Мяч накачали. Мяч сдулся.";
-        assertTestResult("Мяч", wordService.findMostFrequentWord(text), "Test 1");
+        String testText = "Oracle (and others) highly recommend uninstalling outdated Java versions because of \n" +
+                "serious risks due to unresolved security issues.";
+        assertTestResult("Oracle", wordService.findMostFrequentWord(testText), "No equal words");
     }
 
     private void testSecond() {
-        String text = "Мяч упал. Мяч накачали. Солнце светит. Солнце заходит.";
-        assertTestResult("Мяч", wordService.findMostFrequentWord(text), "Test 2");
+        String testText = "Intellij Idea is the best Java language IDE. One of most popular programming language \n" +
+                "is Java. Try that most popular language once, and you will love Java.";
+        assertTestResult("Java", wordService.findMostFrequentWord(testText), "Four equal words(Java and language)");
     }
 
     private void testThird() {
-        String text = "Мяч упал. Солнце заходит. На улице темнеет.";
-        assertTestResult("Мяч", wordService.findMostFrequentWord(text), "Test 3");
+        String testText = "In this program, we need to find the most repeated word present in given text file. \n" +
+                "This can be done by opening a file in read mode using file pointer.";
+        assertTestResult("file", wordService.findMostFrequentWord(testText), "Three equal words(file)");
     }
-
 }
