d07e8cc280028b5c0ff0e4205dc767ad5f17a0b7
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-06-21 00:09:42 +0300

    Completed   Lesson 8 Level 4 Task 17

---
+++ src/main/java/students/arturs_arutjunovs/lesson_8/level_4/task_17/Shape.java
@@ -0,0 +1,15 @@
+package students.arturs_arutjunovs.lesson_8.level_4.task_17;
+
+abstract class Shape {
+
+    private String title;
+
+    Shape(String title) {
+        this.title = title;
+    }
+
+
+    abstract double area();
+
+    abstract double perimeter();
+}
---
+++ src/main/java/students/arturs_arutjunovs/lesson_8/level_4/task_17/Square.java
@@ -0,0 +1,21 @@
+package students.arturs_arutjunovs.lesson_8.level_4.task_17;
+
+class Square extends Shape {
+
+    private double sideLength;
+
+    public Square(String title, double sideLength) {
+        super(title);
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    double area() {
+        return sideLength * sideLength;
+    }
+
+    @Override
+    double perimeter() {
+        return sideLength * 4;
+    }
+}
