c699be8720bd55c84e4097b9005329ba1ac41b35
dk-t <shukailo.d@gmail.com>
2021-06-21 20:57:07 +0300

    Task 2 has been corrected

--- src/main/java/students/dmitriy_shukailo/lesson_8/level_1/task_2/ImplicitDefaultConstructorDemo.java
+++ src/main/java/students/dmitriy_shukailo/lesson_8/level_1/task_2/ImplicitDefaultConstructorDemo.java
@@ -2,4 +2,9 @@ package students.dmitriy_shukailo.lesson_8.level_1.task_2;
 
 class ImplicitDefaultConstructorDemo {
 
+    public static void main(String[] args) {
+
+        ImplicitDefaultConstructor implicitDefaultConstructor = new ImplicitDefaultConstructor();
+    }
+
 }
