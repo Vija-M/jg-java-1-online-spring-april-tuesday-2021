accad9c41e374ff4f3f53f887741d67c6562dc1a
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-22 17:48:44 +0300

    Lesson 8 - Level 4 - Task 21

---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/level_5/task_22/CircleTest.java
@@ -0,0 +1,29 @@
+package students.ernests_subhankulovs.lesson_8.level_5.task_22;
+
+class CircleTest {
+
+    Circle circle = new Circle(5);
+
+    public static void main(String[] args) {
+        CircleTest test = new CircleTest();
+        test.testArea();
+        test.testPerimeter();
+    }
+
+    public void testArea() {
+        assertTestResult(78.539816339744830961566084581988, circle.area(), "Area test");
+    }
+
+    public void testPerimeter() {
+        assertTestResult(31.415926535897932384626433832795, circle.perimeter(), "Perimeter test");
+    }
+
+    private void assertTestResult(double expectedResult, double realResult, String testName) {
+        if (expectedResult == realResult) {
+            System.out.println(testName + " = OK");
+        }
+        else {
+            System.out.println(testName + " = FAIL");
+        }
+    }
+}
