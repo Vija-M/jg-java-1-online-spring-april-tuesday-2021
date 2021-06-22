3640c66c3b589743b805c3b7f20b660ab8aa501e
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-22 11:36:04 +0300

    Lesson 7 - Level 7 - Task 14.

---
+++ src/main/java/students/ernests_subhankulovs/lesson_7/level_7/task_14/UserRepository.java
@@ -0,0 +1,61 @@
+package students.ernests_subhankulovs.lesson_7.level_7.task_14;
+
+import java.util.UUID;
+
+class UserRepository {
+
+    private UserEntity[] users;
+    private int cursor = 0;
+
+    UserRepository() {
+        users = new UserEntity[100];
+    }
+
+    UUID save(UserEntity entity) {
+        UUID randomUUID = UUID.randomUUID();
+            entity.setId(randomUUID);
+            users[cursor] = entity;
+            cursor++;
+        return randomUUID;
+    }
+
+    UserEntity findById(UUID uuid) {
+        for (UserEntity entity : users) {
+            if (entity.getId().equals(uuid)) {
+                return entity;
+            }
+        }
+        return null;
+    }
+
+    UserEntity findByPersonalCode(String personalCode) {
+        for (UserEntity entity : users) {
+            if (entity.getPersonalCode().equals(personalCode)) {
+                return entity;
+            }
+        }
+        return null;
+    }
+
+    UserEntity findByFirstName(String firstName) {
+        for (UserEntity entity : users) {
+            if (entity.getFirstName().equals(firstName)) {
+                return entity;
+            }
+        }
+        return null;
+    }
+
+    UserEntity[] findAll() {
+        return users;
+    }
+
+    void delete(UUID uuid) {
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getId().equals(uuid)) {
+                users[i] = null;
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/students/ernests_subhankulovs/lesson_7/level_7/task_14/UserRepositoryApplication.java
@@ -0,0 +1,50 @@
+package students.ernests_subhankulovs.lesson_7.level_7.task_14;
+
+import java.util.UUID;
+
+class UserRepositoryApplication {
+
+    UserRepository userRepository = new UserRepository();
+
+    public static void main(String[] args) {
+        UserRepositoryApplication application = new UserRepositoryApplication();
+        application.addNewUsers();
+        application.printAllUsers();
+
+
+    }
+
+    public void addNewUsers() {
+        UserEntity user1 = new UserEntity();
+        user1.setFirstName("Zachary");
+        user1.setLastName("Peterson");
+        user1.setPersonalCode("66684565220");
+
+        UserEntity user2 = new UserEntity();
+        user2.setFirstName("Jarrett");
+        user2.setLastName("Bates");
+        user2.setPersonalCode("42432651636");
+
+        UserEntity user3 = new UserEntity();
+        user3.setFirstName("Jair");
+        user3.setLastName("Dudley");
+        user3.setPersonalCode("72505954901");
+
+        UUID user1Id = userRepository.save(user1);
+        UUID user2Id = userRepository.save(user2);
+        UUID user3Id = userRepository.save(user3);
+    }
+
+    public void printAllUsers() {
+        UserEntity[] users = userRepository.findAll();
+        for (UserEntity user : users) {
+            if (user != null) {
+                System.out.println("User ID: " + user.getId());
+                System.out.println("Personal Code: " + user.getPersonalCode());
+                System.out.println("First Name: " + user.getFirstName());
+                System.out.println("Last Name: " + user.getLastName());
+                System.out.println();
+            }
+        }
+    }
+}
