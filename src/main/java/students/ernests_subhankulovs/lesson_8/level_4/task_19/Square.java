1599c08e372adbc216ed8a62587b40338e158a88
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-22 17:25:10 +0300

    Lesson 8 - Level 4 - Task 19

---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/level_4/task_19/Square.java
@@ -0,0 +1,20 @@
+package students.ernests_subhankulovs.lesson_8.level_4.task_19;
+
+class Square extends Shape {
+    private double side;
+
+    Square(double side) {
+        super("Square");
+        this.side = side;
+    }
+
+    @Override
+    double area() {
+        return Math.pow(side, 2);
+    }
+
+    @Override
+    double perimeter() {
+        return 4 * side;
+    }
+}
