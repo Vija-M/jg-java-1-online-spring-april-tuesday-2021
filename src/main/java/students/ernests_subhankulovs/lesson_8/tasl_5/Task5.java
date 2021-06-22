abdfedb2b9c083c8cd4ddb9538e95ac0ff444f61
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-22 12:32:31 +0300

    Lesson 8 - Level 2 - Task 5

---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/tasl_5/Task5.java
@@ -0,0 +1,6 @@
+package students.ernests_subhankulovs.lesson_8.tasl_5;
+
+class Task5 {
+    //В языке Java запрещено множественное наследование.
+    //Класс не может наследовать свойства более чем ожного класса.
+}
