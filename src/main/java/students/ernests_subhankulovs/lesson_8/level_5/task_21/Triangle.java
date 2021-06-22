6f0863bbdde0ca6788211fc534aa99c57d2081b2
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-22 17:56:21 +0300

    Lesson 8 - Level 4 - Task 21. Corrected.

---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/level_5/task_21/Triangle.java
@@ -0,0 +1,20 @@
+package students.ernests_subhankulovs.lesson_8.level_5.task_21;
+
+class Triangle extends Shape {
+    private double side;
+
+    Triangle(double side) {
+        super("Triangle");
+        this.side = side;
+    }
+
+    @Override
+    double area() {
+        return (Math.sqrt(3) / 4) * Math.pow(side, 2);
+    }
+
+    @Override
+    double perimeter() {
+        return 3 * side;
+    }
+}
