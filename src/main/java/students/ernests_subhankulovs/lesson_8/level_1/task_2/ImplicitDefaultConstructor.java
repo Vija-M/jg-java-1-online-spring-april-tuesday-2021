eb718114cac26c4aed49531baf962f5b729b6bea
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-22 12:00:42 +0300

    Lesson 8 - Level 1 - Task 2

---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/level_1/task_2/ImplicitDefaultConstructor.java
@@ -0,0 +1,4 @@
+package students.ernests_subhankulovs.lesson_8.level_1.task_2;
+
+public class ImplicitDefaultConstructor {
+}
---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/level_1/task_2/ImplicitDefaultConstructorDemo.java
@@ -0,0 +1,7 @@
+package students.ernests_subhankulovs.lesson_8.level_1.task_2;
+
+public class ImplicitDefaultConstructorDemo {
+    public static void main(String[] args) {
+        ImplicitDefaultConstructor constructor = new ImplicitDefaultConstructor();
+    }
+}
