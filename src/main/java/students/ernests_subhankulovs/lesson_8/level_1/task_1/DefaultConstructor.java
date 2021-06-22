5cee102b1ae7b78eab8a36bc5a112ed3c420fd48
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-22 11:44:26 +0300

    Lesson 8 - Level 1 - Task 1

---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/level_1/task_1/DefaultConstructor.java
@@ -0,0 +1,7 @@
+package students.ernests_subhankulovs.lesson_8.level_1.task_1;
+
+class DefaultConstructor {
+    DefaultConstructor() {
+        System.out.println("This is a default constructor with no input parameters.");
+    }
+}
---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/level_1/task_1/DefaultConstructorDemo.java
@@ -0,0 +1,7 @@
+package students.ernests_subhankulovs.lesson_8.level_1.task_1;
+
+class DefaultConstructorDemo {
+    public static void main(String[] args) {
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+    }
+}
