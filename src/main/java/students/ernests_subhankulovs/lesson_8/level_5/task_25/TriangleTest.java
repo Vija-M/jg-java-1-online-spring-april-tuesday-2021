a6c77db87aac1fe37252343b4e76ba5d983c9e84
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-22 18:05:17 +0300

    Lesson 8 - Level 4 - Task 25

---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/level_5/task_25/TriangleTest.java
@@ -0,0 +1,29 @@
+package students.ernests_subhankulovs.lesson_8.level_5.task_25;
+
+class TriangleTest {
+
+    Triangle triangle = new Triangle(5);
+
+    public static void main(String[] args) {
+        SquareTest test = new SquareTest();
+        test.testArea();
+        test.testPerimeter();
+    }
+
+    public void testArea() {
+        assertTestResult(10.825317547305483084546539634412, triangle.area(), "Area test");
+    }
+
+    public void testPerimeter() {
+        assertTestResult(15, triangle.perimeter(), "Perimeter test");
+    }
+
+    private void assertTestResult(double expectedResult, double realResult, String testName) {
+        if (expectedResult == realResult) {
+            System.out.println(testName + " = OK");
+        }
+        else {
+            System.out.println(testName + " = FAIL");
+        }
+    }
+}
