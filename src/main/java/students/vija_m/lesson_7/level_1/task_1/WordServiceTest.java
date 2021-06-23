22a109f156b3b66a3e15dc75cebb7db6fff5ce18
Vija-M <vija.riga@gmailcom>
2021-06-23 13:02:56 +0300

    Lesson 7, Level 1, Task 1

--- src/main/java/students/vija_m/lesson_7/level_1/task_1/WordServiceTest.java
+++ src/main/java/students/vija_m/lesson_7/level_1/task_1/WordServiceTest.java
@@ -6,22 +6,22 @@ class WordServiceTest {
     public static void main(String[] args) {
 
         WordServiceTest test = new WordServiceTest();
-        test.EqualWords1();
-        test.EqualWords2();
-        test.EqualWords3();
+        test.equalWords1();
+        test.equalWords2();
+        test.equalWords3();
     }
 
-    public void EqualWords1() {
+    public void equalWords1() {
         String text = "Я люблю море. Я лечу на море. Я умею плавать в море. Какое чистое море! Хочу на море. Завтра поедем на море";
         assertTest("море", wordService.findMostFrequentWord(text), "Six equal words");
     }
 
-    public void EqualWords2() {
+    public void equalWords2() {
         String text = "Я люблю море. Я лечу на море. Я умею плавать в море.";
         assertTest("Я", wordService.findMostFrequentWord(text), "Two words are repeating twice");
     }
 
-    public void EqualWords3() {
+    public void equalWords3() {
         String text = "Я люблю море. Лечу плавать, отдыхать, загарать и любоваться просторами.";
         assertTest("Я", wordService.findMostFrequentWord(text), "No equal words");
     }
