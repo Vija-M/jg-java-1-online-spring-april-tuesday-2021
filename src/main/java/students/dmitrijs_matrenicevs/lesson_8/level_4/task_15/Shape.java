b48b29c859284af7ed5fd7482c4f1001a660e4d6
DeTELo <mrdinas@gmail.com>
2021-06-24 16:42:13 +0300

    Lesson 8 Task-15 - LEVEL4

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_4/task_15/Shape.java
@@ -0,0 +1,14 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_4.task_15;
+
+abstract class Shape {
+
+    private String title;
+
+    Shape(String title) {
+        this.title = title;
+    }
+
+    abstract double calculateArea();
+    abstract double calculatePerimeter();
+
+}
