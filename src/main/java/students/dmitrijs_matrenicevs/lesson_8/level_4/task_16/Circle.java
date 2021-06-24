a5c29fab485b7357939996771b50fb6ff6950b4f
DeTELo <mrdinas@gmail.com>
2021-06-24 16:48:33 +0300

    Lesson 8 Task-16 - LEVEL4

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_4/task_16/Circle.java
@@ -0,0 +1,22 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_4.task_16;
+
+class Circle extends Shape {
+
+    private double radius;
+
+    Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
+
+    }
+
+    @Override
+    double calculateArea() {
+        return 0;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 0;
+    }
+}
