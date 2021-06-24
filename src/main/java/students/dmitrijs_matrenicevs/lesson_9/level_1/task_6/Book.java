de0c8a21ecb2af16f71e82b8765d08d6ee4668fc
DeTELo <mrdinas@gmail.com>
2021-06-24 23:04:55 +0300

    Lesson 9 Task-6 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_1/task_6/Book.java
@@ -0,0 +1,12 @@
+package students.dmitrijs_matrenicevs.lesson_9.level_1.task_6;
+
+class Book {
+
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
