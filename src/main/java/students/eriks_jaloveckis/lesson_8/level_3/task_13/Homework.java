06727b922a71385c201bd3ef841008e170a8cf18
eriks <erikslv88@gmail.com>
2021-06-25 14:44:45 +0300

    Finish - 8 lesson 3 level 13 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_8/level_3/task_13/Homework.java
@@ -0,0 +1,8 @@
+package students.eriks_jaloveckis.lesson_8.level_3.task_13;
+
+abstract class Homework {
+    public void info() {
+        System.out.println("Subject: ");
+    }
+    abstract void subjectName();
+}
---
+++ src/main/java/students/eriks_jaloveckis/lesson_8/level_3/task_13/HomeworkDemo.java
@@ -0,0 +1,14 @@
+package students.eriks_jaloveckis.lesson_8.level_3.task_13;
+
+// Получилась чушь какая то :D
+
+public class HomeworkDemo {
+    public static void main(String[] args) {
+        Biology biology = new Biology();
+        biology.info();
+        biology.subjectName();
+        Sport sport = new Sport();
+        sport.info();
+        sport.subjectName();
+    }
+}
