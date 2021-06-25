2ffc5d24b61176cd0246de5a3be825daba59e20f
eriks <erikslv88@gmail.com>
2021-06-25 17:45:19 +0300

    Finish - 8 lesson 4 level 18 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_8/level_4/task_18/RectangleDemo.java
@@ -0,0 +1,9 @@
+package students.eriks_jaloveckis.lesson_8.level_4.task_18;
+
+class RectangleDemo {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(2,3);
+        System.out.println(rectangle.calculateArea());
+        System.out.println(rectangle.calculatePerimeter());
+    }
+}
