4a3bb2bf6f1081779da3f8209dece5bf7726fdc1
eriks <erikslv88@gmail.com>
2021-06-25 16:57:49 +0300

    Finish - 8 lesson 3 level 16 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_8/level_4/task_16/CircleDemo.java
@@ -0,0 +1,9 @@
+package students.eriks_jaloveckis.lesson_8.level_4.task_16;
+
+class CircleDemo {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3);
+        System.out.println(circle.calculateArea());
+        System.out.println(circle.calculatePerimeter());
+    }
+}
