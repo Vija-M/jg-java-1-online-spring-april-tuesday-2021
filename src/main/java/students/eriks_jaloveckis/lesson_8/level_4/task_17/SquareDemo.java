a4eda11d9198db5e40498e3c564a806ee9ea4267
eriks <erikslv88@gmail.com>
2021-06-25 17:30:51 +0300

    Finish - 8 lesson 3 level 17 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_8/level_4/task_17/SquareDemo.java
@@ -0,0 +1,9 @@
+package students.eriks_jaloveckis.lesson_8.level_4.task_17;
+
+class SquareDemo {
+    public static void main(String[] args) {
+        Square square = new Square(3);
+        System.out.println(square.calculateArea());
+        System.out.println(square.calculatePerimeter());
+    }
+}
