8315a5a9bab116b9f6a8910a1ef6013941e1778d
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-26 18:21:47 +0300

    Lesson 8 - Level 5 - Task 28

---
+++ src/main/java/students/ernests_subhankulovs/lesson_8/level_5/task_28/Rectangle.java
@@ -0,0 +1,32 @@
+package students.ernests_subhankulovs.lesson_8.level_5.task_28;
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    Rectangle(double width, double height) {
+        super("Rectangle");
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    double area() {
+        return width * height;
+    }
+
+    @Override
+    double perimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    String calculateArea() {
+        return width + " * " + height + " = ";
+    }
+
+    @Override
+    String calculatePerimeter() {
+        return "2 * (" + width + " + " + height + " = ";
+    }
+}
