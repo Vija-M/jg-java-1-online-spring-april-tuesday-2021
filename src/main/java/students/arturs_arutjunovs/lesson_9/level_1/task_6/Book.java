33bcb576030b08317369c3d230f5c772b3a60160
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-06-26 22:57:54 +0100

    Completed   Lesson 9 Level 1 Task 6

---
+++ src/main/java/students/arturs_arutjunovs/lesson_9/level_1/task_6/Book.java
@@ -0,0 +1,14 @@
+package students.arturs_arutjunovs.lesson_9.level_1.task_6;
+
+class Book {
+
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+
+        //Можно еще добавить гетерры и сеттеры для полноты картины
+    }
+}
