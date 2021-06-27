fa205756889372ef89a63c49c718ab3f1cf7a282
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-27 14:53:07 +0300

    Lesson 9 - Level 2 - Task 12

---
+++ src/main/java/students/ernests_subhankulovs/lesson_9/level_2/task_12/MainClass.java
@@ -0,0 +1,12 @@
+package students.ernests_subhankulovs.lesson_9.level_2.task_12;
+
+public class MainClass {
+    public static void main(String[] args) {
+        C c = new C();
+    }
+}
+
+//    Result:
+//    Class A Constructor
+//    Class B Constructor
+//    Class C Constructor
