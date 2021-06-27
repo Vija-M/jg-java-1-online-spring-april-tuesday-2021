cceb5bb70c575a1a02aafde7753385f73fc2d84b
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-06-27 20:08:26 +0100

    Completed   Lesson 9 Level 2 Task 11

---
+++ src/main/java/students/arturs_arutjunovs/lesson_9/level_2/task_11/Computer.java
@@ -0,0 +1,16 @@
+package students.arturs_arutjunovs.lesson_9.level_2.task_11;
+
+class Computer {
+
+    private String monitorName;
+    private int monitorSize;
+    private int ssdVolume;
+
+
+
+    protected Computer(String monitorName, int monitorSize, int ssdVolume) {
+        this.monitorName = monitorName;
+        this.monitorSize = monitorSize;
+        this.ssdVolume = ssdVolume;
+    }
+}
