9ef4c8129d9bafe8653e87e392074a5c9902c7bf
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-27 18:29:17 +0300

    Lesson 9 - Level 3 - Task 16

---
+++ src/main/java/students/ernests_subhankulovs/lesson_9/level_3/task_16/SuperClassConstructorInvoker.java
@@ -0,0 +1,9 @@
+package students.ernests_subhankulovs.lesson_9.level_3.task_16;
+
+public class SuperClassConstructorInvoker extends SuperClassConstructor {
+    public SuperClassConstructorInvoker() {
+        super();
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
