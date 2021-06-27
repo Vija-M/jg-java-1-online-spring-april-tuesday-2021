46d673436f39c104a5b2f2ee2520a32f7ec5d11e
Vija-M <vija.riga@gmailcom>
2021-06-27 10:37:16 +0300

    Lesson 7, Level 1, Task 1

--- src/main/java/students/vija_m/lesson_7/level_1/task_1/WordService.java
+++ src/main/java/students/vija_m/lesson_7/level_1/task_1/WordService.java
@@ -7,15 +7,17 @@ class WordService {
         int maxCount = 0;                                 // число макс кол-вал встречающихся слов
         int index = 0;                                    // последний индекс с макс.
         for (int i = 0; i < words.length; i++)
-            for (int j = 0; j < words.length; j++)
+            for (int j = 0; j < words.length; j++) {
                 if (words[i].equals(words[j])) {
                     numOfEqualWords[i]++;                        // заполним массив
                 }
-        for (int i = 0; i < numOfEqualWords.length; i++)
+            }
+        for (int i = 0; i < numOfEqualWords.length; i++) {
             if (numOfEqualWords[i] > maxCount) {
                 maxCount = numOfEqualWords[i];                  // найдем макс.
                 index = i;                                      // найдем индекс с макс.
             }
+        }
         return words[index];
     }
 
