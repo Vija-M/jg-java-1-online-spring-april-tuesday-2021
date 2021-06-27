a6bfd701bc73e0cabbd7941b91a6746be6980a93
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-06-27 20:02:45 +0100

    Completed   Lesson 9 Level 2 Task 13

---
+++ src/main/java/students/arturs_arutjunovs/lesson_9/level_2/task_13/ClassX.java
@@ -0,0 +1,16 @@
+package students.arturs_arutjunovs.lesson_9.level_2.task_13;
+
+class ClassX {
+
+    public ClassX(int i) {
+        System.out.println(1);
+    }
+}
+
+class ClassY extends ClassX {
+    public ClassY() {
+        super(1);
+        System.out.println(2);
+
+    }
+}
