e03987468d7d14c617e8f8940a467be49df11d38
Vija-M <vija.riga@gmailcom>
2021-06-27 21:37:04 +0300

    Lesson-8, Level-3, Task-13

---
+++ src/main/java/students/vija_m/lesson_8/level_3/task_13/Cow.java
@@ -0,0 +1,10 @@
+package students.vija_m.lesson_8.level_3.task_13;
+
+class Cow extends Animal {
+    private String cowColour;
+
+    public Cow(String name, int age) {
+        super(name, age);
+
+    }
+}
