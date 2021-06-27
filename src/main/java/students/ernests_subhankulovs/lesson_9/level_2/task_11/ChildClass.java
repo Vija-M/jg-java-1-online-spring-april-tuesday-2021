67a6c9e7f44b0b0627fc45f48bad7b39f214668a
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-27 14:44:28 +0300

    Lesson 9 - Level 2 - Task 11

---
+++ src/main/java/students/ernests_subhankulovs/lesson_9/level_2/task_11/ChildClass.java
@@ -0,0 +1,5 @@
+package students.ernests_subhankulovs.lesson_9.level_2.task_11;
+
+class ChildClass extends ParentClass {
+    ParentClass parentClass = new ParentClass();
+}
---
+++ src/main/java/students/ernests_subhankulovs/lesson_9/level_2/task_11/ParentClass.java
@@ -0,0 +1,7 @@
+package students.ernests_subhankulovs.lesson_9.level_2.task_11;
+
+class ParentClass {
+    protected ParentClass() {
+
+    }
+}
