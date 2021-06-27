edb0685a4603b1ccc1f322fd743afc56f5440918
Vija-M <vija.riga@gmailcom>
2021-06-27 19:18:26 +0300

    Lesson-8, Level-2, Task-7

---
+++ src/main/java/students/vija_m/lesson_8/level_2/task_7/LadyAnna.java
@@ -0,0 +1,12 @@
+package students.vija_m.lesson_8.level_2.task_7;
+
+class LadyAnna extends Lady {
+    boolean isSheStudying;
+
+    public LadyAnna(String name, String eyesColour, String hairColour, int age, boolean isSheStudying) {
+        super(name, eyesColour, hairColour, age);
+        this.isSheStudying = isSheStudying;
+    }
+
+
+}
