e78543bbfbfc907d8155037f17482f989a6ab4ae
Linda-Ju <linda.junkina@gmail.com>
2021-06-27 23:09:51 +0300

    Lesson 9, Level 1, Task 4

--- src/main/java/students/linda_junkina/lesson_9/level_1/task_4/SafeDemo.java
+++ src/main/java/students/linda_junkina/lesson_9/level_1/task_4/SafeDemo.java
@@ -4,7 +4,7 @@ class SafeDemo {
     public static void main(String[] args) {
         Safe safe = new Safe("1234", 25);
         if (safe.pin.equals("1234")) {
-            System.out.println("You have " + safe.amountOfMoney);
+            System.out.println("You have " + safe.amountOfMoney + "$");
         } else {
             System.out.println("Incorrect password. Try again.");
         }
