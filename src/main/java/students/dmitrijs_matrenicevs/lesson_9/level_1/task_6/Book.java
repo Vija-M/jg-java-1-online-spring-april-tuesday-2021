4cd8b2ffb7ea5cb57c0c8647ec0802e30361b0bf
DeTELo <mrdinas@gmail.com>
2021-06-27 22:48:04 +0300

    Lesson 9 Task-6 - LEVEL1

--- src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_1/task_6/Book.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_1/task_6/Book.java
@@ -5,11 +5,24 @@ class Book {
     private String title;
     private String author;
 
-    private String getTitle() {
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
         return title;
     }
 
-    private String getAuthor() {
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
