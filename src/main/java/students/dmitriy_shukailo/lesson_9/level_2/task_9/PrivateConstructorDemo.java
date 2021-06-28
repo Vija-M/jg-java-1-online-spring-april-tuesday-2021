5eb0a356225379289daadc5906ca960d4c9547de
dk-t <shukailo.d@gmail.com>
2021-06-28 21:14:31 +0300

    Task 9 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_9/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,10 @@
+package students.dmitriy_shukailo.lesson_9.level_2.task_9;
+
+class PrivateConstructorDemo {
+
+    public static void main(String[] args) {
+
+//        PrivateConstructor privateConstructor = new PrivateConstructor();
+    }
+
+}
