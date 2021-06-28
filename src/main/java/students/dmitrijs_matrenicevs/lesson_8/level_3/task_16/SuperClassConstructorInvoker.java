3ef25e62dfd6f012bed3fa8be622c298478591de
DeTELo <mrdinas@gmail.com>
2021-06-28 19:00:52 +0300

    Lesson 9 Task-16 - LEVEL3

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_8/level_3/task_16/SuperClassConstructorInvoker.java
@@ -0,0 +1,10 @@
+package students.dmitrijs_matrenicevs.lesson_8.level_3.task_16;
+
+class SuperClassConstructorInvoker {
+
+    public SuperClassConstructorInvoker(int x) {
+        super();
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
