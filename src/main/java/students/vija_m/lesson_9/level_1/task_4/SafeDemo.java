acc266c1247d8384a2abe3cec673642e54a57078
Vija-M <vija.riga@gmailcom>
2021-06-28 19:27:33 +0300

    Lesson-9, Level-1, Task-4

--- src/main/java/students/vija_m/lesson_9/level_1/task_4/SafeDemo.java
+++ src/main/java/students/vija_m/lesson_9/level_1/task_4/SafeDemo.java
@@ -3,8 +3,11 @@ package students.vija_m.lesson_9.level_1.task_4;
 
 class SafeDemo {
     public static void main(String[] args) {
-        Safe safe = new Safe(1234, 875068.67);
-        System.out.println("Pin code of this safe is: " + safe.pinCode + ". You can open it and take " + safe.moneyAmount + " EUR.");
+        Safe safe = new Safe(1234, 8.67);
+        System.out.println("Pin code of this safe is: " + safe.pinCode + ". You can open it and take only " + safe.moneyAmount + " EUR.");
+        safe.pinCode = 1111;
+        safe.moneyAmount = 9875068.67;
+        System.out.println("Now pin code of this safe is: " + safe.pinCode + ". You can open it and take " + safe.moneyAmount + " EUR.");
     }
 
 }
