1299e313bd5d17a307cee9377c92706c7be60b2d
Vija-M <vija.riga@gmailcom>
2021-06-28 19:53:20 +0300

    Lesson-9, Level-1, Task-6

--- src/main/java/students/vija_m/lesson_9/level_1/task_6/Book.java
+++ src/main/java/students/vija_m/lesson_9/level_1/task_6/Book.java
@@ -4,7 +4,7 @@ class Book {
     private String title;
     private String author;
 
-    public Book(String title, String author) {
+    private Book(String title, String author) {
         this.title = title;
         this.author = author;
     }
