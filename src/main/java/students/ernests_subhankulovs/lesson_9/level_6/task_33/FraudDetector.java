d0056ac16d9b70951bb39ad161eb944b53f6ceca
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-28 12:39:39 +0300

    Lesson 9 - Level 6 - Task 33

---
+++ src/main/java/students/ernests_subhankulovs/lesson_9/level_6/task_33/FraudDetector.java
@@ -0,0 +1,23 @@
+package students.ernests_subhankulovs.lesson_9.level_6.task_33;
+
+class FraudDetector {
+
+    FraudRule[] fraudRules;
+
+    FraudDetector(FraudRule[] fraudRules) {
+        this.fraudRules = fraudRules;
+    }
+
+    FraudDetectionResult isFraud(Transaction t) {
+        for (FraudRule rule : fraudRules) {
+            if (rule.isFraud(t)) {
+                System.out.println("Fraud detected!");
+                System.out.println("Rule: " + rule.getRuleName());
+                System.out.println(t.toString());
+                return new FraudDetectionResult(true, rule.getRuleName());
+            }
+        }
+        return new FraudDetectionResult(false, "Not Fraud");
+    }
+
+}
