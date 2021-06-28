b06d6dd95db8b622e7f807ce2dd3b042c1ebee3a
DeTELo <mrdinas@gmail.com>
2021-06-28 17:46:10 +0300

    Lesson 9 Task-5 - LEVEL1

--- src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_1/task_5/Safe.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_1/task_5/Safe.java
@@ -4,41 +4,32 @@ import java.util.Scanner;
 
 class Safe {
 
-    public int money;
-    public String password;
+    private double money;
+    private final String password;
 
-    public Safe(int money, String password) {
+    public Safe(double money, String password) {
         this.money = money;
         this.password = password;
     }
 
-    public boolean getMoney() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите пароль, чтобы забрать деньги из сейфа: ");
-        int getMoney = scanner.nextInt();
-
-        while (getMoney != 222) {
-            System.out.println("Попробуйте снова!");
-            getMoney = scanner.nextInt();
-        }
-        System.out.println("Поздравляю, теперь все деньги твои!");
-        return false;
+    public double getMoney (){
+        return money;
     }
 
-    public boolean putMoney() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println();
-        System.out.print("Введите пароль, чтобы положить деньги в сейф: ");
-        int putMoney = scanner.nextInt();
-
-        while (putMoney != 222) {
-            System.out.println("Попробуйте снова!");
-            putMoney = scanner.nextInt();
+    public void retMoney(double money, String password){
+        if (this.password.equals(password)) {
+            this.money -= money;
         }
-        System.out.println("Поздравляю, теперь все деньги в сейфе!");
-        return false;
+        else {
+            System.out.println("Введи пароль!");
+        }
+    }
+
+    public void putMoney(double money, String password) {
+        if (this.password.equals(password)) {
+            this.money += money;
+        } else {
+            System.out.println("Введи пароль!");
+        }
     }
 }
-
-//Обьясните пожалуйста, как убрать false, чтобы оно не отображалось на выходе.
-//И как сделать так, чтобы когда я ввожу неправильный пароль он мне писал по новой: Введите пароль, чтобы..
--- src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_1/task_5/SafeDemo.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_1/task_5/SafeDemo.java
@@ -4,9 +4,9 @@ class SafeDemo {
 
     public static void main(String[] args) {
         Safe safe = new Safe(500,"222");
-        System.out.println("У тебя в сейфе " + safe.money + "$");
-        System.out.println("Твой пароль от сейфа: " + safe.password);
-        System.out.println(safe.getMoney());
-        System.out.println(safe.putMoney());
+        System.out.println("У тебя в сейфе " + safe.getMoney());
+        System.out.println("Твой пароль от сейфа:");
+        safe.retMoney(500,"222");
+        System.out.println("Введи пароль:");
     }
 }
