f3dd4874b5243652f76d9bf219fb49dc830578fe
DeTELo <mrdinas@gmail.com>
2021-06-28 18:58:08 +0300

    Lesson 9 Task-13 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_2/task_13/ClassY.java
@@ -0,0 +1,9 @@
+package students.dmitrijs_matrenicevs.lesson_9.level_2.task_13;
+
+class ClassY extends ClassX {
+
+    public ClassY() {
+        super(2);
+        System.out.println(2);
+    }
+}
