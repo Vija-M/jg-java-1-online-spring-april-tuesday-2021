cd39bcabec30e5c5e139de6e0fb981d047969a0c
dk-t <shukailo.d@gmail.com>
2021-06-28 22:28:10 +0300

    Task 11 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_9/level_2/task_11/Parents.java
@@ -0,0 +1,13 @@
+package students.dmitriy_shukailo.lesson_9.level_2.task_11;
+
+class Parents {
+
+    private String title;
+    private String author;
+
+    protected Parents(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+}
