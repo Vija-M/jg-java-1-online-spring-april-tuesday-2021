31e032e1b9fc4baae51a0e84f9d4a0e158fc86bb
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-06-28 12:29:22 +0300

    Lesson 9 - Level 6 - Task 32

---
+++ src/main/java/students/ernests_subhankulovs/lesson_9/level_6/task_32/FraudDetector.java
@@ -0,0 +1,20 @@
+package students.ernests_subhankulovs.lesson_9.level_6.task_32;
+
+class FraudDetector {
+
+    FraudRule[] fraudRules;
+
+    FraudDetector(FraudRule[] fraudRules) {
+        this.fraudRules = fraudRules;
+    }
+
+    FraudDetectionResult isFraud(Transaction t) {
+        for (FraudRule rule : fraudRules) {
+            if (rule.isFraud(t)) {
+                return new FraudDetectionResult(true, rule.getRuleName());
+            }
+        }
+        return new FraudDetectionResult(false, "Not Fraud");
+    }
+
+}
