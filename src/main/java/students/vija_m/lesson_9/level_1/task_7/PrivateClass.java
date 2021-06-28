2c3efcdeccccc3813064a8b53a5f1888e07a7e94
Vija-M <vija.riga@gmailcom>
2021-06-28 19:49:16 +0300

    Lesson-9, Level-1, Task-7

---
+++ src/main/java/students/vija_m/lesson_9/level_1/task_7/PrivateClass.java
@@ -0,0 +1,11 @@
+package students.vija_m.lesson_9.level_1.task_7;
+
+class PrivateClass {
+    private String title;
+    private String author;
+
+    private PrivateClass(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
