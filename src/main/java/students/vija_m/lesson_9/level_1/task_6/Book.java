0fd0caf863f9ab80599db28e15c9e5544dcfb7be
Vija-M <vija.riga@gmailcom>
2021-06-28 19:38:38 +0300

    Lesson-9, Level-1, Task-6

---
+++ src/main/java/students/vija_m/lesson_9/level_1/task_6/Book.java
@@ -0,0 +1,28 @@
+package students.vija_m.lesson_9.level_1.task_6;
+
+class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
+
