bca4adf070178aa4e44047882736e9adadcbb8b5
Linda-Ju <linda.junkina@gmail.com>
2021-06-28 11:10:57 +0300

    Lesson 8, Level 5, Task 20,21,22,23,24,25,26,27,28

---
+++ src/main/java/students/linda_junkina/lesson_8/level_5/task_20_21_22_23_24_25_26_27_28/Circle.java
@@ -0,0 +1,23 @@
+package students.linda_junkina.lesson_8.level_5.task_20_21_22_23_24_25_26_27_28;
+
+
+class Circle extends Shape {
+
+    private double radius;
+    double pi = 3.14;
+
+    public Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    @Override
+    double calculateArea() {
+        return pi * (radius * radius);
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return pi * 2 * radius;
+    }
+}
