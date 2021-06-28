bc296e5dac60e694f4d03af4847a6dfaee4d8133
DeTELo <mrdinas@gmail.com>
2021-06-28 18:24:52 +0300

    Lesson 9 Task-9 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_2/task_9/PrivateConstructor.java
@@ -0,0 +1,7 @@
+package students.dmitrijs_matrenicevs.lesson_9.level_2.task_9;
+
+class PrivateConstructor {
+
+    private PrivateConstructor() {
+    }
+}
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,9 @@
+package students.dmitrijs_matrenicevs.lesson_9.level_2.task_9;
+
+class PrivateConstructorDemo {
+
+    public static void main(String[] args) {
+
+        //PrivateConstructor privateConstructor = new PrivateConstructor();
+    }
+}
