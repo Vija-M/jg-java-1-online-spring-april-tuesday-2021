83cf4d426be8041910a317869f3a721c4f2c5f4e
DeTELo <mrdinas@gmail.com>
2021-06-28 18:07:37 +0300

    Lesson 9 Task-5 - LEVEL1

--- src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_1/task_5/Safe.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_9/level_1/task_5/Safe.java
@@ -1,7 +1,5 @@
 package students.dmitrijs_matrenicevs.lesson_9.level_1.task_5;
 
-import java.util.Scanner;
-
 class Safe {
 
     private double money;
@@ -12,24 +10,19 @@ class Safe {
         this.password = password;
     }
 
-    public double getMoney (){
+    public double getMoney() {
         return money;
     }
 
-    public void retMoney(double money, String password){
+    public void retMoney(double money, String password) {
         if (this.password.equals(password)) {
             this.money -= money;
-        }
-        else {
-            System.out.println("Введи пароль!");
         }
     }
 
     public void putMoney(double money, String password) {
         if (this.password.equals(password)) {
             this.money += money;
-        } else {
-            System.out.println("Введи пароль!");
         }
     }
 }
