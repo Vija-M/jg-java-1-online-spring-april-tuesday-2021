23bdfad044053409e99c20e9e4d70082f33cdc50
dk-t <shukailo.d@gmail.com>
2021-06-28 22:38:07 +0300

    Task 12 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_9/level_2/task_12/B.java
@@ -0,0 +1,9 @@
+package students.dmitriy_shukailo.lesson_9.level_2.task_12;
+
+class B extends A {
+
+    public B() {
+        System.out.println("Class B Constructor");
+    }
+
+}
---
+++ src/main/java/students/dmitriy_shukailo/lesson_9/level_2/task_12/C.java
@@ -0,0 +1,8 @@
+package students.dmitriy_shukailo.lesson_9.level_2.task_12;
+
+class C extends B {
+
+    public C() {
+        System.out.println("Class C Constructor");
+    }
+}
