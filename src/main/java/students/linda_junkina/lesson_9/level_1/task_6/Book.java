c7825c5bf33831d521e5087c0eba9e9d95a87232
Linda-Ju <linda.junkina@gmail.com>
2021-06-29 16:32:19 +0300

    Lesson 9, Level 1, Task 6

---
+++ src/main/java/students/linda_junkina/lesson_9/level_1/task_6/Book.java
@@ -0,0 +1,14 @@
+package students.linda_junkina.lesson_9.level_1.task_6;
+
+class Book {
+    private String title;
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
