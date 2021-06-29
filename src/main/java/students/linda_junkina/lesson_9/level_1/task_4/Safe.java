52e64f0976d44f8ca92bdfada63b8c22e14c1519
Linda-Ju <linda.junkina@gmail.com>
2021-06-29 16:18:35 +0300

    Lesson 9, Level 1, Task 4

--- src/main/java/students/linda_junkina/lesson_9/level_1/task_4/Safe.java
+++ src/main/java/students/linda_junkina/lesson_9/level_1/task_4/Safe.java
@@ -1,11 +1,6 @@
 package students.linda_junkina.lesson_9.level_1.task_4;
 
 class Safe {
-    public String pin;
-    public double amountOfMoney;
-
-    public Safe(String pin, double amountOfMoney) {
-        this.pin = pin;
-        this.amountOfMoney = amountOfMoney;
-    }
+    public String pin = "1234";
+    public double amountOfMoney = 25;
 }
--- src/main/java/students/linda_junkina/lesson_9/level_1/task_4/SafeDemo.java
+++ src/main/java/students/linda_junkina/lesson_9/level_1/task_4/SafeDemo.java
@@ -2,11 +2,8 @@ package students.linda_junkina.lesson_9.level_1.task_4;
 
 class SafeDemo {
     public static void main(String[] args) {
-        Safe safe = new Safe("1234", 25);
-        if (safe.pin.equals("1234")) {
-            System.out.println("You have " + safe.amountOfMoney + "$");
-        } else {
-            System.out.println("Incorrect password. Try again.");
-        }
+        Safe safe = new Safe();
+        safe.pin = "9876";
+        safe.amountOfMoney = 100;
     }
 }
