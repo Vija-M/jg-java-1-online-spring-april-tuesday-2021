a4116b531201fb00d4253c576443d5ca2a503c90
Linda-Ju <linda.junkina@gmail.com>
2021-06-30 12:57:05 +0300

    Lesson 9, Level 2, Task 11

---
+++ src/main/java/students/linda_junkina/lesson_9/level_2/task_11/Parent.java
@@ -0,0 +1,11 @@
+package students.linda_junkina.lesson_9.level_2.task_11;
+
+class Parent {
+    protected String name;
+    protected int age;
+
+    protected Parent(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
