4df3989402265930bef12ab15e82c2ec730281da
dk-t <shukailo.d@gmail.com>
2021-06-30 22:05:31 +0300

    Task 1 has been corrected

--- src/main/java/students/dmitriy_shukailo/lesson_10/level_1/task_1/JavaStarLoginService.java
+++ src/main/java/students/dmitriy_shukailo/lesson_10/level_1/task_1/JavaStarLoginService.java
@@ -2,4 +2,6 @@ package students.dmitriy_shukailo.lesson_10.level_1.task_1;
 
 interface JavaStarLoginService {
 
+    boolean canLogin(String login, String password);
+
 }
--- src/main/java/students/dmitriy_shukailo/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
+++ src/main/java/students/dmitriy_shukailo/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
@@ -2,6 +2,7 @@ package students.dmitriy_shukailo.lesson_10.level_1.task_1;
 
 class JavaStarLoginServiceImpl implements JavaStarLoginService {
 
+    @Override
     public boolean canLogin(String login, String password) {
         return (login != null)
                 && login.equals("javastar")
