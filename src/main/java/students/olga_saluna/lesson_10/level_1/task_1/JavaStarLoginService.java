2c18cb8ab95f231fba4b8bc9d567b66ab12014c2
salun <salun.alex@gmail.com>
2021-06-30 19:02:11 +0300

    Task 1, level 1, lesson 10

---
+++ src/main/java/students/olga_saluna/lesson_10/level_1/task_1/JavaStarLoginService.java
@@ -0,0 +1,5 @@
+package students.olga_saluna.lesson_10.level_1.task_1;
+
+interface JavaStarLoginService {
+    void canLogin();
+}
---
+++ src/main/java/students/olga_saluna/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
@@ -0,0 +1,18 @@
+package students.olga_saluna.lesson_10.level_1.task_1;
+
+class JavaStarLoginServiceImpl implements JavaStarLoginService {
+
+    public boolean canLogin(String login, String password) {
+        return (login != null)
+                && login.equals("javastar")
+                && (password != null)
+                && password.equals("pass");
+    }
+
+
+    @Override
+    public void canLogin() {
+
+    }
+}
+
