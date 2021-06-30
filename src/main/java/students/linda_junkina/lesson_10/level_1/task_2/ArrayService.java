b9398590cbe163e920115200ce59c5841c70e08a
Linda-Ju <linda.junkina@gmail.com>
2021-06-30 16:19:24 +0300

    Lesson 10, Level 1, Task 2

---
+++ src/main/java/students/linda_junkina/lesson_10/level_1/task_2/ArrayService.java
@@ -0,0 +1,9 @@
+package students.linda_junkina.lesson_10.level_1.task_2;
+
+interface ArrayService {
+    int[] createArray(int arrayLength);
+    void fillArrayWithRandomNumbers(int[] array);
+    void printArrayToConsole (int[] array);
+    int findMaxNumber(int[] array);
+    int findMinNumber(int[] array);
+}
