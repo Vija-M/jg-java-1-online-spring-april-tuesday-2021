f2740022d16293022af071e3fbace6aec3f9830d
salun <salun.alex@gmail.com>
2021-06-30 19:10:45 +0300

    Lesson 10, level 1, task 2

---
+++ src/main/java/students/olga_saluna/lesson_10/level_1/task_2/ArrayService.java
@@ -0,0 +1,8 @@
+package students.olga_saluna.lesson_10.level_1.task_2;
+
+interface ArrayService {
+    void fillArrayWithRandomNumbers();
+    void printArrayToConsole();
+    void findMaxNumber();
+    void findMinNumber();
+}
---
+++ src/main/java/students/olga_saluna/lesson_10/level_1/task_2/DefaultArrayService.java
@@ -0,0 +1,60 @@
+package students.olga_saluna.lesson_10.level_1.task_2;
+
+import java.util.Random;
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+class DefaultArrayService implements ArrayService {
+    public int[] createArray(int arrayLength) {
+        return new int[arrayLength];
+    }
+
+    public void fillArrayWithRandomNumbers(int[] array) {
+        for (int count = 0; count < array.length; count++) {
+            int rndValue = new Random().nextInt(11);
+            array[count] = rndValue;
+        }
+    }
+
+    public void printArrayToConsole(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+
+    public int findMaxNumber(int[] array) {
+        int maxValue = -1;
+        OptionalInt max = Arrays.stream(array).max();
+        if (max.isPresent()) {
+            maxValue = max.getAsInt();
+        }
+        return maxValue;
+    }
+
+    public int findMinNumber(int[] array) {
+        int minValue = -1;
+        OptionalInt min = Arrays.stream(array).min();
+        if (min.isPresent()) {
+            minValue = min.getAsInt();
+        }
+        return minValue;
+    }
+
+    @Override
+    public void fillArrayWithRandomNumbers() {
+
+    }
+
+    @Override
+    public void printArrayToConsole() {
+
+    }
+
+    @Override
+    public void findMaxNumber() {
+
+    }
+
+    @Override
+    public void findMinNumber() {
+
+    }
+}
