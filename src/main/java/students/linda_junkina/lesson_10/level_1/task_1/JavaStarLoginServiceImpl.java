f05d72823a20b7b019916fabccc78078ca36b916
Linda-Ju <linda.junkina@gmail.com>
2021-06-30 15:25:28 +0300

    Lesson 10, Level 1, Task 1

---
+++ src/main/java/students/linda_junkina/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
@@ -0,0 +1,10 @@
+package students.linda_junkina.lesson_10.level_1.task_1;
+
+class JavaStarLoginServiceImpl implements JavaStarLoginService {
+    public boolean canLogin(String login, String password) {
+        return (login != null)
+                && login.equals("javastar")
+                && (password != null)
+                && password.equals("pass");
+    }
+}
