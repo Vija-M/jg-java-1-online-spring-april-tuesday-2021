3d0e06f6f70f54dc6efd08bea75f540ef60a93a6
dk-t <shukailo.d@gmail.com>
2021-06-30 23:01:48 +0300

    Task 3 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_10/level_1/task_3/SimpleList.java
@@ -0,0 +1,38 @@
+package students.dmitriy_shukailo.lesson_10.level_1.task_3;
+
+interface SimpleList {
+
+    void createList();
+// создает список
+
+    void deleteList();
+// удаляет список
+
+    void copyList();
+// копирует список
+
+    void printList();
+// печатает список
+
+    void addElementToList();
+// добавляет элемент в список
+
+    void removeElementFromList();
+// удаляет элемент из списка
+
+    void sortElementUp();
+// сортирует элементы по возрастанию
+
+    void sortElementDown();
+// сортирует элементы по убыванию
+
+    void findMaxElement();
+// находит максимальный элемент
+
+    void findMinElement();
+// находит минимальный элемент
+
+    void findEqualsElement();
+// находит эквивалентые элементы
+
+}
