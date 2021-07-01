8cd17c620a12a3b1ae6adc979cacb92c440c109a
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-01 21:28:51 +0300

    Lesson 10 - Level 4 - Task 11. Package name corrected.

--- src/main/java/students/ernests_subhankulovs/lesson_10/level_4/task_11/KelvinTemperature.java
+++ src/main/java/students/ernests_subhankulovs/lesson_10/level_4/task_11/KelvinTemperature.java
@@ -1,4 +1,4 @@
-package students.ernests_subhankulovs.lesson_10.level_3.task_11;
+package students.ernests_subhankulovs.lesson_10.level_4.task_11;
 
 class KelvinTemperature implements Temperature{
     @Override
