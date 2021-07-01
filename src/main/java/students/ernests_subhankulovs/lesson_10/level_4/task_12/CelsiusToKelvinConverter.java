f150770288740b23c2cf82bea0878a2b094ae48e
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-01 16:55:56 +0300

    Lesson 10 - Level 1 - Task 3. Correction 2/2.

--- src/main/java/students/ernests_subhankulovs/lesson_10/level_4/task_12/CelsiusToKelvinConverter.java
+++
@@ -1,8 +0,0 @@
-package students.ernests_subhankulovs.lesson_10.level_4.task_12;
-
-class CelsiusToKelvinConverter implements TemperatureConverter {
-    @Override
-    public double convert(double celsiusTemperature) {
-        return celsiusTemperature + 273.15;
-    }
-}
