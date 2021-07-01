d621920b3dbb77fed7f0f3958d4d6f6d0b1c6b61
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-01 15:54:21 +0300

    Lesson 10 - Level 2 - Task 5. Corrected.

--- src/main/java/students/ernests_subhankulovs/lesson_10/level_2/task_5/DayOfTheWeekDetectorDemo.java
+++ src/main/java/students/ernests_subhankulovs/lesson_10/level_2/task_5/DayOfTheWeekDetectorDemo.java
@@ -4,33 +4,29 @@ import java.util.Scanner;
 
 class DayOfTheWeekDetectorDemo {
 
-    Scanner scanner = new Scanner(System.in);
     DayOfTheWeekDetector detector;
-
-    DayOfTheWeekDetectorDemo() {
-
-    }
+    static int number;
 
     DayOfTheWeekDetectorDemo(DayOfTheWeekDetector detector) {
         this.detector = detector;
     }
 
     public static void main(String[] args) {
-        DayOfTheWeekDetectorDemo detectorDemo = new DayOfTheWeekDetectorDemo();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 7:");
+        number = scanner.nextInt();
+        System.out.println("Day of the week detected using the if-else method:");
+        DayOfTheWeekDetectorDemo detectorDemo = new DayOfTheWeekDetectorDemo(new IfDayOfTheWeekDetector());
+        detectorDemo.run();
+        System.out.println("Day of the week detected using the switch method:");
+        detectorDemo = new DayOfTheWeekDetectorDemo(new SwitchDayOfTheWeekDetector());
+        detectorDemo.run();
+        System.out.println("Day of the week detected using a string array of day names:");
+        detectorDemo = new DayOfTheWeekDetectorDemo(new StringArrayDayOfTheWeekDetector());
         detectorDemo.run();
     }
 
     public void run() {
-        System.out.println("Enter a number between 1 and 7:");
-        int number = scanner.nextInt();
-        System.out.println("Day of the week detected using the if-else method:");
-        DayOfTheWeekDetectorDemo detectorDemo = new DayOfTheWeekDetectorDemo(new IfDayOfTheWeekDetector());
-        System.out.println(detectorDemo.detector.detectDayName(number));
-        System.out.println("Day of the week detected using the switch method:");
-        detectorDemo = new DayOfTheWeekDetectorDemo(new SwitchDayOfTheWeekDetector());
-        System.out.println(detectorDemo.detector.detectDayName(number));
-        System.out.println("Day of the week detected using a string array of day names:");
-        detectorDemo = new DayOfTheWeekDetectorDemo(new StringArrayDayOfTheWeekDetector());
-        System.out.println(detectorDemo.detector.detectDayName(number));
+        System.out.println(detector.detectDayName(number));
     }
 }
