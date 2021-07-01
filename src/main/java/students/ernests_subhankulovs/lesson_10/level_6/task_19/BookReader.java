35804ecfc9cdd07bab82ec4d080eeaf0fd3c3067
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-01 22:30:13 +0300

    Lesson 10 - Level 6 - Task 19

---
+++ src/main/java/students/ernests_subhankulovs/lesson_10/level_6/task_19/BookReader.java
@@ -0,0 +1,12 @@
+package students.ernests_subhankulovs.lesson_10.level_6.task_19;
+
+interface BookReader {
+    boolean addBook(Book book);
+    boolean isDuplicate(Book book);
+    boolean validateTitleAndAuthor(Book book);
+    boolean removeBook(Book book);
+    String[] listAllBooks();
+    Book[] findBookByAuthor(String author);
+    Book[] findBookByBeginningOfAuthorName(String author);
+    Book[] findBookByTitle(String title);
+}
