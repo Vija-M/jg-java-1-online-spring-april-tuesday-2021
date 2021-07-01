14ee720c1ded27ed4eaf3a6b615b38f953e811ed
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-01 19:56:14 +0300

    Lesson 10 - Level 5 - Task 14

---
+++ src/main/java/students/ernests_subhankulovs/lesson_10/level_5/task_14/BookReader.java
@@ -0,0 +1,7 @@
+package students.ernests_subhankulovs.lesson_10.level_5.task_14;
+
+interface BookReader {
+    boolean addBook(Book book);
+    boolean isDuplicate(Book book);
+    boolean validateTitleAndAuthor(Book book);
+}
---
+++ src/main/java/students/ernests_subhankulovs/lesson_10/level_5/task_14/BookReaderImpl.java
@@ -0,0 +1,37 @@
+package students.ernests_subhankulovs.lesson_10.level_5.task_14;
+
+class BookReaderImpl implements BookReader {
+
+    Book[] electronicLibrary = new Book[1];
+
+    @Override
+    public boolean addBook(Book book) {
+        if (electronicLibrary[0] != null && !isDuplicate(book) && validateTitleAndAuthor(book)) {
+            Book[] newBooks = new Book[electronicLibrary.length + 1];
+            System.arraycopy(electronicLibrary, 0, newBooks, 0, electronicLibrary.length);
+            newBooks[newBooks.length - 1] = book;
+            electronicLibrary = newBooks;
+            return true;
+        }
+        else {
+            electronicLibrary[0] = book;
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isDuplicate(Book newBook) {
+        for (Book savedBook : electronicLibrary) {
+            if (savedBook.getAuthor().equals(newBook.getAuthor()) &&
+                savedBook.getTitle().equals(newBook.getTitle())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean validateTitleAndAuthor(Book book) {
+        return (!book.getAuthor().trim().equals("") && !book.getTitle().trim().equals(""));
+    }
+}
