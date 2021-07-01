acec03f17599f3d606050e5e468fcd4d4c69367c
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-01 16:29:14 +0300

    Lesson 10 - Level 3 - Task 10. Correction 3/4.

--- src/main/java/students/ernests_subhankulovs/lesson_10/level_3/task_10/TestInMemoryProductRepository.java
+++ src/main/java/students/ernests_subhankulovs/lesson_10/level_3/task_10/TestInMemoryProductRepository.java
@@ -12,26 +12,31 @@ public class TestInMemoryProductRepository {
     }
 
     public void testProductSearchAtStart() {
-        assertTestResult("Product A", getActualProductTitle("Product A"),
-                "Test product search at the beginning of the repository");
+        if (checkIfItemExists("Product A")) {
+            assertTestResult("Product A", getActualProductTitle("Product A"),
+                    "Test product search at the beginning of the repository");
+        }
+
     }
 
     public void testProductSearchInTheMiddle() {
-        InMemoryProductRepository repository = createProductRepository();
-        assertTestResult("Product B", getActualProductTitle("Product B"),
-                "Test product search in the middle of the repository");
+        if (checkIfItemExists("Product B")) {
+            assertTestResult("Product B", getActualProductTitle("Product B"),
+                    "Test product search in the middle of the repository");
+        }
     }
 
     public void testProductSearchAtTheEnd() {
-        InMemoryProductRepository repository = createProductRepository();
-        assertTestResult("Product C", getActualProductTitle("Product C"),
-                "Test product search at the end of the repository");
+        if (checkIfItemExists("Product C")) {
+            assertTestResult("Product C", getActualProductTitle("Product C"),
+                    "Test product search at the end of the repository");
+        }
     }
 
     public void testNonExistingProductSearch() {
         InMemoryProductRepository repository = createProductRepository();
-        assertTestResult("", getActualProductTitle("Product D"),
-                "Test non-existing product search");
+        Optional<Product> repositoryEntry = repository.findByTitle("Product D");
+        assertFalse(repositoryEntry.isPresent(), "Test non-existing product search");
     }
 
     public InMemoryProductRepository createProductRepository() {
@@ -44,17 +49,36 @@ public class TestInMemoryProductRepository {
 
     public String getActualProductTitle(String productTitle) {
         InMemoryProductRepository repository = createProductRepository();
-        Optional<Product> repositoryEntry = repository.findByTitle(productTitle);
-        if (repositoryEntry.isPresent()) {
-            return repository.findByTitle(productTitle).get().getTitle();
-        }
-        else {
-            return "";
-        }
+        return repository.findByTitle(productTitle).get().getTitle();
+    }
+
+    public boolean checkIfItemExists(String productTitle) {
+        InMemoryProductRepository repository = createProductRepository();
+        return assertTrue(repository.findByTitle(productTitle).isPresent(), "Test if item \"" + productTitle + "\" exists");
     }
 
     private void assertTestResult(String  expectedResult, String realResult, String testName) {
         if (expectedResult.equals(realResult)) {
+            System.out.println(testName + " = OK");
+        }
+        else {
+            System.out.println(testName + " = FAIL");
+        }
+    }
+
+    private boolean assertTrue(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " = OK");
+            return true;
+        }
+        else {
+            System.out.println(testName + " = FAIL");
+            return false;
+        }
+    }
+
+    private void assertFalse(boolean condition, String testName) {
+        if (!condition) {
             System.out.println(testName + " = OK");
         }
         else {
