2c4d774301988fa62acb56afebd52b2ba7316286
Linda-Ju <linda.junkina@gmail.com>
2021-07-02 14:01:26 +0300

    Lesson 10, Level 1, Task 3

--- src/main/java/students/linda_junkina/lesson_10/level_1/task_3/SimpleList.java
+++ src/main/java/students/linda_junkina/lesson_10/level_1/task_3/SimpleList.java
@@ -1,13 +1,13 @@
 package students.linda_junkina.lesson_10.level_1.task_3;
 
 interface SimpleList<T> {
-    // добавить елемент на место с указанным индексом
+    // добавить элемент на место с указанным индексом
     void addElement(int i, T element);
 
-    // удалить элемент под указанным индексом
+    // удалить элемент с указанным индексом
     void deleteElement(int i);
 
-    // изменить элемент с конкретным индексом
+    // изменить элемент с указанным индексом
     void changeElement(T element, int i);
 
     // поменять элементы местами
