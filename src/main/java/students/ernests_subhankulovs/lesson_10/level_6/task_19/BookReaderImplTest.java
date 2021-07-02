aef016760444cd07c778075a346831d41b8e54ce
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-02 17:13:31 +0300

    Lesson 10 - Level 5 - Task 19. Corrected.

--- src/main/java/students/ernests_subhankulovs/lesson_10/level_6/task_19/BookReaderImplTest.java
+++ src/main/java/students/ernests_subhankulovs/lesson_10/level_6/task_19/BookReaderImplTest.java
@@ -5,6 +5,7 @@ class BookReaderImplTest {
         BookReaderImplTest test = new BookReaderImplTest();
         test.addNewBookTest();
         test.addDuplicateBookTest();
+        test.addDuplicateBookTest2();
         test.addBookWithoutAuthorTest();
         test.addBookWithoutTitleTest();
         test.removeExistingBookTest();
@@ -21,11 +22,11 @@ class BookReaderImplTest {
         boolean realResult = bookReader.addBook(new Book("Design Patterns: Elements of Reusable Object-Oriented Software", "Gang of Four"));
         assertLogicalTestResult(true, realResult, "Successfully added book test");
         if (realResult) {
-            assertTestResult("Gang of Four", bookReader.getElectronicLibrary()[1].getAuthor(),
+            Book book = bookReader.getElectronicLibrary()[1];
+            assertTestResult("Gang of Four", book.getAuthor(),
                     "Added author test");
-            assertTestResult("Design Patterns: Elements of Reusable Object-Oriented Software", bookReader.getElectronicLibrary()[1].getTitle(),
+            assertTestResult("Design Patterns: Elements of Reusable Object-Oriented Software", book.getTitle(),
                     "Added book title test");
-            assertNumericTestResult(2, bookReader.getElectronicLibrary().length, "Size of library increased test");
         }
     }
 
@@ -34,6 +35,22 @@ class BookReaderImplTest {
         bookReader.addBook(new Book("Clean Code: A Handbook of Agile Software Craftsmanship", "Robert C. Martin"));
         boolean realResult = bookReader.addBook(new Book("Clean Code: A Handbook of Agile Software Craftsmanship", "Robert C. Martin"));
         assertLogicalTestResult(false, realResult, "Unable to add duplicate book test");
+    }
+
+    public void addDuplicateBookTest2() {
+        BookReader reader = new BookReaderImpl();
+        reader.addBook(new Book("The C Programming Language", "K&R"));
+        reader.addBook(new Book("The Lord of the Rings", "John Tolkien"));
+        reader.addBook(new Book("A Game of Thrones", "George Martin"));
+        reader.addBook(new Book("The Lord of the Rings", "John Tolkien"));
+
+        for (Book book : reader.getElectronicLibrary()) {
+            System.out.println("Title: " + book.getTitle() + ", Author: " + book.getAuthor());
+        }
+        assertNumericTestResult(3 ,reader.getElectronicLibrary().length, "Number of books in library after duplicate check test");
+        assertTestResult("The C Programming Language", reader.getElectronicLibrary()[0].getTitle(), "1st book after duplicate check test");
+        assertTestResult("The Lord of the Rings", reader.getElectronicLibrary()[1].getTitle(), "2nd book after duplicate check test");
+        assertTestResult("A Game of Thrones", reader.getElectronicLibrary()[2].getTitle(), "3rd book after duplicate check test");
     }
 
     public void addBookWithoutAuthorTest() {
