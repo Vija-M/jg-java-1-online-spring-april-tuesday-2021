807ba6c7888747ae349817e802535c35896273dd
eriks <erikslv88@gmail.com>
2021-07-02 16:23:44 +0300

    Finish - 9 lesson 2 level 8 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_9/level_2/task_8/ProtectedClass.java
@@ -0,0 +1,9 @@
+package students.eriks_jaloveckis.lesson_9.level_2.task_8;
+
+class ProtectedClass {
+
+    protected String title;
+    protected String author;
+
+// - ну я считаю, что также нету смысла скорее всего, как я понял, он работает только в одном пакете и в подклассах.
+}
