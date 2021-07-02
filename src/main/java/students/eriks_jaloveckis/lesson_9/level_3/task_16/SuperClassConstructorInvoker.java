17b13140ae4b6a48ae0f1552185d9f0aecd0d3f5
eriks <erikslv88@gmail.com>
2021-07-02 17:18:57 +0300

    Finish - 9 lesson 3 level 16 task

---
+++ src/main/java/students/eriks_jaloveckis/lesson_9/level_3/task_16/SuperClassConstructorInvoker.java
@@ -0,0 +1,8 @@
+package students.eriks_jaloveckis.lesson_9.level_3.task_16;
+
+class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
