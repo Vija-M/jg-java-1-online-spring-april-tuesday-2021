ff14a5daf9000e66e8fc851d2a2c82126981872f
Linda-Ju <linda.junkina@gmail.com>
2021-07-02 13:39:05 +0300

    Lesson 10, Level 3, Task 7

---
+++ src/main/java/students/linda_junkina/lesson_10/level_3/task_7/Product.java
@@ -0,0 +1,13 @@
+package students.linda_junkina.lesson_10.level_3.task_7;
+
+class Product {
+    private final String title;
+
+    public Product(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
