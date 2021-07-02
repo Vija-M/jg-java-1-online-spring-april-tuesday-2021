3dd28159248d4362803d809edbaef9c5acfdbae7
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-02 17:07:20 +0300

    Lesson 10 - Level 5 - Task 18. Corrected.

--- src/main/java/students/ernests_subhankulovs/lesson_10/level_5/task_18/BookReader.java
+++ src/main/java/students/ernests_subhankulovs/lesson_10/level_5/task_18/BookReader.java
@@ -1,6 +1,7 @@
 package students.ernests_subhankulovs.lesson_10.level_5.task_18;
 
 interface BookReader {
+    Book[] getElectronicLibrary();
     boolean addBook(Book book);
     boolean isDuplicate(Book book);
     boolean validateTitleAndAuthor(Book book);
