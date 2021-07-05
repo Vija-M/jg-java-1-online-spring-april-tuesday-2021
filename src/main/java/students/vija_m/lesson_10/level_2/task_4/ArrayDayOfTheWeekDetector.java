6ee0e4aad8ba3622753dc15ef2b41f144a7fb77f
Vija-M <vija.riga@gmailcom>
2021-07-05 19:02:18 +0300

    Lesson-10, Level-2, Task-4

--- src/main/java/students/vija_m/lesson_10/level_2/task_4/ArrayDayOfTheWeekDetector.java
+++ src/main/java/students/vija_m/lesson_10/level_2/task_4/ArrayDayOfTheWeekDetector.java
@@ -5,7 +5,7 @@ class ArrayDayOfTheWeekDetector implements DayOfTheWeekDetector {
     public String detectDayName(int number) {
         String[] daysOfTheWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
         if (number >= 1 && number <= 7) {
-            return daysOfTheWeek[number];
+            return daysOfTheWeek[number - 1];
         } else {
             return "Please input a valid number between 1 and 7";
         }
