91d68149ba734930359e8e67d0098bc2b64567bb
jleshkev <evgeny.leshkevich@gmail.com>
2021-07-06 17:03:04 +0300

    Completed Task 5 - Lesson 6 - Level 1

---
+++ src/main/java/students/jevgenij_leshkevich/lesson_6/level_1/task_5/WhileDefinition5.java
@@ -0,0 +1,17 @@
+package students.jevgenij_leshkevich.lesson_6.level_1.task_5;
+
+class WhileDefinition5 {
+
+    public static void main(String[] args) {
+        int i = 1;
+//        while (i >= 100) {
+//            System.out.println(i);
+//            i--;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
+
