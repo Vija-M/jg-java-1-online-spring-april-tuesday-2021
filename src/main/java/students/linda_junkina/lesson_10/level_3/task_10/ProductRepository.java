c67a32a8ddcf3a4722ae7f4a6d1bf98dab80c70a
Linda-Ju <linda.junkina@gmail.com>
2021-07-06 14:34:27 +0300

    Lesson 10, Level 3, Task 8

--- src/main/java/students/linda_junkina/lesson_10/level_3/task_10/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package students.linda_junkina.lesson_10.level_3.task_10;
-
-interface ProductRepository {
-//    void save(Product product);
-//    Optional<Product> findByTitle(String productTitle);
-}
-
