1bf07451211e24eb2b624d5df09d880291a9c3e8
salun <salun.alex@gmail.com>
2021-07-06 18:07:01 +0300

    Changes in lesson 10, level1, task 1

--- src/main/java/students/olga_saluna/lesson_10/level_1/task_1/JavaStarLoginService.java
+++ src/main/java/students/olga_saluna/lesson_10/level_1/task_1/JavaStarLoginService.java
@@ -1,5 +1,5 @@
 package students.olga_saluna.lesson_10.level_1.task_1;
 
 interface JavaStarLoginService {
-    void canLogin();
+    boolean canLogin(String login, String password);
 }
--- src/main/java/students/olga_saluna/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
+++ src/main/java/students/olga_saluna/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
@@ -2,17 +2,13 @@ package students.olga_saluna.lesson_10.level_1.task_1;
 
 class JavaStarLoginServiceImpl implements JavaStarLoginService {
 
-    public boolean canLogin(String login, String password) {
-        return (login != null)
-                && login.equals("javastar")
-                && (password != null)
-                && password.equals("pass");
+    public boolean canLogin() {
+        return canLogin("abcd", "qwerty123");
     }
 
-
     @Override
-    public void canLogin() {
-
+    public boolean canLogin(String login, String password) {
+        return false;
     }
 }
 
