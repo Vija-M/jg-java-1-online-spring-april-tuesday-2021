f6c318e17234f9ee29bbdd6cc936006045c52882
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-07-06 08:43:28 +0100

    Completed   Lesson 10 Level 1 Task 3

--- src/main/java/students/arturs_arutjunovs/lesson_10/level_1/task_3/SimpleList.java
+++ src/main/java/students/arturs_arutjunovs/lesson_10/level_1/task_3/SimpleList.java
@@ -2,29 +2,18 @@ package students.arturs_arutjunovs.lesson_10.level_1.task_3;
 
 interface SimpleList<T> {
 
-    void createEmptyList(); //создает пустой список
-
-    void deleteList(); //удаляет список
-
-    void copyList(); //копирует список
-
+    void createList(T value); //создает список
+    void deleteList(T value); //удаляет список
+    void copyElement(int unit, T value); //копирует элемент списка
     void addElementToList(int unit, T value); //добавляет элемент в список
-
     void printList();  //выводит список на печать
-
     void removeElementFromList(int unit, T value);  //удаляет элемент из списка
-
-    void findMinElement(T value);  //находит минимальный элемент
-
-    void findMaxElement(T value);  //находит максимальный элемент
-
+    int findMinElement(int unit, T value);  //находит минимальный элемент
+    int findMaxElement(int unit, T value);  //находит максимальный элемент
     void removeElementsButMin(int unit, T value); //удаляет элементы кроме минимального
-
     void removeElementsButMax(int unit, T value); //удаляет элементы кроме максимального
-
     void sortElementUp(T list);  //сортирует элементы по возрастанию
-
-    void sortElementDown(T list);  //сортирует элементы по убыванию
-
-    void findEqualElements(int unit, T value);  //находит равные элементы
+    int findSumOfElements(int unit, T list);  //считает сумму элементов в списке
+    boolean findEqualElements(int unit, T value);  //находит, есть равные элементы в списке
+    SimpleList<T> sortReverseOrder();  //сортировать список в обратном порядке
 }
