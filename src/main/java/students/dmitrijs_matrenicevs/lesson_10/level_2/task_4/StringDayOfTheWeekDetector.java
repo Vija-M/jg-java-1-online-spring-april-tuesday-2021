60a0ce0f4db45d9b4bd9aad4f9aec6c669d213ca
DeTELo <mrdinas@gmail.com>
2021-07-06 14:17:12 +0300

    Lesson 10 Task-4 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_10/level_2/task_4/StringDayOfTheWeekDetector.java
@@ -0,0 +1,14 @@
+package students.dmitrijs_matrenicevs.lesson_10.level_2.task_4;
+
+class StringDayOfTheWeekDetector implements DayOfTheWeekDetector {
+
+    @Override
+    public String detectDayName(int number) {
+        String[] daysWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        if (number <= 7 && number != 0) {
+            return daysWeek[number];
+        } else {
+            return "Please input a valid number between 1 and 7";
+        }
+    }
+}
