a44465097ad12c6cc256b78c9fc208e6e109ed6e
DeTELo <mrdinas@gmail.com>
2021-07-06 14:21:29 +0300

    Lesson 10 Task-4-5 - LEVEL2

--- src/main/java/students/dmitrijs_matrenicevs/lesson_10/level_2/task_4_5/StringDayOfTheWeekDetector.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_10/level_2/task_4_5/StringDayOfTheWeekDetector.java
@@ -1,4 +1,4 @@
-package students.dmitrijs_matrenicevs.lesson_10.level_2.task_4;
+package students.dmitrijs_matrenicevs.lesson_10.level_2.task_4_5;
 
 class StringDayOfTheWeekDetector implements DayOfTheWeekDetector {
 
