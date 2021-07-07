5a5c68f11661a603603be215db86987f114a8b2c
dk-t <shukailo.d@gmail.com>
2021-07-07 22:13:36 +0300

    Task 1 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_11/level_1/task_1/ListDemo.java
@@ -0,0 +1,42 @@
+package students.dmitriy_shukailo.lesson_11.level_1.task_1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class ListDemo {
+
+    public static void main(String[] args) {
+
+        List <String> listA = new ArrayList<>();
+
+        listA.add("Hello");
+        listA.add("World");
+        listA.add("!");
+
+        System.out.println("ArrayList: " + listA);
+
+        List <String> listL = new LinkedList<>();
+        listL.add("I");
+        listL.add("am");
+        listL.add("is");
+        listL.add("...");
+
+        System.out.println("LinkedList: " + listL);
+    }
+
+}
+
+/*
+Cуществует две реализации java.util.List:
+ArrayList - на основе массива
+LinkedList - на основе двусвязного списка
+
+Основное техническое отличие ArrayList от LinkedList состоит в том,
+что ArrayList использует native методы для увеличения размера,
+в то время как LinkedList полностью написана на Java.
+
+Производительность LinkedList для add() и remove() лучше, чем у Arraylist.
+А вот get() и set() методы имеют худшую производительность, чем у ArrayList,
+так как LinkedList не обеспечивает прямого доступа к своим членам.
+ */
