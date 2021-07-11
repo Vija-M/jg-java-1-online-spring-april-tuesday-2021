d439f2c724ffcaaaf2efad00ce1531d54b4a9b3b
DeTELo <mrdinas@gmail.com>
2021-07-11 12:27:25 +0300

    Lesson 10 Task-3 - LEVEL2

--- src/main/java/students/dmitrijs_matrenicevs/lesson_10/level_1/task_3/SimpleList.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_10/level_1/task_3/SimpleList.java
@@ -2,7 +2,7 @@ package students.dmitrijs_matrenicevs.lesson_10.level_1.task_3;
 
 interface SimpleList<T> {
 
-    void deleteObject(int a, T value);
+    void deleteObject(int i);
     //удалить обьект из определенного места в списке
 
     void lengthIncreaseList(int size);
@@ -11,22 +11,16 @@ interface SimpleList<T> {
     void lengthReduceList(int size);
     //уменьшить заданную длину
 
-    void createList(T value);
-    //создать список
-
     void deleteList(T value);
     //удалить спискок
 
-    int findMin(int a,T value);
+    int findMinElement(int unit,T value);
     //найти минимальный элемент
 
-    int findMax(int a,T value);
+    int findMaxElement(int unit,T value);
     //найти максимальный элемент
 
-    void printList();
-    //печатает список
-
-    void removeElementBeginning(int a);
+    void removeElementBeginning();
     //убрать элемент в начало списка
 
     void removeElementToTheEnd();
