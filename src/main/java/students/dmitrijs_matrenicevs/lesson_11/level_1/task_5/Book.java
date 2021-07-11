cb05e53afb683ef8e99c3f101645a8fb5340452c
DeTELo <mrdinas@gmail.com>
2021-07-11 13:40:35 +0300

    Lesson 11 Task-5 - LEVEL1

--- src/main/java/students/dmitrijs_matrenicevs/lesson_11/level_1/task_5/Book.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_11/level_1/task_5/Book.java
@@ -1,5 +1,7 @@
 package students.dmitrijs_matrenicevs.lesson_11.level_1.task_5;
 
+import java.util.Objects;
+
 record Book(String author, String title) {
 
     public String getTitle() {
@@ -8,5 +10,18 @@ record Book(String author, String title) {
 
     public String getAuthor() {
         return this.author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
     }
 }
