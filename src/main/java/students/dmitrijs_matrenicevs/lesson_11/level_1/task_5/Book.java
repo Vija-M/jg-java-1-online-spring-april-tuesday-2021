8882475c75951fdc618c84ec1f3db4dfef1c74b7
DeTELo <mrdinas@gmail.com>
2021-07-11 13:34:46 +0300

    Lesson 11 Task-5 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_11/level_1/task_5/Book.java
@@ -0,0 +1,12 @@
+package students.dmitrijs_matrenicevs.lesson_11.level_1.task_5;
+
+record Book(String author, String title) {
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+}
