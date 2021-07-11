63dc134b33942ceafee77a8901911bae2ceb1b90
DeTELo <mrdinas@gmail.com>
2021-07-11 12:54:42 +0300

    Lesson 11 Task-2 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_11/level_1/task_2/Task2.java
@@ -0,0 +1,16 @@
+package students.dmitrijs_matrenicevs.lesson_11.level_1.task_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Task2 {
+
+    public static void main(String[] args) {
+
+        List<String> strings = new ArrayList<>();
+
+        strings.add("JAVA");
+        strings.add("GURU");
+        System.out.println(strings);
+    }
+}
