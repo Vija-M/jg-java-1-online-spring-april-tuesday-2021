0278739e2d197bbbc6554d35cb34ad95b6ed7346
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-13 15:37:58 +0300

    Lesson 11 - Level 2 - Task 7

---
+++ src/main/java/students/ernests_subhankulovs/lesson_11/level_2/task_7/InMemoryBookRepository.java
@@ -0,0 +1,26 @@
+package students.ernests_subhankulovs.lesson_11.level_2.task_7;
+
+import java.util.ArrayList;
+
+class InMemoryBookRepository implements BookRepository {
+    ArrayList<Book> bookDataBase = new ArrayList<>();
+
+    @Override
+    public Long save(Book book) {
+        Long newBookId = (long) (bookDataBase.size() + 1);
+        book.setId(newBookId);
+        bookDataBase.add(book);
+        return newBookId;
+    }
+
+    @Override
+    public boolean delete(Long bookId) {
+        for (Book book : bookDataBase) {
+            if (book.getId().equals(bookId)) {
+                bookDataBase.remove(book);
+                return true;
+            }
+        }
+        return false;
+    }
+}
