9f39ae545f8807c7aea41ac2456585f3d07d0c1c
Vija-M <vija.riga@gmailcom>
2021-07-13 18:30:37 +0300

    Lesson-11, Level-1, Task-2

---
+++ src/main/java/students/vija_m/lesson_11/level_1/task_3/IntegerListDemo.java
@@ -0,0 +1,33 @@
+package students.vija_m.lesson_11.level_1.task_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerListDemo {
+    public static void main(String[] args) {
+
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(8);
+        numbers.add(3);
+        System.out.println(numbers);
+
+        numbers.add(0, -196);
+        numbers.add(numbers.size(), 57896);
+        System.out.println(numbers);
+
+        System.out.println("ArrayList numbers size is: " + numbers.size());
+
+        numbers.remove(2);
+        numbers.remove((Integer) 8);
+        System.out.println(numbers);
+
+        System.out.println("Is our array emty? : " + numbers.isEmpty());
+
+        for (Integer i : numbers) {
+            System.out.println(i);
+        }
+    }
+}
