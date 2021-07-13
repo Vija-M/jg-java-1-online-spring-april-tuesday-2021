cf19a3336098cafb20d3a6d10b29abafe1df5c9f
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-13 16:28:03 +0300

    Lesson 11 - Level 2 - Task 8

--- src/main/java/students/vija_m/lesson_6/level_7/task_34/ArraysSort.java
+++ src/main/java/students/vija_m/lesson_6/level_7/task_34/ArraysSort.java
@@ -1,6 +1,5 @@
 package students.vija_m.lesson_6.level_7.task_34;
 
-import org.apache.commons.lang.ArrayUtils;
 import java.util.Arrays;
 
 public class ArraysSort {
@@ -8,7 +7,7 @@ public class ArraysSort {
         int[] array = new int[]{3, 186, -5, 0, 8};
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
-        ArrayUtils.reverse(array);
+        //ArrayUtils.reverse(array);
         System.out.println(Arrays.toString(array));
     }
 }
