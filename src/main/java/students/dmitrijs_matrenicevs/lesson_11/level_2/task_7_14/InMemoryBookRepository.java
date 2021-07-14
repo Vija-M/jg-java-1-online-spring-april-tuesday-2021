69b651ba3b93cbfce4764655922b10ba6fe96af0
DeTELo <mrdinas@gmail.com>
2021-07-14 23:49:27 +0300

    Lesson 11 Task-7-14 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_11/level_2/task_7_14/InMemoryBookRepository.java
@@ -0,0 +1,17 @@
+package students.dmitrijs_matrenicevs.lesson_11.level_2.task_7_14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class InMemoryBookRepository implements BookRepository {
+
+    List<Book> books = new ArrayList<>();
+
+    @Override
+    public Long save(Book book) {
+        Long bookID = (long) (books.size() + 1);
+        book.setId(bookID);
+        books.add(book);
+        return bookID;
+    }
+}
