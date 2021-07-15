b00cb5d77e92a6ffb93d3672c6069b712472f0ad
Vija-M <vija.riga@gmailcom>
2021-07-15 23:46:07 +0300

    Lesson-11, Level-1, Task-4

--- src/main/java/students/vija_m/lesson_11/level_1/task_4/DuplicatesAllowed.java
+++ src/main/java/students/vija_m/lesson_11/level_1/task_4/DuplicatesAllowed.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DuplicatesAllowed {
-    public static void main(String[] args) {
+    static void main(String[] args) {
 
         List<Integer> numbers = new ArrayList<>();
         numbers.add(1);
