a2e49acfdcee200ed311f0d3ea3e89858836d7a1
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-16 20:14:18 +0300

    Lesson 11 - Level 3 - Task 20

---
+++ src/main/java/students/ernests_subhankulovs/lesson_11/level_3/task_20/BookRepository.java
@@ -0,0 +1,16 @@
+package students.ernests_subhankulovs.lesson_11.level_3.task_20;
+
+import java.util.List;
+import java.util.Optional;
+
+interface BookRepository {
+    Long save(Book book);
+    boolean delete(Long bookId);
+    boolean delete(Book book);
+    Optional<Book> findById(Long bookId);
+    List<Book> findByAuthor(String author);
+    List<Book> findByTitle(String title);
+    int count();
+    void deleteByAuthor(String author);
+    void deleteByTitle(String title);
+}
