e51f26a1a77d015663a62024d6a817e7306aba71
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-19 19:54:42 +0300

    Lesson 11 - Level 6 - Task 33

---
+++ src/main/java/students/ernests_subhankulovs/lesson_11/level_6/task_33/FindByAuthorUIAction.java
@@ -0,0 +1,22 @@
+package students.ernests_subhankulovs.lesson_11.level_6.task_33;
+
+import java.util.List;
+import java.util.Scanner;
+
+class FindByAuthorUIAction {
+    private BookRepository repository;
+
+    public FindByAuthorUIAction(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter book author:");
+        String author = scanner.nextLine();
+        List<Book> booksFoundBYAuthor = repository.findByAuthor(author);
+        for (Book book : booksFoundBYAuthor) {
+            System.out.println("Author: " + book.getAuthor() + " Title:" + book.getTitle());
+        }
+    }
+}
