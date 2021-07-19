1497200283011c6af5f3ad0063465bf03b9ef7eb
Vija-M <vija.riga@gmailcom>
2021-07-19 23:13:09 +0300

    Lesson-12, Level-1, Task-5

---
+++ src/main/java/students/vija_m/lesson_12/level_1/task_5/ExceptionDemo.java
@@ -0,0 +1,10 @@
+package students.vija_m.lesson_12.level_1.task_5;
+
+class ExceptionDemo {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println("str = " + str);
+    }
+}
+
+
