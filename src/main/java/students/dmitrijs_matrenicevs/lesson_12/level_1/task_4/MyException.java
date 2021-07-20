feec34fedd85d5398a28ee7c77e374e1684b83c9
DeTELo <mrdinas@gmail.com>
2021-07-20 13:16:07 +0300

    Lesson 12 Task-4 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_12/level_1/task_4/MyException.java
@@ -0,0 +1,8 @@
+package students.dmitrijs_matrenicevs.lesson_12.level_1.task_4;
+
+class MyException extends Exception {
+
+    public MyException(String message) {
+        super(message);
+    }
+}
