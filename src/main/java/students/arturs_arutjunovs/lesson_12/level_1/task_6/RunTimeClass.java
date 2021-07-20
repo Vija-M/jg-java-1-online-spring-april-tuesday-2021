6acca7afc35be1ad65d2833f7977de1541d35272
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-07-20 03:35:36 +0100

    Completed Lesson 12 level 1 task 6

---
+++ src/main/java/students/arturs_arutjunovs/lesson_12/level_1/task_6/RunTimeClass.java
@@ -0,0 +1,15 @@
+package students.arturs_arutjunovs.lesson_12.level_1.task_6;
+
+class RunTimeClass {
+/* Иерархия выглядит таким образом:
+
+    java.lang.Object
+    java.lang.Throwable
+    java.lang.Exception
+    java.lang.RuntimeException
+
+java.lang.RuntimeException включает в себя ArithmeticException, NullPointerException,
+ArrayIndexOutOfBoundException, IllegalArgumentException
+ */
+
+}
