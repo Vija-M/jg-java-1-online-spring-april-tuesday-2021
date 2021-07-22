0c978c52a72dbc8ebe3f3535fd4e06970cf13e0b
jleshkev <evgeny.leshkevich@gmail.com>
2021-07-22 23:59:59 +0300

    Completed Task 2 - Lesson 8 - Level 1

---
+++ src/main/java/students/jevgenij_leshkevich/lesson_8/level_1/task_2/ImplicitDefaultConstructorDemo.java
@@ -0,0 +1,12 @@
+package students.jevgenij_leshkevich.lesson_8.level_1.task_2;
+
+class ImplicitDefaultConstructorDemo {
+
+    public static void main(String[] args) {
+
+        ImplicitDefaultConstructor object = new ImplicitDefaultConstructor();
+
+        System.out.println(object);
+    }
+
+}
