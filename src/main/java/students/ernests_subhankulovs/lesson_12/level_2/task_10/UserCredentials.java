77480bd308a0d8ecf8bbb174d09011195874cd0f
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-23 12:36:05 +0300

    Lesson 12 - Level 2 - Task 10

---
+++ src/main/java/students/ernests_subhankulovs/lesson_12/level_2/task_10/UserCredentials.java
@@ -0,0 +1,16 @@
+package students.ernests_subhankulovs.lesson_12.level_2.task_10;
+
+import java.util.Arrays;
+import java.util.List;
+
+class UserCredentials {
+    private List<Role> roles;
+
+    UserCredentials() {
+        this.roles = Arrays.asList(Role.values());
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+}
