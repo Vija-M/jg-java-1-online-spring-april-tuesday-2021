5991de316d0c7c05fc99c56a6f2507442d637517
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-07-24 23:23:26 +0300

    Completed Lesson 12 level 2 task 10

---
+++ src/main/java/students/arturs_arutjunovs/lesson_12/level_2/task_10/UserCredentials.java
@@ -0,0 +1,13 @@
+package students.arturs_arutjunovs.lesson_12.level_2.task_10;
+
+import javax.management.relation.Role;
+import java.util.List;
+
+class UserCredentials {
+
+    private List<Role> roles;
+
+    public UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+}
