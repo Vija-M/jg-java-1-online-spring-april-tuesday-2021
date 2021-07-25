72e667d7ebe13dc28898153c367ab0e2342683ff
Vija-M <vija.riga@gmailcom>
2021-07-25 15:53:33 +0300

    Lesson-6, Level-7, Task-33

--- src/main/java/students/vija_m/lesson_6/level_7/task_33/TicTacToe.java
+++ src/main/java/students/vija_m/lesson_6/level_7/task_33/TicTacToe.java
@@ -90,17 +90,11 @@ class TicTacToe {
         Random random = new Random();
         int x;
         int y;
-        for (int i = 0; i < 3; i++){
-        if (field[i][0] == 0 && field[i][1] == 0 ) { x = i; y = 2;}
-          if (field[i][0] == 0 && field[i][2] == 0) { x = i; y = 1;}
-            if (field[i][1] == 0 && field[i][2] == 0) { x = i; y = 0;}
-            if (field[0][i] == 0 && field[1][i] == 0 ) { x = 2; y = i;}
-
-        do {        x = random.nextInt(3);
+        do {
+            x = random.nextInt(3);
             y = random.nextInt(3);
-                    }
+        }
         while (field[x][y] != -1);
-
         return new Move(x, y);
     }
 
