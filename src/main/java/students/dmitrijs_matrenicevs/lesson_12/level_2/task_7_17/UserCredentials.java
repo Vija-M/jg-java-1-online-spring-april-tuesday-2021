a018c3c09cdb82f7e9852d773ba7ed4e72a062f2
DeTELo <mrdinas@gmail.com>
2021-07-26 18:01:31 +0300

    Lesson 12 Task-8 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_12/level_2/task_7_17/UserCredentials.java
@@ -0,0 +1,12 @@
+package students.dmitrijs_matrenicevs.lesson_12.level_2.task_7_17;
+
+import java.util.List;
+
+class UserCredentials {
+
+    private List<Role> roles;
+
+    public UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+}
