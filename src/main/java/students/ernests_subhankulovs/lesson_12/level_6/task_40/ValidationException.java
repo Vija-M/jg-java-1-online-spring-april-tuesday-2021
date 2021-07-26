cfaaf2d41436f82815c6c1ac8cfac5f5cffb33b4
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-26 21:33:15 +0300

    Lesson 12 - Level 6 - Task 40

---
+++ src/main/java/students/ernests_subhankulovs/lesson_12/level_6/task_40/ValidationException.java
@@ -0,0 +1,25 @@
+package students.ernests_subhankulovs.lesson_12.level_6.task_40;
+
+class ValidationException extends Exception  {
+    private String ruleName;
+    private String description;
+    private String fieldName;
+
+    ValidationException(String ruleName, String description, String fieldName) {
+        this.ruleName = ruleName;
+        this.description = description;
+        this.fieldName = fieldName;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+}
