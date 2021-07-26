6d851c7dcde0b10574894829438ef5fbd0732ee4
DeTELo <mrdinas@gmail.com>
2021-07-26 18:22:32 +0300

    Lesson 12 Task-12 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_12/level_2/task_7_17/AccessDeniedException.java
@@ -0,0 +1,8 @@
+package students.dmitrijs_matrenicevs.lesson_12.level_2.task_7_17;
+
+class AccessDeniedException extends Exception {
+
+    public AccessDeniedException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/students/dmitrijs_matrenicevs/lesson_12/level_2/task_7_17/BankApi.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_12/level_2/task_7_17/BankApi.java
@@ -4,5 +4,5 @@ import java.util.Optional;
 
 interface BankApi {
 
-    Optional<BankClient> findByUid(UserCredentials credentials, String uid) throws Exception;
+    Optional<BankClient> findByUid(UserCredentials credentials, String uid) throws AccessDeniedException;
 }
