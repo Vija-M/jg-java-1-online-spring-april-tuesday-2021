9b3af38277c87d8a062486ac46da14d4bf525328
DeTELo <mrdinas@gmail.com>
2021-07-26 18:58:55 +0300

    Lesson 12 Task-13-17 - LEVEL2

--- src/main/java/students/dmitrijs_matrenicevs/lesson_12/level_2/task_7_17/BankApiImplTest.java
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_12/level_2/task_7_17/BankApiImplTest.java
@@ -2,13 +2,14 @@ package students.dmitrijs_matrenicevs.lesson_12.level_2.task_7_17;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 class BankApiImplTest {
 
     public static void main(String[] args) {
         BankApiImplTest bankApiImplTest = new BankApiImplTest();
         bankApiImplTest.shouldThrowExceptionWhenCredentialsNotHaveAppropriateRole();
-
+        bankApiImplTest.findByUid();
     }
 
     public void shouldThrowExceptionWhenCredentialsNotHaveAppropriateRole() {
@@ -23,6 +24,23 @@ class BankApiImplTest {
             System.out.println("TEST OK");
         } catch (AccessDeniedException e) {
             System.out.println("TEST FAIL");
+        }
+    }
+
+    public void findByUid() {
+        BankClient bankClient = new BankClient("MAKS", "FILM");
+        BankClient bankClient1 = new BankClient("RICA", "MICA");
+        BankClient bankClient2 = new BankClient("NIKA", "PIKA");
+
+        List<BankClient> clients = List.of(bankClient,bankClient1,bankClient2);
+        List<Role> roles = List.of(Role.CAN_SEARCH_CLIENTS);
+        UserCredentials credentials = new UserCredentials(roles);
+
+        BankApi bankApi = new BankApiImpl(clients);
+
+        try {
+            Optional<BankClient> realResult = Optional.of(bankClient);
+            Optional<BankClient> expectedResult = bankApi.findByUid(credentials,"1");
         }
     }
 }
