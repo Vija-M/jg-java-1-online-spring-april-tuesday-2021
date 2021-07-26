e1a87e104a2baf1bf898131451cdcd3948286ac3
Linda-Ju <linda.junkina@gmail.com>
2021-07-26 13:46:05 +0300

    Lesson 9, Level 1, Task 6

--- src/main/java/students/linda_junkina/lesson_9/level_1/task_6/Book.java
+++ src/main/java/students/linda_junkina/lesson_9/level_1/task_6/Book.java
@@ -4,6 +4,11 @@ class Book {
     private String title;
     private String author;
 
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
     public String getTitle() {
         return title;
     }
