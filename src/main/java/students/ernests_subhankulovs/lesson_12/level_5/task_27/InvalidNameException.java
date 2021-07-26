b688ea0bc4de5a6336c9869b9deda17a693703ef
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-26 12:35:10 +0300

    Lesson 12 - Level 5 - Task 27

---
+++ src/main/java/students/ernests_subhankulovs/lesson_12/level_5/task_27/InvalidNameException.java
@@ -0,0 +1,7 @@
+package students.ernests_subhankulovs.lesson_12.level_5.task_27;
+
+class InvalidNameException extends Exception {
+    InvalidNameException() {
+        super("Invalid product name");
+    }
+}
