018c01cfeaeb38a1682665db91e23318756be25b
Linda-Ju <linda.junkina@gmail.com>
2021-07-26 12:17:03 +0300

    Lesson 13, Level 3, Task 10-18

---
+++ src/main/java/students/linda_junkina/lesson_13/level_3/task_10_11_12_13_14_15_16_17_18/GameOfLifeNextGenerationCalculator.java
@@ -0,0 +1,15 @@
+package students.linda_junkina.lesson_13.level_3.task_10_11_12_13_14_15_16_17_18;
+
+class GameOfLifeNextGenerationCalculator {
+    public boolean[][] calculate(boolean[][] currentGeneration) {
+
+        for (int i = 0; i < currentGeneration.length; i++) {
+            for (int j = 0; j < currentGeneration.length; j++) {
+                
+            }
+            
+        }
+        return currentGeneration;
+
+    }
+}
---
+++ src/test/java/students/linda_junkina/lesson_13/level_3/task_10_11_12_13_14_15_16_17_18/GameOfLifeNextGenerationCalculatorTest.java
@@ -0,0 +1,14 @@
+package students.linda_junkina.lesson_13.level_3.task_10_11_12_13_14_15_16_17_18;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameOfLifeNextGenerationCalculatorTest {
+
+    private GameOfLifeNextGenerationCalculator gameOfLife;
+    @Test
+    void shouldDieIfLessThan2Neighbours() {
+
+    }
+}
