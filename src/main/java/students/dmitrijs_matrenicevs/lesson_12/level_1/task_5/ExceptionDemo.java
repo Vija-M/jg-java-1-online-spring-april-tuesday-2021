a65d45ee69c4518bf868a20432a688e0b0469f3c
DeTELo <mrdinas@gmail.com>
2021-07-26 17:56:48 +0300

    Lesson 12 Task-5 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_12/level_1/task_5/ExceptionDemo.java
@@ -0,0 +1,22 @@
+package students.dmitrijs_matrenicevs.lesson_12.level_1.task_5;
+
+import java.util.Scanner;
+
+class ExceptionDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        Exception one = new Exception();
+        Exception two = new Exception();
+        Exception three = new Exception(one);
+        Exception four = new Exception(two);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1");
+        System.out.println("2");
+        int selection = scanner.nextInt();
+        switch (selection) {
+            case 1 -> throw three;
+            case 2 -> throw four;
+        }
+    }
+}
