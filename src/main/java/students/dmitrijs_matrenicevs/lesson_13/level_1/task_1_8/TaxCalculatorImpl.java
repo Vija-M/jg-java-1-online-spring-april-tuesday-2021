5349b302580b56098d3780d44110d90bc408acaf
DeTELo <mrdinas@gmail.com>
2021-07-28 22:52:27 +0300

    Lesson 13 Task-2 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_13/level_1/task_1_8/TaxCalculatorImpl.java
@@ -0,0 +1,11 @@
+package students.dmitrijs_matrenicevs.lesson_13.level_1.task_1_8;
+
+import java.math.BigDecimal;
+
+class TaxCalculatorImpl implements TaxCalculator {
+
+    @Override
+    public BigDecimal calculateTax(BigDecimal income) {
+        return null;
+    }
+}
