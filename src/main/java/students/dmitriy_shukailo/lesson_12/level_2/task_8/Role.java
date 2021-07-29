97af0c37301fec3b7b95b72cc4f72c12dc6c8af4
dk-t <shukailo.d@gmail.com>
2021-07-29 22:46:19 +0300

    Task 8 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_12/level_2/task_8/Role.java
@@ -0,0 +1,7 @@
+package students.dmitriy_shukailo.lesson_12.level_2.task_8;
+
+public enum Role {
+
+    CAN_SEARCH_CLIENT
+
+}
---
+++ src/main/java/students/dmitriy_shukailo/lesson_12/level_2/task_8/UserCredentials.java
@@ -0,0 +1,13 @@
+package students.dmitriy_shukailo.lesson_12.level_2.task_8;
+
+import java.util.List;
+
+class UserCredentials {
+
+    private final List<Role> roles;
+
+    UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+
+}
