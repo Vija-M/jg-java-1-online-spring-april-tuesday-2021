0ae3f6de29e58379d608fc283b2788c36e364382
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-07-29 16:33:48 +0300

    Completed Lesson 12 level 2 task 14

---
+++ src/main/java/students/arturs_arutjunovs/lesson_12/level_2/task_14/UserCredentials.java
@@ -0,0 +1,16 @@
+package students.arturs_arutjunovs.lesson_12.level_2.task_14;
+
+import java.util.List;
+
+class UserCredentials {
+
+    private List<Role> roles;
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+}
