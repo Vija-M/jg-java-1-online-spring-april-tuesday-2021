b2f7c6fb999aa89b047a075c97e35159c96db473
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-29 13:08:41 +0300

    Lesson 13 - Level 1 - Task 5

---
+++ src/main/java/students/ernests_subhankulovs/lesson_13/level_1/task_5/TaxCalculatorImpl.java
@@ -0,0 +1,10 @@
+package students.ernests_subhankulovs.lesson_13.level_1.task_5;
+
+import java.math.BigDecimal;
+
+class TaxCalculatorImpl implements TaxCalculator {
+    @Override
+    public BigDecimal calculateTax(BigDecimal income) {
+        return income.multiply(new BigDecimal("0.25"));
+    }
+}
