7d80aa97d72df69d8f4813a27dec96fb42564f53
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-07-30 12:34:28 +0300

    Lesson 13 - Level 4 - Task 24

---
+++ src/main/java/students/ernests_subhankulovs/lesson_13/level_4/task_24/GameOfLifeRandomFieldGenerator.java
@@ -0,0 +1,16 @@
+package students.ernests_subhankulovs.lesson_13.level_4.task_24;
+
+import java.util.Random;
+
+class GameOfLifeRandomFieldGenerator {
+    public boolean[][] generate(int row, int column) {
+        Random random = new Random();
+        boolean[][] field = new boolean[row][column];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                field[i][j] = random.nextBoolean();
+            }
+        }
+        return field;
+    }
+}
