d006776d8c2ec3a4b4c074d9844386f760c9dd31
Linda-Ju <linda.junkina@gmail.com>
2021-07-31 01:01:37 +0300

    Lesson 14, Level 2, Task 7-17

---
+++ src/main/java/students/linda_junkina/lesson_14/level_2/task_7_8_9_10_11_12_13_14_15_16_17/Apple.java
@@ -0,0 +1,34 @@
+package students.linda_junkina.lesson_14.level_2.task_7_8_9_10_11_12_13_14_15_16_17;
+
+import java.util.Objects;
+
+class Apple {
+    private String color;
+    private int weight;
+
+    public Apple(String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Apple apple = (Apple) o;
+        return weight == apple.weight && Objects.equals(color, apple.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, weight);
+    }
+}
---
+++ src/main/java/students/linda_junkina/lesson_14/level_2/task_7_8_9_10_11_12_13_14_15_16_17/ApplePredicate.java
@@ -0,0 +1,6 @@
+package students.linda_junkina.lesson_14.level_2.task_7_8_9_10_11_12_13_14_15_16_17;
+
+interface ApplePredicate {
+    boolean test (Apple apple);
+
+}
