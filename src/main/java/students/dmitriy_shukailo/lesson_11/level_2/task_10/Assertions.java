c442b08f9f98ff419ab8ef864fd6b1a0bdefcd97
dk-t <shukailo.d@gmail.com>
2021-08-02 22:39:39 +0300

    Task 10 has been corrected

--- src/main/java/students/dmitriy_shukailo/lesson_11/level_2/task_10/Assertions.java
+++ src/main/java/students/dmitriy_shukailo/lesson_11/level_2/task_10/Assertions.java
@@ -11,11 +11,11 @@ class Assertions {
     }
 
     public static void assertEquals(Object o1, Object o2, String testName) {
-        assertCondition(o1.equals(o2),testName, "Expected " + o1 + "to be equal to" + o2);
+        assertCondition(o1.equals(o2), testName, "Expected " + o1 + " to be equal to " + o2);
     }
 
     public static void assertNotEquals(Object o1, Object o2, String testName) {
-        assertCondition(!o1.equals(o2),testName, "Expected " + o1 + " to be not equal to " + o2);
+        assertCondition(!o1.equals(o2), testName, "Expected " + o1 + " to be not equal to " + o2);
     }
 
     // универсальный метод проверки условия и репорта об ошибке теста
