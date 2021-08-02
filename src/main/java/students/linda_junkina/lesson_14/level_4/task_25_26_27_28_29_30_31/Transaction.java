2e611a20390eaae84148644893462a252ae0bde9
Linda-Ju <linda.junkina@gmail.com>
2021-08-03 01:41:58 +0300

    Lesson 14, Level 4, Task 25-31

---
+++ src/main/java/students/linda_junkina/lesson_14/level_4/task_25_26_27_28_29_30_31/Transaction.java
@@ -0,0 +1,46 @@
+package students.linda_junkina.lesson_14.level_4.task_25_26_27_28_29_30_31;
+
+import java.util.Objects;
+
+class Transaction {
+    private Trader trader;
+    private int year;
+    private int value;
+
+    public Transaction(Trader trader, int year, int value) {
+        this.trader = trader;
+        this.year = year;
+        this.value = value;
+    }
+
+    public Trader getTrader() {
+        return this.trader;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public String toString() {
+        return "{" + this.trader + ", " +
+                "year: "+this.year+", " +
+                "value:" + this.value +"}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return year == that.year && value == that.value && Objects.equals(trader, that.trader);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(trader, year, value);
+    }
+}
---
+++ src/main/java/students/linda_junkina/lesson_14/level_4/task_25_26_27_28_29_30_31/TransactionAnalysisService.java
@@ -0,0 +1,44 @@
+package students.linda_junkina.lesson_14.level_4.task_25_26_27_28_29_30_31;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+class TransactionAnalysisService {
+
+    public List<Integer> findYearsOfTransactions(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getYear())
+                .collect(toList());
+    }
+
+    public List<Integer> findUniqueYearsOfTransactions(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getYear())
+                .distinct()
+                .collect(toList());
+    }
+
+    public List<String> findUniqueTradersNames(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .distinct()
+                .collect(toList());
+    }
+
+    public List<String> findUniqueCities(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .collect(toList());
+    }
+
+    public List<String> findAllTradersInCity(List<Transaction> transactions, String cityToFind) {
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .filter(city -> city.equals(cityToFind))
+                .distinct()
+                .collect(toList());
+    }
+
+}
