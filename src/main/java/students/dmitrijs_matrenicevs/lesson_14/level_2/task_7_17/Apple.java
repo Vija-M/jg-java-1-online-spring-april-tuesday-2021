96409544d80be708eca14dbb1a2324eb8a61a4a3
DeTELo <mrdinas@gmail.com>
2021-08-02 15:40:02 +0300

    Lesson 14 Task-7-17 - LEVEL2

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_14/level_2/task_7_17/Apple.java
@@ -0,0 +1,37 @@
+package students.dmitrijs_matrenicevs.lesson_14.level_2.task_7_17;
+
+import java.util.Objects;
+
+class Apple {
+
+    private final String color;
+    private final int weight;
+
+    public Apple(String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Apple apple = (Apple) obj;
+        return weight == apple.weight && Objects.equals(color, apple.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
