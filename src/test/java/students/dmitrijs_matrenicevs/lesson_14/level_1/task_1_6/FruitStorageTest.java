378ae79194e3bd6f9e257bc82a6a9f0b6eb74fc2
DeTELo <mrdinas@gmail.com>
2021-08-02 15:31:01 +0300

    Lesson 14 Task-1-6 - LEVEL1

--- src/test/java/students/dmitrijs_matrenicevs/lesson_14/level_1/task_1_6/FruitStorageTest.java
+++ src/test/java/students/dmitrijs_matrenicevs/lesson_14/level_1/task_1_6/FruitStorageTest.java
@@ -1,6 +1,6 @@
 package students.dmitrijs_matrenicevs.lesson_14.level_1.task_1_6;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,18 +35,18 @@ class FruitStorageTest {
     public void findWFilterApplesTest(){
         List<Apple> inventory = fruitStorage.getAllApples();
         List<Apple> expected = new ArrayList<>();
-        expected.add(new Apple("red", 200));
         expected.add(new Apple("red", 160));
-        expected.add(new Apple("green", 90));
-        expected.add(new Apple("yellow", 50));
-        assertEquals (expected, fruitStorage.findApplesByWeight(inventory, 170));
+        expected.add(new Apple("red", 200));
+        expected.add(new Apple("green", 200));
+        expected.add(new Apple("yellow", 170));
+        assertEquals (expected, fruitStorage.findApplesByWeight(inventory, 150));
     }
 
     @Test
     public void findFilterApplesTest(){
         List<Apple> inventory = fruitStorage.getAllApples();
         List<Apple> expected = new ArrayList<>();
-        expected.add(new Apple("red", 200));
+        expected.add(new Apple("red", 100));
         expected.add(new Apple("green", 90));
         expected.add(new Apple("green", 50));
         expected.add(new Apple("yellow", 50));
