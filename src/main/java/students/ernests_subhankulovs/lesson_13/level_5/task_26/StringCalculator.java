90b797f30ce27164b15a392859ee1161879749ac
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-08-02 23:13:22 +0300

    Lesson 13 - Level 4 - Task 26. Corrected.

--- src/main/java/students/ernests_subhankulovs/lesson_13/level_5/task_26/StringCalculator.java
+++ src/main/java/students/ernests_subhankulovs/lesson_13/level_5/task_26/StringCalculator.java
@@ -2,24 +2,16 @@ package students.ernests_subhankulovs.lesson_13.level_5.task_26;
 
 public class StringCalculator {
 
-    public static void main(String[] args) {
-        StringCalculator calculator = new StringCalculator();
-        System.out.println(calculator.add("1"));
-    }
-
     int add(String numbers) {
         int sum = 0;
         String[] numberList = numbers.split(",");
-            if (numbers.equals("")) {
-                numbers = "0";
+        for (String number : numberList) {
+            try {
+                sum += Integer.parseInt(number);
+            } catch (Exception e) {
+                System.out.println("All values should be integer numbers divided by commas!");
             }
-            for (String number : numberList) {
-                try {
-                    sum += Integer.parseInt(number);
-                } catch (Exception e) {
-                    System.out.println("All values should be integer numbers divided by commas!");
-                }
-            }
-            return sum;
+        }
+        return sum;
     }
 }
