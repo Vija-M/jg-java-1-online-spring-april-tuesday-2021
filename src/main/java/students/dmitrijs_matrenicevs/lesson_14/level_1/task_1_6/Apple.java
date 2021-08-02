1c61f253b159332a4ee12d4e2c59c1f7f67c1634
DeTELo <mrdinas@gmail.com>
2021-08-02 13:42:39 +0300

    Lesson 14 Task-1-6 - LEVEL1

---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_14/level_1/task_1_6/Apple.java
@@ -0,0 +1,20 @@
+package students.dmitrijs_matrenicevs.lesson_14.level_1.task_1_6;
+
+class Apple {
+
+    private String color;
+    private int weight;
+
+    public Apple (String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/students/dmitrijs_matrenicevs/lesson_14/level_1/task_1_6/FruitStorage.java
@@ -0,0 +1,17 @@
+package students.dmitrijs_matrenicevs.lesson_14.level_1.task_1_6;
+
+class FruitStorage {
+
+    public List<Apple> getAllApples() {
+        List<Apple> apples = new ArrayList<>();
+        apples.add(new Apple("red", 100));
+        apples.add(new Apple("red", 160));
+        apples.add(new Apple("red", 200));
+        apples.add(new Apple("green", 200));
+        apples.add(new Apple("green", 90));
+        apples.add(new Apple("green", 50));
+        apples.add(new Apple("yellow", 50));
+        apples.add(new Apple("yellow", 170));
+        return apples;
+    }
+}
