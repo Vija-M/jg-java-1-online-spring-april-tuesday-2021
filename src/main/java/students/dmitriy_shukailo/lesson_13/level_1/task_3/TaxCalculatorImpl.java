0e4acb4b13174c87e570ab9e257c926f0b65475d
dk-t <shukailo.d@gmail.com>
2021-08-02 21:10:08 +0300

    Task 3 has been done

---
+++ src/main/java/students/dmitriy_shukailo/lesson_13/level_1/task_3/TaxCalculatorImpl.java
@@ -0,0 +1,12 @@
+package students.dmitriy_shukailo.lesson_13.level_1.task_3;
+
+import java.math.BigDecimal;
+
+class TaxCalculatorImpl implements TaxCalculator {
+
+    @Override
+    public BigDecimal calculateTax(BigDecimal income) {
+        return null;
+    }
+
+}
