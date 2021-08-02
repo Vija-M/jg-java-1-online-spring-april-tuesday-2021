52081c8a3da12a32992a9a522d3e7ea05f60a08f
jleshkev <evgeny.leshkevich@gmail.com>
2021-08-02 22:27:15 +0300

    Completed Task 6 - Lesson 9 - Level 1

---
+++ src/main/java/students/jevgenij_leshkevich/lesson_9/level_1/task_6/Book.java
@@ -0,0 +1,20 @@
+package students.jevgenij_leshkevich.lesson_9.level_1.task_6;
+
+class Book {
+
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
