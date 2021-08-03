0e94478d7428c6030597d37d0fcf62120ced00b3
Linda-Ju <linda.junkina@gmail.com>
2021-08-03 17:56:19 +0300

    Lesson 14, Level 6, Task 35-38

---
+++ src/main/java/students/linda_junkina/lesson_14/level_6/task_35_36_37_38/Transaction.java
@@ -0,0 +1,33 @@
+package students.linda_junkina.lesson_14.level_6.task_35_36_37_38;
+
+import java.util.Objects;
+
+class Transaction {
+    private Trader trader;
+    private int year;
+    private int value;
+
+    public Transaction(Trader trader, int year, int value) {
+        this.trader = trader;
+        this.year = year;
+        this.value = value;
+    }
+
+    public Trader getTrader() {
+        return this.trader;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public String toString() {
+        return "{" + this.trader + ", " +
+                "year: " + this.year + ", " +
+                "value:" + this.value + "}";
+    }
+}
---
+++ src/main/java/students/linda_junkina/lesson_14/level_6/task_35_36_37_38/TransactionTestData.java
@@ -0,0 +1,23 @@
+package students.linda_junkina.lesson_14.level_6.task_35_36_37_38;
+
+import java.util.Arrays;
+import java.util.List;
+
+class TransactionTestData {
+    public List<Transaction> getTransactions() {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+        return transactions;
+    }
+}
