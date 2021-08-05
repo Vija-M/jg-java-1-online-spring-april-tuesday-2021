8a516b5f6064e46c24febd0ccad1063c51c177b3
Vija-M <vija.riga@gmailcom>
2021-08-05 20:45:53 +0300

    Lesson-7, Level-7, Task-12

--- src/main/java/students/vija_m/lesson_7/level_7/task_12/Encapsulation.java
+++ src/main/java/students/vija_m/lesson_7/level_7/task_12/Encapsulation.java
@@ -2,9 +2,9 @@ package students.vija_m.lesson_7.level_7.task_12;
 
 class Encapsulation {
     private int age;
-    private String name;
+    private final String name;
 
-     Encapsulation(int age, String name) {
+    Encapsulation(int age, String name) {
         this.age = age;
         this.name = name;
     }
@@ -16,6 +16,7 @@ class Encapsulation {
     String getName() {
         return name;
     }
+
     private void newLetter() {
 
     }
