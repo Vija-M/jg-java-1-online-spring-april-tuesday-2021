62f11b61501ccc0435713b612be72b6830d0cf3c
Vija-M <vija.riga@gmailcom>
2021-08-06 20:02:14 +0300

    Lesson-7, Level-7, Task-14

--- src/main/java/students/vija_m/lesson_7/level_7/task_14/UserRepositoryApplication.java
+++ src/main/java/students/vija_m/lesson_7/level_7/task_14/UserRepositoryApplication.java
@@ -17,24 +17,24 @@ class UserRepositoryApplication {
         user1.setPersonalCode("000000-00000");
 
         UserEntity user2 = new UserEntity();
-        user1.setName("John");
-        user1.setFamilyName("Smith");
-        user1.setPersonalCode("000001-00000");
+        user2.setName("John");
+        user2.setFamilyName("Smith");
+        user2.setPersonalCode("000001-00000");
 
         UserEntity user3 = new UserEntity();
-        user1.setName("Mary");
-        user1.setFamilyName("Green");
-        user1.setPersonalCode("000002-00000");
+        user3.setName("Mary");
+        user3.setFamilyName("Green");
+        user3.setPersonalCode("000002-00000");
 
         UserEntity user4 = new UserEntity();
-        user1.setName("Alan");
-        user1.setFamilyName("Po");
-        user1.setPersonalCode("000003-00000");
+        user4.setName("Alan");
+        user4.setFamilyName("Po");
+        user4.setPersonalCode("000003-00000");
 
         UserEntity user5 = new UserEntity();
-        user1.setName("John");
-        user1.setFamilyName("Chaney");
-        user1.setPersonalCode("000003-00000");
+        user5.setName("John");
+        user5.setFamilyName("Chaney");
+        user5.setPersonalCode("000004-00000");
 
         UUID user1Id = repository.save(user1);
         UUID user2Id = repository.save(user2);
@@ -44,19 +44,39 @@ class UserRepositoryApplication {
 
         System.out.println("repository.findAll() = " + Arrays.toString(repository.findAll()));
 
-        /*SongEntity foundSong1 = repository.findById(song1Id);
-        SongEntity foundSong2 = repository.findById(song2Id);
-        SongEntity foundSong3 = repository.findById(song3Id);
+        UserEntity foundUser1 = repository.findByID(user1Id);
+        UserEntity foundUser2 = repository.findByID(user2Id);
+        UserEntity foundUser3 = repository.findByID(user3Id);
+        UserEntity foundUser4 = repository.findByID(user4Id);
+        UserEntity foundUser5 = repository.findByID(user5Id);
 
-        System.out.println("foundSong1.getTitle() = " + foundSong1.getTitle());
-        System.out.println("foundSong2.getTitle() = " + foundSong2.getTitle());
-        System.out.println("foundSong3.getTitle() = " + foundSong3.getTitle());
+        System.out.println("User1 ID is: " + foundUser1.getId());
+        System.out.println("User1 personal code is: " + foundUser1.getPersonalCode());
+        System.out.println("User1 name is: " + foundUser1.getName());
+        System.out.println("User1 family name is: " + foundUser1.getFamilyName());
+        System.out.println("***");
+        System.out.println("User2 ID is: " + foundUser2.getId());
+        System.out.println("User2 personal code is: " + foundUser2.getPersonalCode());
+        System.out.println("User2 name is: " + foundUser2.getName());
+        System.out.println("User2 family name is: " + foundUser2.getFamilyName());
+        System.out.println("***");
+        System.out.println("User3 ID is: " + foundUser3.getId());
+        System.out.println("User3 personal code is: " + foundUser3.getPersonalCode());
+        System.out.println("User3 name is: " + foundUser3.getName());
+        System.out.println("User3 family name " + foundUser3.getFamilyName());
+        System.out.println("***");
+        System.out.println("User4 ID is: " + foundUser4.getId());
+        System.out.println("User4 personal code is: " + foundUser4.getPersonalCode());
+        System.out.println("User4 name is: " + foundUser4.getName());
+        System.out.println("User4 family name " + foundUser4.getFamilyName());
+        System.out.println("***");
+        System.out.println("User5 ID is: " + foundUser5.getId());
+        System.out.println("User5 personal code is: " + foundUser5.getPersonalCode());
+        System.out.println("User5 name is: " + foundUser5.getName());
+        System.out.println("User5 family name " + foundUser5.getFamilyName());
+        System.out.println("***");
 
-        repository.delete(song1Id);
-        repository.delete(song2Id);
-        repository.delete(song3Id);
-
+        repository.delete(user2Id);
         System.out.println("repository.findAll() = " + Arrays.toString(repository.findAll()));
 
-    }*/
-}}
+    }}
