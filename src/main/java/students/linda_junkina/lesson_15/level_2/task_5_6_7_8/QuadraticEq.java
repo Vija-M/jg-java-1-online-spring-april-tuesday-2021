6557109e7fb0ee02e9af14f250e042c1bbe8dc30
Linda-Ju <linda.junkina@gmail.com>
2021-08-07 01:03:30 +0300

    Lesson 15, Level 2, Task 5-8

---
+++ src/main/java/students/linda_junkina/lesson_15/level_2/task_5_6_7_8/QuadraticEq.java
@@ -0,0 +1,30 @@
+package students.linda_junkina.lesson_15.level_2.task_5_6_7_8;
+
+class QuadraticEq {
+
+    public String discriminantGreaterThan0(double a, double b, double discriminant) {
+        double x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        double x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        return "x1 = " + x1 + ", x2 = " + x2;
+    }
+
+    public String discriminantEquals0(double a, double b) {
+        double x = -b / (2 * a);
+        return "x = " + x;
+    }
+
+    public String discriminantLessThan0() {
+        return "Equation has no roots";
+    }
+
+    public String calc(double a, double b, double c) {
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant > 0) {
+            return discriminantGreaterThan0(a, b, discriminant);
+        } else if (discriminant == 0) {
+            return discriminantEquals0(a, b);
+        } else {
+            return discriminantLessThan0();
+        }
+    }
+}
---
+++ src/test/java/students/linda_junkina/lesson_15/level_2/task_5_6_7_8/QuadraticEqTest.java
@@ -0,0 +1,14 @@
+package students.linda_junkina.lesson_15.level_2.task_5_6_7_8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuadraticEqTest {
+    QuadraticEq quadraticEq = new QuadraticEq();
+
+    @Test
+    void discriminantGreaterThan0() {
+//        quadraticEq.calc(1, 5, 3);
+    }
+}
