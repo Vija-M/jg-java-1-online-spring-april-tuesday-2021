884921ecc2508fd0cb079cac31dde6cd4047eb6b
Konstantins Buts <kjb.dgsv@gmail.com>
2021-08-06 18:29:27 +0300

    Level 11 Lesson 2

---
+++ src/main/java/students/linda_junkina/lesson_11/level_2/task_6_7_8_9_10_11_12_13_14/InMemoryBookRepository.java
@@ -0,0 +1,96 @@
+package students.linda_junkina.lesson_11.level_2.task_6_7_8_9_10_11_12_13_14;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+class InMemoryBookRepository implements BookRepository {
+
+    List<Book> booksDataBase = new ArrayList<>();
+    private Long id = 0L;
+
+    public List<Book> getBooksDataBase() {
+        return booksDataBase;
+    }
+
+    @Override
+    public Long save(Book book) {
+        book.setId(id++);
+        booksDataBase.add(book);
+        return book.getId();
+    }
+
+    @Override
+    public boolean delete(Long bookId) {
+        for (Book book : booksDataBase) {
+            return booksDataBase.removeIf(b -> book.getId().equals(bookId));
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(Book book) {
+        return booksDataBase.remove(book);
+    }
+
+    @Override
+    public Optional<Book> findById(Long bookId) {
+        for (Book book : booksDataBase) {
+            if (book.getId().equals(bookId)) {
+                return Optional.of(book);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findByAuthor(String author) {
+        List<Book> filtered = new ArrayList<>();
+        for (Book book : booksDataBase) {
+            if (book.getAuthor().equals(author)) {
+                filtered.add(book);
+            }
+        }
+        return filtered;
+    }
+
+    @Override
+    public List<Book> findByTitle(String title) {
+        List<Book> filtered = new ArrayList<>();
+        for (Book book : booksDataBase) {
+            if (book.getTitle().equals(title)) {
+                filtered.add(book);
+            }
+        }
+        return filtered;
+    }
+
+    @Override
+    public int count() {
+        return booksDataBase.size();
+    }
+
+    @Override
+    public void deleteByAuthor(String author) {
+        List<Book> filtered = new ArrayList<>();
+        for (Book book : booksDataBase) {
+            if (book.getAuthor().equals(author)) {
+                continue;
+            }
+            filtered.add(book);
+        }
+        booksDataBase = filtered;
+    }
+
+    @Override
+    public void deleteByTitle(String title) {
+        List<Book> filtered = new ArrayList<>();
+        for (Book book : booksDataBase) {
+            if (book.getTitle().equals(title)) {
+                continue;
+            }
+            filtered.add(book);
+        }
+        booksDataBase = filtered;
+    }
+}
---
+++ src/main/java/students/linda_junkina/lesson_11/level_2/task_6_7_8_9_10_11_12_13_14/TestInMemoryBookRepository.java
@@ -0,0 +1,125 @@
+package students.linda_junkina.lesson_11.level_2.task_6_7_8_9_10_11_12_13_14;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+class TestInMemoryBookRepository {
+    public static void main(String[] args) {
+        TestInMemoryBookRepository test = new TestInMemoryBookRepository();
+        test.shouldDeleteById();
+        test.shouldDeleteByAuthorAndName();
+        test.shouldFindById();
+        test.shouldFindByAuthor();
+        test.shouldFindByTitle();
+        test.shouldCountBooks();
+        test.shouldDeleteByAuthor();
+        test.shouldDeleteByTitle();
+    }
+
+    public void shouldDeleteById() {
+        InMemoryBookRepository inMemoryBookRepository = new InMemoryBookRepository();
+        Book book1 = new Book("Foo1", "Bar1");
+        inMemoryBookRepository.save(book1);
+        checkTestResultBool(true, inMemoryBookRepository.delete(0L), "Book is removed by ID");
+    }
+
+    public void shouldDeleteByAuthorAndName() {
+        InMemoryBookRepository inMemoryBookRepository = new InMemoryBookRepository();
+        Book book1 = new Book("Foo1", "Bar1");
+        Book book2 = new Book("Foo2", "Bar2");
+        inMemoryBookRepository.save(book1);
+        inMemoryBookRepository.save(book2);
+        inMemoryBookRepository.delete(book1);
+        List<Book> expected = List.of(book2);
+        checkTestResultList(expected, inMemoryBookRepository.getBooksDataBase(),
+                "Book is removed by the Author and Name");
+    }
+
+    public void shouldFindById() {
+        InMemoryBookRepository inMemoryBookRepository = new InMemoryBookRepository();
+        Book book1 = new Book("Foo1", "Bar1");
+        Book book2 = new Book("Foo2", "Bar2");
+        inMemoryBookRepository.save(book1);
+        inMemoryBookRepository.save(book2);
+        Optional<Book> bookFound = inMemoryBookRepository.findById(1L);
+        checkTestResultBool(true, bookFound.isPresent(), "Book found by ID");
+    }
+
+    public void shouldFindByAuthor() {
+        InMemoryBookRepository inMemoryBookRepository = new InMemoryBookRepository();
+        Book book1 = new Book("Foo1", "Bar1");
+        Book book2 = new Book("Foo2", "Bar2");
+        inMemoryBookRepository.save(book1);
+        inMemoryBookRepository.save(book2);
+        List<Book> expected = List.of(book1);
+        checkTestResultList(expected, inMemoryBookRepository.findByAuthor("Foo1"), "Book found by Author");
+    }
+
+    public void shouldFindByTitle() {
+        InMemoryBookRepository inMemoryBookRepository = new InMemoryBookRepository();
+        Book book1 = new Book("Foo1", "Bar1");
+        Book book2 = new Book("Foo2", "Bar2");
+        inMemoryBookRepository.save(book1);
+        inMemoryBookRepository.save(book2);
+        List<Book> expected = List.of(book1);
+        checkTestResultList(expected, inMemoryBookRepository.findByTitle("Bar1"), "Book found by Title");
+    }
+
+    public void shouldCountBooks() {
+        InMemoryBookRepository inMemoryBookRepository = new InMemoryBookRepository();
+        Book book1 = new Book("Foo1", "Bar1");
+        Book book2 = new Book("Foo2", "Bar2");
+        inMemoryBookRepository.save(book1);
+        inMemoryBookRepository.save(book2);
+        checkTestResultInt(2, inMemoryBookRepository.count(), "Books counted");
+    }
+
+    public void shouldDeleteByAuthor() {
+        InMemoryBookRepository inMemoryBookRepository = new InMemoryBookRepository();
+        Book book1 = new Book("Foo1", "Bar1");
+        Book book2 = new Book("Foo2", "Bar2");
+        inMemoryBookRepository.save(book1);
+        inMemoryBookRepository.save(book2);
+        inMemoryBookRepository.deleteByAuthor("Foo1");
+        List<Book> expected = List.of(book2);
+        checkTestResultList(expected, inMemoryBookRepository.getBooksDataBase(), "Book deleted by Author");
+
+    }
+
+    public void shouldDeleteByTitle() {
+        InMemoryBookRepository inMemoryBookRepository = new InMemoryBookRepository();
+        Book book1 = new Book("Foo1", "Bar1");
+        Book book2 = new Book("Foo2", "Bar2");
+        inMemoryBookRepository.save(book1);
+        inMemoryBookRepository.save(book2);
+        inMemoryBookRepository.deleteByTitle("Bar2");
+        List<Book> expected = List.of(book1);
+        checkTestResultList(expected, inMemoryBookRepository.getBooksDataBase(), "Book deleted by Title");
+    }
+
+    private void checkTestResultBool(boolean expected, boolean actual, String testName) {
+        if (expected == actual) {
+            System.out.println(testName + " OK!");
+        } else {
+            System.out.println(testName + " FAILED!");
+        }
+    }
+
+    private void checkTestResultList(List<Book> expected, List<Book> actual, String testName) {
+        if (Objects.equals(expected, actual)) {
+            System.out.println(testName + " OK!");
+        } else {
+            System.out.println(testName + " FAILED!");
+        }
+    }
+
+    private void checkTestResultInt(int expected, int actual, String testName) {
+        if (expected == actual) {
+            System.out.println(testName + " OK!");
+        } else {
+            System.out.println(testName + " FAILED!");
+        }
+    }
+}
