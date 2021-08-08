3a9e1be9c1e0b2ef97b70edf6d94b1cb82bbd1de
Vija-M <vija.riga@gmailcom>
2021-08-08 20:22:00 +0300

    Lesson-15, Level-2, Task-9

--- src/main/java/students/vija_m/lesson_15/level_2/task_9/FraudDetector.java
+++ src/main/java/students/vija_m/lesson_15/level_2/task_9/FraudDetector.java
@@ -2,20 +2,20 @@ package students.vija_m.lesson_15.level_2.task_9;
 
 class FraudDetector {
     public boolean isFraud(Trader trader) {
-        return (compareTraderName(trader)
+        return (isTraderFraud(trader)
                 ||
-                (compareCity(trader))
+                (isCityBlocked(trader))
         );
     }
 
-    private boolean ifTraderIsFraud(Trader trader) {
+    private boolean isTraderFraud(Trader trader) {
         String traderName = trader.getFullName();
         {
             return traderName.equals("Pokemon");
         }
     }
 
-    private boolean ifCityIsBlocked(Trader trader) {
+    private boolean isCityBlocked(Trader trader) {
         String cityName = trader.getCity();
         {
             return cityName.equals("Sidney");
