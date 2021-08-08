2f38284886b8f386562acae8dc725c1eafe1a666
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-08-08 16:12:41 +0300

    Lesson 14 - Level 2 - Task 13

--- src/test/java/students/ernests_subhankulovs/lesson_14/level_2/task_13/FruitStorageTest.java
+++ src/test/java/students/ernests_subhankulovs/lesson_14/level_2/task_13/FruitStorageTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FruitStorageTest {
 
     @Test
@@ -171,11 +169,7 @@ class FruitStorageTest {
     @Test
     void testGetAllRedApples1stColorWithPredicate() {
         FruitStorage fruitStorage = new FruitStorage();
-        Assertions.assertEquals("red", fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
-            public boolean test(Apple apple) {
-                return "red".equals(apple.getColor());
-            }
-        }).get(0).getColor());
+        Assertions.assertEquals("red", fruitStorage.findApples(fruitStorage.getAllApples(), new AppleRedColorPredicate()).get(0).getColor());
     }
 
     @Test
@@ -270,5 +264,179 @@ class FruitStorageTest {
     void testFindApplesByGreenColorAndWeightOver150() {
         FruitStorage fruitStorage = new FruitStorage();
         Assertions.assertEquals("green", fruitStorage.findApples(fruitStorage.getAllApples(), new AppleGreenAndHeavyWeightPredicate()).get(0).getColor());
+    }
+
+    @Test
+    void testGetAllRedApplesSizeWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals(3, fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        }).size());
+    }
+
+    @Test
+    void testGetAllRedApples1stColorWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals("red", fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        }).get(0).getColor());
+    }
+
+    @Test
+    void testGetAllRedApples1stWeightWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals(100, fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        }).get(0).getWeight());
+    }
+
+    @Test
+    void testGetAllRedApples2ndColorWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals("red", fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        }).get(1).getColor());
+    }
+
+    @Test
+    void testGetAllRedApples2ndWeightWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals(160, fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        }).get(1).getWeight());
+    }
+
+    @Test
+    void testGetAllRedApples3rdColorWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals("red", fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        }).get(2).getColor());
+    }
+
+    @Test
+    void testGetAllRedApples3rdWeightWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals(200, fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        }).get(2).getWeight());
+    }
+
+    @Test
+    void testGetAllGreenApplesSizeWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals(3, fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "green".equals(apple.getColor());
+            }
+        }).size());
+    }
+
+    @Test
+    void testGetAllGreenApples1stColorWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals("green", fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "green".equals(apple.getColor());
+            }
+        }).get(0).getColor());
+    }
+
+    @Test
+    void testGetAllGreenApples1stWeightWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals(200, fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "green".equals(apple.getColor());
+            }
+        }).get(0).getWeight());
+    }
+
+    @Test
+    void testGetAllGreenApples2ndColorWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals("green", fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "green".equals(apple.getColor());
+            }
+        }).get(1).getColor());
+    }
+
+    @Test
+    void testGetAllGreenApples2ndWeightWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals(90, fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "green".equals(apple.getColor());
+            }
+        }).get(1).getWeight());
+    }
+
+    @Test
+    void testGetAllGreenApples3rdColorWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals("green", fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "green".equals(apple.getColor());
+            }
+        }).get(2).getColor());
+    }
+
+    @Test
+    void testGetAllGreenApples3rdWeightWithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        Assertions.assertEquals(50, fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return "green".equals(apple.getColor());
+            }
+        }).get(2).getWeight());
+    }
+
+    @Test
+    void testFindApplesByWeightOver150WithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        List<Apple> apples = new ArrayList<>();
+        apples.add(new Apple("red", 160));
+        apples.add(new Apple("red", 200));
+        apples.add(new Apple("green", 200));
+        apples.add(new Apple("yellow", 170));
+        for (int i = 0; i < apples.size(); i++) {
+            Assertions.assertEquals(apples.get(i).getWeight(), fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+                public boolean test(Apple apple) {
+                    return apple.getWeight() > 150;
+                }
+            }).get(i).getWeight());
+        }
+    }
+
+    @Test
+    void testFindApplesByWeightLessThan150WithAnonymousClass() {
+        FruitStorage fruitStorage = new FruitStorage();
+        List<Apple> apples = new ArrayList<>();
+        apples.add(new Apple("red", 100));
+        apples.add(new Apple("green", 90));
+        apples.add(new Apple("green", 50));
+        apples.add(new Apple("yellow", 50));
+        for (int i = 0; i < apples.size(); i++) {
+            Assertions.assertEquals(apples.get(i).getWeight(), fruitStorage.findApples(fruitStorage.getAllApples(), new ApplePredicate() {
+                public boolean test(Apple apple) {
+                    return apple.getWeight() < 150;
+                }
+            }).get(i).getWeight());
+        }
     }
 }
