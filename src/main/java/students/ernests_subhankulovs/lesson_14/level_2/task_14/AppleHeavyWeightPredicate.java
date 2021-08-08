800c004de9b91fb66cc6ad4f1811a6082c60fe22
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-08-08 16:46:32 +0300

    Lesson 14 - Level 2 - Task 14

---
+++ src/main/java/students/ernests_subhankulovs/lesson_14/level_2/task_14/AppleHeavyWeightPredicate.java
@@ -0,0 +1,7 @@
+package students.ernests_subhankulovs.lesson_14.level_2.task_14;
+
+class AppleHeavyWeightPredicate implements ApplePredicate {
+    public boolean test (Apple apple) {
+        return apple.getWeight() > 150;
+    }
+}
