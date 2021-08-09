7ba5d9b15db6eac02b6dd85253803318c6d3b882
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-08-09 21:42:32 +0300

    Completed Lesson 13 level 3 task 10

---
+++ src/main/java/students/arturs_arutjunovs/lesson_13/level_3/task_10/GameOfLifeNextGenerationCalculator.java
@@ -0,0 +1,8 @@
+package students.arturs_arutjunovs.lesson_13.level_3.task_10;
+
+class GameOfLifeNextGenerationCalculator {
+
+    public boolean[][] calculate(boolean[][] currentGeneration) {
+        return null;
+    }
+}
