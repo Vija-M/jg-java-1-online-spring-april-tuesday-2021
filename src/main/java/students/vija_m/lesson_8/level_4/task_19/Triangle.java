42e987b70e7da7a37dae8ed783cdddba57408e4f
Vija-M <vija.riga@gmailcom>
2021-08-10 14:33:00 +0300

    Lesson-8, Level-4, Task-19

---
+++ src/main/java/students/vija_m/lesson_8/level_4/task_19/Triangle.java
@@ -0,0 +1,20 @@
+package students.vija_m.lesson_8.level_4.task_19;
+
+class Triangle extends Shape {
+    private double sideLength;
+
+    public Triangle(double sideLength) {
+        super("Triangle");
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    double calculateArea() {
+        return sideLength * sideLength * Math.sqrt(3) / 4;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 3 * sideLength;
+    }
+}
