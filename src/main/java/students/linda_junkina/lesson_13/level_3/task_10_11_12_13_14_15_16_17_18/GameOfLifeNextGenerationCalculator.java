8c60aabbba5f80b3f98ac1dee43f0634b9cd5659
Linda-Ju <linda.junkina@gmail.com>
2021-08-13 00:19:42 +0300

    Lesson 13, Level 3, Task 10-18

--- src/main/java/students/linda_junkina/lesson_13/level_3/task_10_11_12_13_14_15_16_17_18/GameOfLifeNextGenerationCalculator.java
+++ src/main/java/students/linda_junkina/lesson_13/level_3/task_10_11_12_13_14_15_16_17_18/GameOfLifeNextGenerationCalculator.java
@@ -1,13 +1,17 @@
 package students.linda_junkina.lesson_13.level_3.task_10_11_12_13_14_15_16_17_18;
 
 class GameOfLifeNextGenerationCalculator {
+    //Rule No. 1
     public boolean[][] calculate(boolean[][] currentGeneration) {
+        boolean[][] nextGeneration = new boolean[currentGeneration.length][currentGeneration.length];
 
+        int numberOfNeighbors = 0;
         for (int i = 0; i < currentGeneration.length; i++) {
             for (int j = 0; j < currentGeneration.length; j++) {
-                
+//                if ()
+                    numberOfNeighbors++;
             }
-            
+
         }
         return currentGeneration;
 
