a47bea6ae46188df231154fc9a38859f2a22fbd3
Vija-M <vija.riga@gmailcom>
2021-08-13 14:35:55 +0300

    Lesson-14, Level-1, Task-2

---
+++ src/main/java/students/vija_m/lesson_14/level_1/task_2/FruitStorage.java
@@ -0,0 +1,27 @@
+package students.vija_m.lesson_14.level_1.task_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+class FruitStorage {
+    public List<Apple> getAllApples() {
+        List<Apple> apples = new ArrayList<>();
+        apples.add(new Apple("red", 100));
+        apples.add(new Apple("red", 160));
+        apples.add(new Apple("red", 200));
+        apples.add(new Apple("green", 200));
+        apples.add(new Apple("green", 90));
+        apples.add(new Apple("green", 50));
+        apples.add(new Apple("yellow", 50));
+        apples.add(new Apple("yellow", 170));
+        return apples;
+    }
+
+    public List<Apple> getApplesByColor(String color) {
+        return getAllApples().stream()
+                .filter(s -> s.getColor().equals(color))
+                .collect(toList());
+    }
+}
