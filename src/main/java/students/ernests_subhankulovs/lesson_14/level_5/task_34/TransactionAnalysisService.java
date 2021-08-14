bfeb0ef5744541abc53f2e090c6040e277f3a45d
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-08-14 17:14:12 +0300

    Lesson 14 - Level 4 - Task 34

---
+++ src/main/java/students/ernests_subhankulovs/lesson_14/level_5/task_34/TransactionAnalysisService.java
@@ -0,0 +1,82 @@
+package students.ernests_subhankulovs.lesson_14.level_5.task_34;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class TransactionAnalysisService {
+
+    public List<Transaction> findTransactionsByYear(List<Transaction> transactions, int year) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .collect(Collectors.toList());
+    }
+
+    public List<Transaction> sortTransactionsByValue(List<Transaction> transactions) {
+        return transactions.stream()
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .collect(Collectors.toList());
+
+    }
+
+    public List<Transaction> sortTransactionsByValueReversed(List<Transaction> transactions) {
+        return transactions.stream()
+                .sorted(Comparator.comparing(Transaction::getValue).reversed())
+                .collect(Collectors.toList());
+
+    }
+
+    public List<Transaction> findTransactionsByYearAndByValue(List<Transaction> transactions, int year) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .collect(Collectors.toList());
+
+    }
+
+    public List<Integer> findTransactionYears(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(Transaction::getYear)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> findDistinctTransactionYears(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(Transaction::getYear)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public List<String> findDistinctTraderCities(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getCity)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public List<String> findDistinctTraderNames(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getName)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public List<String> findTraderNamesByCity(List<Transaction> transactions, String city) {
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(trader -> trader.getCity().equals(city))
+                .map(Trader::getName)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public boolean checkTradersFromCity(List<Transaction> transactions, String city) {
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(trader -> trader.getCity().equals(city))
+                .map(Trader::getName)
+                .count() > 0;
+    }
+}
