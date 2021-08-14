ae7863177eb00677ea9a40b0a7772b58a1c1f7ba
Linda-Ju <linda.junkina@gmail.com>
2021-08-14 21:34:25 +0300

    Lesson 13, Level 5, Task 26-32

---
+++ src/main/java/students/linda_junkina/lesson_13/level_5/task_26_27_28_29_30_31_32/StringCalculator.java
@@ -0,0 +1,13 @@
+package students.linda_junkina.lesson_13.level_5.task_26_27_28_29_30_31_32;
+
+import java.util.Scanner;
+
+class StringCalculator {
+
+//    int add(String numbers){
+//
+//    }
+    public boolean emptyString(String input){
+        return false;
+    }
+}
