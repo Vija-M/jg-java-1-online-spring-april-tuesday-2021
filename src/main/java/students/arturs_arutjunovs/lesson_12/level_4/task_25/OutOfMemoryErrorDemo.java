6ce17f3939f6fb6a96cc1ebbcf673da5d6c7d283
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-08-16 12:17:44 +0300

    Completed Lesson 12 level 4 task 25

---
+++ src/main/java/students/arturs_arutjunovs/lesson_12/level_4/task_25/OutOfMemoryErrorDemo.java
@@ -0,0 +1,15 @@
+package students.arturs_arutjunovs.lesson_12.level_4.task_25;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class OutOfMemoryErrorDemo {
+    public static void main(String[] args) {
+
+        List<Object> objectList = new ArrayList<>();
+        while (true) {
+            objectList.add(new Object());
+            System.out.println("Current list size: " + objectList.size());
+        }
+    }
+}
