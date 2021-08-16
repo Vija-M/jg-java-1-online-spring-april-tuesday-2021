ca9b438d9e7e56569fc10dd5a16a5b9b2a3ae693
Vija-M <vija.riga@gmailcom>
2021-08-16 17:26:20 +0300

    Lesson-11, Level-2, Task-6

---
+++ src/main/java/students/vija_m/lesson_11/level_2/task_6/InMemoryBookRepository.java
@@ -0,0 +1,15 @@
+package students.vija_m.lesson_11.level_2.task_6;
+
+import java.util.ArrayList;
+
+class InMemoryBookRepository implements BookRepository {
+    ArrayList<Book> bookDataBase = new ArrayList<>();
+
+    @Override
+    public Long save(Book book) {
+        Long newID = (long) (bookDataBase.size() + 1);
+        book.setId(newID);
+        bookDataBase.add(book);
+        return newID;
+    }
+}
