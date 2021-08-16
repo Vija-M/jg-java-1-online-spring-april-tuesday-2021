1ae88177280ba49b78056d9f98eb3ca452c14afe
Vija-M <vija.riga@gmailcom>
2021-08-16 15:23:44 +0300

    Lesson-11, Level-1, Task-5

--- src/main/java/students/vija_m/lesson_11/level_1/task_5/BookListContainsDemo.java
+++ src/main/java/students/vija_m/lesson_11/level_1/task_5/BookListContainsDemo.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 public class BookListContainsDemo {
     public static void main(String[] args) {
-
         List<Book> books = new ArrayList<>();
-
         Book book1 = new Book("A1", "B1");
         Book book2 = new Book("A2", "B2");
 
@@ -16,8 +14,10 @@ public class BookListContainsDemo {
 
         Book bookInTheList = new Book("A1", "B1");
         boolean containsBook = books.contains(bookInTheList); // должно быть true
+        System.out.println("containsBook = " + containsBook);
 
         Book bookNotInTheList = new Book("A3", "B3");
         boolean notContainsBook = books.contains(bookNotInTheList); // должно быть false
+        System.out.println("notContainsBook = " + notContainsBook);
     }
 }
