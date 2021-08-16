ca221567706ddb816f3e7b0cda63eecac829a6a3
Vija-M <vija.riga@gmailcom>
2021-08-16 20:55:47 +0300

    Lesson-11, Level-2, Tasks-12,13,14

---
+++ src/main/java/students/vija_m/lesson_12/level_2/task_12_13_14/BookRepository.java
@@ -0,0 +1,24 @@
+package students.vija_m.lesson_12.level_2.task_12_13_14;
+
+import java.util.List;
+import java.util.Optional;
+
+interface BookRepository {
+    Long save(Book book);
+
+    boolean delete(Long bookId);
+
+    boolean delete(Book book);
+
+    Optional<Book> findById(Long bookId);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    int count();
+
+    void deleteByAuthor(String author);
+
+    void deleteByTitle(String title);
+}
