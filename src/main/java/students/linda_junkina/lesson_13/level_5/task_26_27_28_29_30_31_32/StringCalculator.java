730c026d4fe8dfcd7dbf7aa63453d2698f49c89e
Linda-Ju <linda.junkina@gmail.com>
2021-08-19 22:41:33 +0300

    Lesson 13, Level 5, Task 26-32

--- src/main/java/students/linda_junkina/lesson_13/level_5/task_26_27_28_29_30_31_32/StringCalculator.java
+++ src/main/java/students/linda_junkina/lesson_13/level_5/task_26_27_28_29_30_31_32/StringCalculator.java
@@ -1,24 +1,23 @@
 package students.linda_junkina.lesson_13.level_5.task_26_27_28_29_30_31_32;
 
-
-import java.util.stream.Stream;
-
 class StringCalculator {
 
     int add(String input) {
-        String delimiters = ",|\n";
-        if(input.startsWith("//")){
+        String delimiters = ",\n";
+        if (input.startsWith("//")) {
             String[] integers = input.split("\n", 2);
             delimiters = integers[0].substring(2);
             input = integers[1];
         }
-        String[] integers = input.split(delimiters);
+
+        String[] integers = input.split("[,\\n" + delimiters + "]");
         if (input.isBlank()) {
             return 0;
         } else {
             return calculateSum(integers);
         }
     }
+
     public int calculateSum(String[] integers) {
         int sum = 0;
         for (String integer : integers) {
