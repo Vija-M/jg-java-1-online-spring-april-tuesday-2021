cb624a9d42eb5fa50419ade8e239ae09beb39153
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-08-20 00:12:13 +0300

    Lesson 15 - Level 2 - Task 6

--- src/main/java/students/ernests_subhankulovs/lesson_15/level_2/task_6/QuadraticEq.java
+++ src/main/java/students/ernests_subhankulovs/lesson_15/level_2/task_6/QuadraticEq.java
@@ -2,24 +2,31 @@ package students.ernests_subhankulovs.lesson_15.level_2.task_6;
 
 class QuadraticEq {
 
-    public void calc(double a, double b, double c) {
+    public String calc(double a, double b, double c) {
         double discriminant = b * b - 4 * a * c;
         if (discriminant > 0) {
-            System.out.println(discriminantGreaterThanZero(a, b, discriminant));
+            String result = discriminantGreaterThanZero(a, b, discriminant);
+            System.out.println(result);
+            return result;
         }
         else if (discriminant == 0) {
-            System.out.println("x = " + discriminantEqualsZero(a, b));
+            String  result = "x = " + discriminantEqualsZero(a, b);
+            System.out.println(result);
+            return result;
         }
         else {
-            System.out.println(discriminantLessThanZero());
+            String result = discriminantLessThanZero();
+            System.out.println(result);
+            return result;
         }
     }
 
     public String discriminantGreaterThanZero(double a, double b, double discriminant) {
         double x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
         double x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
-        System.out.println("x1 = " + x1 + ", x2 = " + x2);
-        return "x1 = " + x1 + ", x2 = " + x2;
+        String result = "x1 = " + x1 + ", x2 = " + x2;
+        System.out.println(result);
+        return result;
     }
 
     public double discriminantEqualsZero(double a, double b) {
--- src/test/java/students/ernests_subhankulovs/lesson_15/level_2/task_6/QuadraticEqTest.java
+++ src/test/java/students/ernests_subhankulovs/lesson_15/level_2/task_6/QuadraticEqTest.java
@@ -1,7 +1,14 @@
 package students.ernests_subhankulovs.lesson_15.level_2.task_6;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuadraticEqTest {
-
+    @Test
+    void testDiscriminantGreaterThanZero() {
+        QuadraticEq quadraticEq = new QuadraticEq();
+        Assertions.assertEquals("x1 = -1.0, x2 = -0.5", quadraticEq.calc(2, 3, 1));
+    }
 }
