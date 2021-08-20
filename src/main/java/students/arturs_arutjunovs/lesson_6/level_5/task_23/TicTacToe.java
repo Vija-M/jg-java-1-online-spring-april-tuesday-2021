1e759dd328893e674e490e29e56d9222f383b206
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-08-20 15:14:48 +0300

    Completed Lesson 6 level 5 task 23

---
+++ src/main/java/students/arturs_arutjunovs/lesson_6/level_5/task_23/TicTacToe.java
@@ -0,0 +1,15 @@
+package students.arturs_arutjunovs.lesson_6.level_5.task_23;
+
+class TicTacToe {
+
+    public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i] == playerToCheck &&
+                    field[1][i] == playerToCheck &&
+                    field[2][i] == playerToCheck) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
