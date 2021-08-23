a189a0fad4d0c653becc3a0fbf670d1bdc946d16
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-08-23 21:53:40 +0300

    Lesson 15 - Level 3 - Task 10. Corrected.

--- src/main/java/students/ernests_subhankulovs/lesson_15/level_3/task_10/TennisGame3.java
+++ src/main/java/students/ernests_subhankulovs/lesson_15/level_3/task_10/TennisGame3.java
@@ -2,37 +2,37 @@ package students.ernests_subhankulovs.lesson_15.level_3.task_10;
 
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int p1point;
+    private int p2point;
+    private String player1Name;
+    private String player2Name;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        if (p1point < 4 && p2point < 4 && p1point + p2point != 6) {
+            String[] p = new String[] { "Love", "Fifteen", "Thirty", "Forty" };
+            s = p[p1point];
+            return (p1point == p2point) ? s + "-All" : s + "-" + p[p2point];
         } else {
-            if (p1 == p2) {
+            if (p1point == p2point) {
                 return "Deuce";
             }
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = p1point > p2point ? player1Name : player2Name;
+            return ((p1point - p2point) * (p1point - p2point) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1") {
-            this.p1 += 1;
+        if (playerName.equals("player1")) {
+            this.p1point += 1;
         }
         else {
-            this.p2 += 1;
+            this.p2point += 1;
         }
     }
 
