6c1b49f0547d8fca908557f69cef71a20138db2c
Ernests Subhankulovs <ernests.subhankulovs@gmail.com>
2021-08-23 22:10:06 +0300

    Lesson 15 - Level 3 - Task 11

---
+++ src/main/java/students/ernests_subhankulovs/lesson_15/level_3/task_11/TennisGame.java
@@ -0,0 +1,6 @@
+package students.ernests_subhankulovs.lesson_15.level_3.task_11;
+
+interface TennisGame {
+    void wonPoint(String playerName);
+    String getScore();
+}
---
+++ src/main/java/students/ernests_subhankulovs/lesson_15/level_3/task_11/TennisGame3.java
@@ -0,0 +1,39 @@
+package students.ernests_subhankulovs.lesson_15.level_3.task_11;
+
+public class TennisGame3 implements TennisGame {
+
+    private int p1point;
+    private int p2point;
+    private String player1Name;
+    private String player2Name;
+
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public String getScore() {
+        String s;
+        if (p1point < 4 && p2point < 4 && p1point + p2point != 6) {
+            String[] p = new String[] { "Love", "Fifteen", "Thirty", "Forty" };
+            s = p[p1point];
+            return (p1point == p2point) ? s + "-All" : s + "-" + p[p2point];
+        } else {
+            if (p1point == p2point) {
+                return "Deuce";
+            }
+            s = p1point > p2point ? player1Name : player2Name;
+            return ((p1point - p2point) * (p1point - p2point) == 1) ? "Advantage " + s : "Win for " + s;
+        }
+    }
+
+    public void wonPoint(String playerName) {
+        if (playerName.equals("player1")) {
+            this.p1point += 1;
+        }
+        else {
+            this.p2point += 1;
+        }
+    }
+
+}
